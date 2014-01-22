1869a4a8171c1849afd80e2aac752d1a3dc8dd66
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2014-01-22 15:23:15 +0100

    [FIX] added i18n, security and fix test

--- pricelist_configurator_by_bom/__openerp__.py
+++ pricelist_configurator_by_bom/__openerp__.py
@@ -23,17 +23,39 @@
 {
     'name': 'Pricelist Configurator By Bom',
     'version': '0.1',
-    'category': 'Generic Modules/Others',
+    'category': 'Sales Management',
     'license': 'AGPL-3',
     'description': """
+Introduction
+------------
+This module is a calculator that establishes the final product’s price based on
+various margins on the single cost of the bill of material. In OpenERP the
+product’s price is on the price list, every partner can have his price list and
+every price list is connected to a product. So this is a calculator for the
+price list.
+
+This module provides a configurator where the user selects the final product
+and the partner. The system propose the BOM with the cost of every component.
+The User can finally set the margin for every component.
+
+How To Use
+----------
+ - In the configurator, fill the Product and Partner fields, the system will
+   automatically set the bom (changeable) and the lines
+ - Work on the several lines
+ - Create or Update the Price List
+ - Compute the final price
 
     """,
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com/',
-    'depends': ['product','sale','mrp'],
+    'depends': ['product', 'sale', 'mrp'],
     'init_xml': [],
     'data': [
-           'pricelist_configurator_by_bom_view.xml',
+            'pricelist_configurator_by_bom_view.xml',
+    ],
+    'update_xml': [
+        'security/ir.model.access.csv'
     ],
     'test': [
         'test/pricelist_configurator_by_bom.yml'
