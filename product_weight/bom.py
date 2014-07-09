1095249e33505b8d4c2988eb5fdc529c993adb83
Florian da Costa <florian.dacosta@akretion.com>
2014-07-09 11:16:48 +0200

    [MIG] product_weight: migration to 9.0 + add gross weight calculation and possibility to calculate weight for multi products

--- product_weight/bom.py
+++ product_weight/bom.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#    Copyright (C) 2015 Akretion (<http://www.akretion.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,5 +18,19 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import bom
-from . import wizard
+
+from openerp import models, api
+
+
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+
+    @api.multi
+    def get_final_components(self):
+        bom_lines = []
+        for line in self:
+            if not line.child_line_ids:
+                bom_lines.append(line)
+            else:
+                bom_lines.extend(line.child_line_ids.get_final_components())
+        return bom_lines
