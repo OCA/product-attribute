19fc81c183a3815a82789c498105cd9d1f5a7855
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-10-21 12:22:41 +0200

    [NEW product_variant_default_code]

---
+++ models/product.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from openerp import models, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def create(self, values):
+        product_tmpl_id = values.get('product_tmpl_id', False)
+        attribute_value_ids = values.get('attribute_value_ids', False)
+        product = super(ProductProduct, self).create(values)
+        if product_tmpl_id and attribute_value_ids:
+            default_code = False
+            if product.attribute_value_ids:
+                for attribute in product.attribute_value_ids:
+                    if not default_code:
+                        default_code = attribute.name
+                    else:
+                        default_code += '/' + attribute.name
+            product.default_code = default_code
+        return product
