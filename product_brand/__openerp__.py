4575f5efe1cfaacec32c2cc2bb67a0ce1c6fad22
vrms <vrms@netcologne.de>
2015-01-28 07:18:43 +0800

    added back lines 32 - 54
    removing them to the README made the travis fail

--- product_brand/__openerp__.py
+++ product_brand/__openerp__.py
@@ -28,3 +28,27 @@
 # Product Brand is an Openobject module wich enable Brand management for      #
 # products                                                                    #
 ###############################################################################
+{
+'name': 'Product Brand Manager',
+'version': '0.1',
+'category': 'Product',
+'summary': ' brand',
+'description': """
+Product Brand Manager
+=====================
+This module allows your user to easily manage product brands. You can
+define brands, attach a logo and a description to them. It also allows to
+attach a partner to a brand. Once installed, check the menu
+Product/configuration/brand
+To do / To come:
+- A view to seeing products by brand.
+""",
+'author': 'Prisnet Telecommunications SA ',
+'website': 'http://www.prisnet.ch ',
+'depends': ['product'],
+'data': [
+'product_brand_view.xml',
+'security/ir.model.access.csv'
+],
+'installable': True,
+}
