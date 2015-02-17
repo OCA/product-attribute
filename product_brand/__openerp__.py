d01903cadbce13dfab89a8fcdd656ccc02976a96
Leonardo Donelli <learts92@gmail.com>
2015-02-17 12:48:37 +0100

    [IMP]product_brand: Several improvements
    * add smart button, move menu to a more visible position.
    * show brand in product.template kanban and tree views.
    * show brand in product variant kanban and tree views.
    * add product_brand kanban view.
    * update module's README and manifest file.
    * Search and group by brand for both product.product and product.template.
    * Convert model to new APIs.
    * Refactor products_count computation using product_ids one2many field.
    * Add public read access to product.brand (fixes 403 error on webshop for public user).
    * Make brand name required.

--- product_brand/__openerp__.py
+++ product_brand/__openerp__.py
@@ -30,17 +30,16 @@
 ###############################################################################
 {
     'name': 'Product Brand Manager',
-    'version': '0.1',
+    'version': '8.0.0.1.0',
     'category': 'Product',
-    'summary': ' brand',
-    'author': "NetAndCo, Akretion,"
-              "Prisnet Telecommunications SA,"
-              "Odoo Community Association (OCA)",
+    'summary': 'Add brand to products',
+    'author': 'NetAndCo, Akretion, Prisnet Telecommunications SA'
+              ', MONK Software, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'depends': ['product'],
     'data': [
         'product_brand_view.xml',
         'security/ir.model.access.csv'
-        ],
+    ],
     'installable': True,
 }
