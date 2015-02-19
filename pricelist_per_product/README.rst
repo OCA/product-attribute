961a33323720ec2a44f96ca6eabc8598238f02eb
Sylvain Calador <sylvain.calador@akretion.com>
2015-02-19 19:35:33 +0100

    module pricelist_per_product

---
+++ pricelist_per_product/README.rst
@@ -0,0 +1,59 @@
+Pricelist Per Product
+======================
+
+This module allows to see all sale prices on product form
+
+Installation
+============
+
+One click installation
+
+
+Configuration
+=============
+
+* Sales / Customer feature :
+  check "Use pricelists to adapt your price per customers"
+* Menu Sales / Configuration / Pricelist Versions
+  Check Product Price Grid field in your pricelists of your choice.
+
+.. image:: static/description/pricelist.png
+
+
+Usage
+=====
+
+ * Go to the product form (Sales tab)
+
+.. image:: static/description/product.png
+
+
+
+Known issues / Roadmap
+======================
+
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain CALADOR <sylvain.calador@akretion.com>
+* David BEAL <david.beal@akretion.com>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+
+
---
+++ pricelist_per_product/pricelist.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: David BEAL
+#    Copyright 2015 Akretion
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ProductPricelist(models.Model):
+    _inherit = 'product.pricelist'
+
+    price_grid = fields.Boolean(
+        string='Product Price Grid',
+        help="Define if the price list items are fulfilled from product form\n"
+             "with a grid of specific values for each product")
+
+
+class ProductPricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    date_end = fields.Date(
+        related='price_version_id.date_end',
+        readonly=True)
+    currency_name = fields.Many2one(
+        related='price_version_id.pricelist_id.currency_id',
+        readonly=True)
