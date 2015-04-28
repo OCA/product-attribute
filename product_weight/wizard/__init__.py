e31f07e4a172368e2b58860e83f11baf5b68c6eb
Florian da Costa <florian.dacosta@akretion.com>
2015-04-28 18:18:26 +0200

    add readme file,fix pep8

--- product_weight/wizard/__init__.py
+++ product_weight/wizard/__init__.py
@@ -4,6 +4,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#    Copyright (C) 2015 Akretion (<http://www.akretion.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,4 +21,4 @@
 #
 ##############################################################################
 
-import product_weight_update
+from . import product_weight_update
