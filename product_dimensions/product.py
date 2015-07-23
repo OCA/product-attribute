3b74b836c96eaf747acdabb52f7cfcb4389ae268
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-23 13:41:59 -0300

    IMP more manifest impr

--- product_dimensions/product.py
+++ product_dimensions/product.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
 
 
 from openerp.osv import osv
