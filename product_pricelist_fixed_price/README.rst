ebe6fd1e757e02524c84050873dbdee5252cc455
gfcapalbo <giovanni@therp.nl>
2015-07-24 13:39:49 +0200

    [FIX] Ported to 8.0

---
+++ product_pricelist_fixed_price/README.rst
@@ -0,0 +1,30 @@
+Fixed price on pricelist rule
+=============================
+Adds a new option on pricelist rules to set a fixed price. This is made using
+a trick that writes on the back 100% in the discount to the base price to get
+a zero base that will add only the price we put in the surcharge price.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+* Giovanni Francesco Capalbo <giovanni@therp.nl>
+* Ronald Portier <ronald@therp.nl>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
--- product_pricelist_fixed_price/model/product_pricelist_item.py
+++ product_pricelist_fixed_price/model/product_pricelist_item.py
@@ -1,51 +1,33 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com> 
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com).
+#        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models, _
 
 
-class product_pricelist_item(orm.Model):
+class ProductPricelistItem(models.Model):
     _inherit = 'product.pricelist.item'
 
-    def _price_field_get_ext(self, cr, uid, context=None):
-        result = super(product_pricelist_item, self)._price_field_get(
-                                    cr, uid, context=context)
+    def _price_field_get_ext(self):
+        """Add fixed price to pricetype selection."""
+        result = super(ProductPricelistItem, self)._price_field_get()
         result.append((-3, _('Fixed Price')))
         return result
 
-    _columns = {
-        'base_ext': fields.selection(_price_field_get_ext, 'Based on',
-                                     required=True, size=-1,
-                                     help="Base price for computation."),
-    }
-    _defaults = {
-        'base_ext': -1,
-    }
+    @api.onchange('base_ext')
+    def change_base_ext(self):
+        if self.base_ext == -3:
+            base = self.env['product.price.type'].search(
+                [], limit=1, order='id'
+            )
+            self.base = base[0]
+            self.price_discount = -1
 
-    def onchange_base_ext(self, cr, uid, ids, base_ext, context=None):
-        if base_ext == -3:
-            # Simulate be based on first found price that allows the trick
-            return {
-                'value': {'base': 1,
-                          'price_discount': -1,}
-            }
-        return {'value': {'base': base_ext}}
+    base_ext = fields.Selection(
+        selection='_price_field_get_ext',
+        string='Based on',
+        required=True,
+        size=-1,
+        default=-1,
+        help="Base price for computation",
+    )
