bc7e8988c2f035853fb4f14dde3fd4adf5b93549
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-07-30 09:41:11 +0200

    lint

--- product_dimensions/__openerp__.py
+++ product_dimensions/__openerp__.py
@@ -27,7 +27,8 @@
     'description': """
 Product Dimensions
 ==================
-Add dimensions (length, width and height) products. Find the volume automatically when you change one of these dimensions.
+Add dimensions (length, width and height) products. Find the volume
+automatically when you change one of these dimensions.
     """,
     'author':  'ADHOC SA',
     'website': 'www.adhoc.com.ar',
@@ -39,12 +40,4 @@ Add dimensions (length, width and height) products. Find the volume automaticall
     'data': [
         'product_view.xml',
     ],
-    'demo': [
-    ],
-    'test': [
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- product_dimensions/product.py
+++ product_dimensions/product.py
@@ -7,18 +7,20 @@
 
 from openerp.osv import osv
 from openerp.osv import fields
-from openerp.tools.translate import _
+
 
 class product_product(osv.osv):
     _name = 'product.product'
     _inherit = 'product.product'
-    
-    def onchange_calculate_volume(self, cr, uid, ids, length, high, width, dimensional_uom_id, context=None):
+
+    def onchange_calculate_volume(self, cr, uid, ids, length, high, width,
+                                  dimensional_uom_id, context=None):
         v = {}
         if not length or not high or not width or not dimensional_uom_id:
             volume = False
         else:
-            dimensional_uom = self.pool.get('product.uom').browse(cr, uid, dimensional_uom_id, context=context)
+            dimensional_uom = self.pool.get('product.uom').browse(
+                cr, uid, dimensional_uom_id, context=context)
             length_m = self.get_measure_in_meters(length, dimensional_uom)
             high_m = self.get_measure_in_meters(high, dimensional_uom)
             width_m = self.get_measure_in_meters(width, dimensional_uom)
@@ -28,12 +30,12 @@ class product_product(osv.osv):
                 volume = length_m * high_m * width_m
         v['volume'] = volume
         return {'value': v}
-            
+
     def get_measure_in_meters(self, measure, dimensional_uom):
         if not dimensional_uom:
             return None
-        
-        measure = float(measure)    
+
+        measure = float(measure)
         if dimensional_uom.name == 'm':
             return measure
         elif dimensional_uom.name == 'km':
@@ -42,23 +44,14 @@ class product_product(osv.osv):
             return measure / 100
         else:
             return None
-    
+
     _columns = {
-        'length': fields.float('Largo'), # Largo
-        'high': fields.float('Alto'), # Alto
-        'width': fields.float('Ancho'), # Ancho
-        'dimensional_uom': fields.many2one('product.uom', 'UdM dimensional',
-                                           domain="[('category_id.name', '=', 'Length / Distance')]",
-                                           help='Unidad de Medida Dimensional para Largo, Alto y Ancho'),
+        'length': fields.float('Largo'),  # Largo
+        'high': fields.float('Alto'),  # Alto
+        'width': fields.float('Ancho'),  # Ancho
+        'dimensional_uom': fields.many2one(
+            'product.uom',
+            'UdM dimensional',
+            domain="[('category_id.name', '=', 'Length / Distance')]",
+            help='Unidad de Medida Dimensional para Largo, Alto y Ancho'),
     }
-
-product_product()
-
-
-
-
-
-
-
-
-
