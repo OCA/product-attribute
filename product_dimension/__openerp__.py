648164ec27f29b8cfd25bfd9664c4e0d65a6f1d0
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-08-03 12:06:55 +0200

    rename product_dimensions -> product_dimension

--- product_dimension/__openerp__.py
+++ product_dimension/__openerp__.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {
-    'name': 'Product Dimensions',
+    'name': 'Product Dimension',
     'version': '1.1',
     'category': 'Product',
     'author':  'ADHOC SA,Odoo Community Association (OCA)',
