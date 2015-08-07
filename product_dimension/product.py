ead7325907e07c344e1f3470230b140b9379126c
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-08-07 15:25:23 +0200

    use a new-api onchange, update tests, refactor
    Also, spell "height" correctly.

--- product_dimension/product.py
+++ product_dimension/product.py
@@ -17,31 +17,22 @@
 
 
 from openerp import models, fields
-from openerp.api import multi
+from openerp import api
 
 
 class Product(models.Model):
     _inherit = 'product.product'
 
-    @multi
-    def compute_volume(self, length, heigth, width, dimensional_uom_id):
-        if not length or not heigth or not width or not dimensional_uom_id:
+    @api.onchange('length', 'height', 'width', 'dimensional_uom_id')
+    def onchange_calculate_volume(self):
+        if (not self.length or not self.height or not self.width
+                or not self.dimensional_uom_id):
             return False
-        else:
-            dimensional_uom = self.env['product.uom'].browse(
-                dimensional_uom_id)
-            length_m = self.convert_to_meters(length, dimensional_uom)
-            heigth_m = self.convert_to_meters(heigth, dimensional_uom)
-            width_m = self.convert_to_meters(width, dimensional_uom)
-            return length_m * heigth_m * width_m
 
-    @multi
-    def onchange_calculate_volume(self, length, heigth, width,
-                                  dimensional_uom_id):
-        return {'value':
-                {'volume': self.compute_volume(length, heigth, width,
-                                               dimensional_uom_id)}
-                }
+        length_m = self.convert_to_meters(self.length, self.dimensional_uom_id)
+        height_m = self.convert_to_meters(self.height, self.dimensional_uom_id)
+        width_m = self.convert_to_meters(self.width, self.dimensional_uom_id)
+        self.volume = length_m * height_m * width_m
 
     def convert_to_meters(self, measure, dimensional_uom):
         uom_meters = self.env['product.uom'].search([('name', '=', 'm')])
@@ -52,10 +43,10 @@ class Product(models.Model):
             to_unit=uom_meters)
 
     length = fields.Float()
-    heigth = fields.Float(oldname='high')
+    height = fields.Float(oldname='high')
     width = fields.Float()
-    dimensional_uom = fields.Many2one(
+    dimensional_uom_id = fields.Many2one(
         'product.uom',
         'Dimensional UoM',
         domain="[('category_id.name', '=', 'Length / Distance')]",
-        help='UoM for length, heigth, width')
+        help='UoM for length, height, width')
--- product_dimension/tests/test_compute_volume.py
+++ product_dimension/tests/test_compute_volume.py
@@ -19,25 +19,30 @@ from openerp.tests.common import TransactionCase
 class TestComputeVolume(TransactionCase):
 
     def test_it_computes_volume_in_cm(self):
+        self.product.length = 10.
+        self.product.height = 200.
+        self.product.width = 100.
+        self.product.dimensional_uom_id = self.uom_cm
+        self.product.onchange_calculate_volume()
         self.assertAlmostEqual(
             0.2,
-            self.Product.onchange_calculate_volume(
-                10., 200., 100., self.uom_cm.id
-            )['value']['volume']
+            self.product.volume
         )
 
     def test_it_computes_volume_in_meters(self):
-
+        self.product.length = 6.
+        self.product.height = 2.
+        self.product.width = 10.
+        self.product.dimensional_uom_id = self.uom_m
+        self.product.onchange_calculate_volume()
         self.assertAlmostEqual(
-            120.,
-            self.Product.onchange_calculate_volume(
-                6., 2., 10., self.uom_m.id
-            )['value']['volume']
+            120,
+            self.product.volume
         )
 
     def setUp(self):
         super(TestComputeVolume, self).setUp()
 
-        self.Product = self.env['product.product']
+        self.product = self.env['product.product'].new()
         self.uom_m = self.env['product.uom'].search([('name', '=', 'm')])
         self.uom_cm = self.env['product.uom'].search([('name', '=', 'cm')])
