f6aa124d0e351e0d97aa1b8453d5bd2b2526ef29
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-08-18 14:31:48 +0200

    [IMP] improve display if there are many prices defined for a given product and supplier (depending of quantity);

--- product_with_supplier_unit_price/models/product_supplierinfo.py
+++ product_with_supplier_unit_price/models/product_supplierinfo.py
@@ -3,6 +3,8 @@
 #    
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as
@@ -21,6 +23,7 @@
 
 from openerp import fields, api
 from openerp.models import Model
+from openerp.tools.translate import _
 
 
 class ProductSupplierinfo(Model):
@@ -29,11 +32,37 @@ class ProductSupplierinfo(Model):
     @api.multi
     def _get_unit_price(self):
         for supplierinfo in self:
-            unit_price = 0
-            for item in supplierinfo.pricelist_ids:
-                if item.min_quantity == 1:
-                    unit_price = item.price
-            supplierinfo.unit_price = unit_price
+            if len(supplierinfo.pricelist_ids) == 0:
+                supplierinfo.unit_price = 0
+                supplierinfo.unit_price_note = _('-')
+            elif len(supplierinfo.pricelist_ids) == 1:
+                item = supplierinfo.pricelist_ids[0]
+                supplierinfo.unit_price = item.price
+                if item.min_quantity not in (0, 1):
+                    supplierinfo.unit_price_note = _("%s (Min Qty: %s)" % (
+                        item.price, item.min_quantity))
+                else:
+                    supplierinfo.unit_price_note = item.price
+            else:
+                min_qty = max_qty = supplierinfo.pricelist_ids[0].min_quantity
+                min_price = max_price = supplierinfo.pricelist_ids[0].price
+                for item in supplierinfo.pricelist_ids:
+                    if item.min_quantity < min_qty:
+                        min_qty = item.min_quantity
+                        min_price = item.price
+                    if item.min_quantity > max_qty:
+                        max_qty = item.min_quantity
+                        max_price = item.price
+                supplierinfo.unit_price = min_price
+                supplierinfo.unit_price_note = _(
+                    "%s (Min Qty: %s) >> %s (Min Qty: %s)" % (
+                        min_price, min_qty, max_price, max_qty))
+
+    unit_price_note = fields.Char(
+        compute=_get_unit_price, multi='unit_price', string='Unit Price')
 
     unit_price = fields.Float(
-        compute=_get_unit_price)
+        compute=_get_unit_price, multi='unit_price',
+        help="""Purchase Price of the product for this supplier. \n If many"""
+        """ prices are defined, The price will be the price associated with"""
+        """ the smallest quantity.""")
