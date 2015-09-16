57a1f080d823cdbb347b848df860f72237739210
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-09-16 08:50:34 +0200

    [REF] New OCA Convention : change 'encoding' into 'coding';

--- product_standard_price_vat_incl/__openerp__.py
+++ product_standard_price_vat_incl/__openerp__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Product - Standard Price VAT Included Module for Odoo
