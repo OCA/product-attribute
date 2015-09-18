41ceee929fc3d989251d032d0c0154ddd0692e44
Ondřej Kuzník <ondrej.kuznik@credativ.co.uk>
2015-09-18 15:18:05 +0100

    [ADD] Product image tab

---
+++ product_image_tab/README.md
@@ -0,0 +1,2 @@
+This module adds a new tab to the product screen where the image is displayed
+in full size.
---
+++ product_image_tab/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Show full product image',
+    'version': '1.0',
+    'category': 'UI',
+    'author': 'credativ ltd',
+    'website': 'http://www.credativ.co.uk',
+    'depends': [
+        'product',
+    ],
+    'data': ['views/product_view.xml'],
+    'installable': True,
+}
