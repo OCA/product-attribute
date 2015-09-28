cc75e9a9144c561070b9869095b3b4ea517fb92a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-09-28 16:50:37 +0200

    product_supplierinfo_for_customer: Create "demo" data, and "tests" folder.

--- product_customerinfo/models/product_pricelist.py
+++ product_customerinfo/models/product_pricelist.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
 from openerp import models, api
 
 
