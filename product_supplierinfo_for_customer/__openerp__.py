odoomrp/odoomrp-wip

[ADD] <product_supplierinfo_for_customer>

product_supplierinfo_for_customer/__openerp__.py
@@ -1,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Use product supplier info for customers too",
+    "version": "1.0",
+    "depends": [
+        "product",
+    ],
+    "author": "OdooMRP team",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+    ],
+    "category": "Custom Module",
+    "website": "http://www.odoomrp.com",
+    "summary": "",
+    "description": """
+This module extends product.supplierinfo object with a combo to allow using
+it for sale pricelist
+    """,
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}

product_supplierinfo_for_customer/models/product_template.py
@@ -1,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    customer_ids = fields.One2many(comodel_name='product.supplierinfo',
+                                   inverse_name='product_tmpl_id',
+                                   string='Customer',
+                                   domain=[('type', '=', 'customer')])
+    seller_ids = fields.One2many(comodel_name='product.supplierinfo',
+                                 inverse_name='product_tmpl_id',
+                                 domain=[('type', '=', 'supplier')])

OCA/product-attribute

product_supplierinfo_for_customer/__openerp__.py
@@ -1,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Use product supplier info for customers too",
+    "version": "1.0",
+    "depends": [
+        "base",
+        "product",
+    ],
+    "author": "OdooMRP team",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+    ],
+    "category": "Sales Management",
+    "website": "http://www.odoomrp.com",
+    "summary": "",
+    "description": """
+This module extends product.supplierinfo object with a combo to allow using
+it for sale pricelists
+    """,
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}

product_supplierinfo_for_customer/models/product_template.py
@@ -1,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    customer_ids = fields.One2many(comodel_name='product.supplierinfo',
+                                   inverse_name='product_tmpl_id',
+                                   string='Customer',
+                                   domain=[('type', '=', 'customer')])
+    supplier_ids = fields.One2many(comodel_name='product.supplierinfo',
+                                   inverse_name='product_tmpl_id',
+                                   string='Supplier',
+                                   domain=[('type', '=', 'supplier')])