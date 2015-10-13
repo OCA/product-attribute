ddf732ce2c4175d33902766bfd297bbcbac242b8
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-13 18:42:27 +0200

    These three addons are actually not installable
    And now I stop committing directly...

--- product_catalog_report/__openerp__.py
+++ product_catalog_report/__openerp__.py
@@ -1,5 +1,5 @@
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
@@ -14,7 +14,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
@@ -29,7 +29,7 @@
     "demo_xml":[],
     "update_xml":['product_report.xml','product_wizard.xml'],
     "active":False,
-    "installable":True,
+    "installable":False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
