294cf687133d561b495f8d68263c21e3712ae515
Jairo Llopis <yajo.sk8@gmail.com>
2015-11-06 17:45:53 +0100

    [IMP] product_multi_image: Split model in abstract & concrete + better icon

---
+++ product_multi_image/models/product_template.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.osv import orm, fields
+
+
+class ProductTemplate(orm.Model):
+    """Reference core image fields to multi-image variants.
+
+    It is needed to use v7 api here because core model fields use the ``multi``
+    attribute, that has no equivalent in v8, and it needs to be disabled or
+    bad things will happen.
+    """
+    _name = "product.template"
+    _inherit = [_name, "base_multi_image.owner"]
+    _columns = {
+        "image": fields.related(
+            "image_main",
+            type="binary",
+            store=False,
+            multi=False),
+        "image_medium": fields.related(
+            "image_main_medium",
+            type="binary",
+            store=False,
+            multi=False),
+        "image_small": fields.related(
+            "image_main_small",
+            type="binary",
+            store=False,
+            multi=False)
+    }
