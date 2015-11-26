52f2a10e272e34430dbfbdf3eca628aaef8729b3
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-26 19:57:43 +0100

    coding: utf-8 instead of 'encoding'

--- product_analytic_account/__init__.py
+++ product_analytic_account/__init__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from . import product
 from . import account_invoice
--- product_analytic_account/account_invoice.py
+++ product_analytic_account/account_invoice.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Product Analytic Account module for Odoo
