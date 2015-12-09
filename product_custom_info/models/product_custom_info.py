612e7eea91b4a2c450d22bb8a9db7a48dd8e837b
Carlos Incaser <carlos@incaser.es>
2015-12-09 02:56:49 +0100

    [ADD][8.0] product_custom_info: Initial development

---
+++ product_custom_info/models/product_custom_info.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import models
+
+
+class ProductTemplate(models.Model):
+    _name = "product.template"
+    _inherit = ["product.template", "custom.info"]
