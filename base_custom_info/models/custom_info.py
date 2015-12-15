8d8f5aa587ba3be447ebd8a2f637ab558df36714
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-15 17:22:32 +0100

    Cleanup.

--- base_custom_info/models/custom_info.py
+++ base_custom_info/models/custom_info.py
@@ -4,7 +4,7 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models, _
+from openerp import api, fields, models
 
 
 class CustomInfoTemplate(models.Model):
