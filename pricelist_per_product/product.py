3dd91f34c00bca8ec86b50a0bbc5a7c53e9b3ca4
David Beal <david.beal@akretion.com>
2016-01-06 18:31:11 +0100

    [FIX] update price after intial with defaut values from product

--- pricelist_per_product/product.py
+++ pricelist_per_product/product.py
@@ -1,47 +1,12 @@
 # coding: utf-8
-##############################################################################
-#
-#    Author: Sylvain CALADOR
-#    Copyright 2015 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import api, fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-
-    def _set_pricelist_grid_info(self, version):
-        return {
-            'price_version_id': version.id,
-            'price_discount': -1,
-            'price_surcharge': 0.0,  # override me
-            'sequence': 1,
-            'base': 1,
-        }
-
-    def default_pricelist_item_ids(self):
-        """ You may inherit it """
-        versions = self.env['product.pricelist.version'].search(
-            [('pricelist_id.price_grid', '=', True)])
-        res = []
-        for version in versions:
-            res.append(self._set_pricelist_grid_info(version))
-        return res
 
     def _default_pricelist_item_ids(self):
         return self.default_pricelist_item_ids()
@@ -56,3 +21,49 @@ class ProductTemplate(models.Model):
         help="These prices are defined with absolute values\n"
              "(no calculation with discount)"
     )
+
+    def get_pricelist_version_domain(self):
+        return [('pricelist_id.price_grid', '=', True)]
+
+    def get_default_pricelist_item_vals(self, version):
+        return {
+            'price_version_id': version.id,
+            'price_discount': -1,
+            'base': 1,
+        }
+
+    def default_pricelist_item_ids(self):
+        versions = self.env['product.pricelist.version'].search(
+            self.get_pricelist_version_domain())
+        res = []
+        for version in versions:
+            res.append(self.get_default_pricelist_item_vals(version))
+        return res
+
+    @api.model
+    def create(self, vals):
+        if 'pricelist_item_ids' in vals:
+            self.create_or_write_pricelist(vals['pricelist_item_ids'])
+        return super(ProductTemplate, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if 'pricelist_item_ids' in vals:
+            self.create_or_write_pricelist(vals['pricelist_item_ids'])
+        return super(ProductTemplate, self).write(vals)
+
+    @api.model
+    def create_or_write_pricelist(self, pricelist_item_vals):
+        for version in pricelist_item_vals:
+            if version[2]:
+                self.update_pricelist_values(version[2])
+        return True
+
+    @api.model
+    def update_pricelist_values(self, vals):
+        vals.update({
+            'price_discount': -1,
+            'sequence': 1,
+            'base': 1,
+        })
+        return True
