984e964f3d37576c29dd7d934884f5905035f4d5
Alexis de Lattre <alexis.delattre@akretion.com>
2016-01-12 19:09:20 +0100

    Add module product_categ_image

---
+++ product_categ_image/models/product_category.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# © 2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+
+    image = fields.Binary(string='Image')
