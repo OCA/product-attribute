5bbcfa6260cccf5c681d686427a7e6efdafdadee
David Beal <david.beal@akretion.com>
2016-01-12 15:42:56 +0100

    simplify features, drop wizard, add a test unit

--- pricelist_per_product/__openerp__.py
+++ pricelist_per_product/__openerp__.py
@@ -1,28 +1,11 @@
 # coding: utf-8
-##############################################################################
-#
-#    Author: Sylvain CALADOR
-#    Copyright 2015 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Sylvain CALADOR @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Pricelist Per Product',
     'version': '1.0',
-    'author': 'Akretion',
+    'author': 'Akretion, Odoo Community Association (OCA)',
     'summarize': 'Display pricelist items in products',
     'maintainer': 'Akretion',
     'category': 'sale',
@@ -34,13 +17,13 @@
         'install_view.xml',
         'product_view.xml',
         'pricelist_view.xml',
-        'transient/view.xml',
         'data/pricelist.xml',
     ],
     'demo': [
         'demo/product.pricelist.item.csv',
     ],
     'tests': [],
+    'images': [],
     'installable': True,
     'license': 'AGPL-3',
     'external_dependencies': {
