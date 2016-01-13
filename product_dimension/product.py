237182462d38e6b1b5bfefc21aa8f36f6e0df286
David Beal <david.beal@akretion.com>
2016-01-13 17:08:36 +0100

    [FIX] flake 8 error on modules product_dimension, product_gtin, product_supplierinfo_tree_price_info

--- product_dimension/product.py
+++ product_dimension/product.py
@@ -25,8 +25,8 @@ class Product(models.Model):
 
     @api.onchange('length', 'height', 'width', 'dimensional_uom_id')
     def onchange_calculate_volume(self):
-        if (not self.length or not self.height or not self.width
-                or not self.dimensional_uom_id):
+        if (not self.length or not self.height or not self.width or
+                not self.dimensional_uom_id):
             return False
 
         length_m = self.convert_to_meters(self.length, self.dimensional_uom_id)
