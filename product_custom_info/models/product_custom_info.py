ae387357b26dc05cc78565e6a7904e753a7acf38
Jairo Llopis <yajo.sk8@gmail.com>
2016-01-15 18:38:52 +0100

    Update to latest base_custom_info code.

--- product_custom_info/models/product_custom_info.py
+++ product_custom_info/models/product_custom_info.py
@@ -3,9 +3,14 @@
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models
+from openerp import fields, models
 
 
 class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = ["product.template", "custom.info"]
+
+    custom_info_template_id = fields.Many2one(
+        context={"default_model": _name})
+    custom_info_ids = fields.One2many(
+        context={"default_model": _name})
