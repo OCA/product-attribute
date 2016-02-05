90df683d94c0515cf34f029e685217f1ebbb703f
Jairo Llopis <yajo.sk8@gmail.com>
2016-02-05 15:42:14 +0100

    Fix linter warning.

--- product_multi_image/models/product_template.py
+++ product_multi_image/models/product_template.py
@@ -4,7 +4,7 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models, fields
+from openerp import models, fields
 
 
 class ProductTemplate(models.Model):
