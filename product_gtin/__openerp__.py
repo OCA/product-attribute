1a280f7cf89d689535dad3697ff06ac36e4a69b5
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2016-02-17 10:34:24 +0100

    [9.0][MIG] product_gtin
    * Improve manifest with current OCA requirements
    * Add a README.rst
    * Move python files into subdirectory models
    * Move views into subdirectory views

--- product_gtin/__openerp__.py
+++ product_gtin/__openerp__.py
@@ -1,42 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Product GTIN module for Odoo
-#    Copyright (C) 2004-2011 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2011 Camptocamp Austria (<http://www.camptocamp.at>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2004-2011 Tiny SPRL (<http://tiny.be>)
+# © 2010-2011 Camptocamp Austria (<http://www.camptocamp.at>)
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Product GTIN EAN8 EAN13 UPC JPC Support",
-    "version": "8.0.1.1.0",
+    "version": "9.0.1.0.0",
     "author": "ChriCar Beteiligungs- und Beratungs- GmbH,"
-              "Odoo Community Association (OCA)",
+              "Odoo Community Association (OCA)"
+              "ACSONE SA/NV",
     "website": "http://www.chricar.at/ChriCar",
     "license": "AGPL-3",
     "category": "Sales Management",
-    "depends": ["product"],
-    "description": """
-Product GTIN module
-===================
-
-Replaces the EAN13 field on products, partners and packaging by a field
-with the same technical name that accepts EAN13, EAN8, JPC, UPC and GTIN.
-    """,
-    "demo": [],
-    "data": ['product_view.xml'],
-    'installable': False,
+    "depends": [
+        "product"
+    ],
+    "summary": "This module provides checks and management to EAN codes",
+    "data": [
+        'views/product_view.xml',
+    ],
+    "demo": [
+    ],
+    'installable': True,
 }
