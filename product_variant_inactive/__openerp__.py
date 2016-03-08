05244665a8f7f292e64da9a9040fa8795addb285
David Beal <david.beal@akretion.com>
2016-03-08 12:52:05 +0100

    [FIX] oca guidelines, add demo data

--- product_variant_inactive/__openerp__.py
+++ product_variant_inactive/__openerp__.py
@@ -1,34 +1,17 @@
-##############################################################################
-#
-#    Copyright (C) All Rights Reserved 2015 Akretion
-#    @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# coding: utf-8
+# © 2015 Abdessamad HILALI <abdessamad.hilali@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Product Variant",
+    'name': "Product Variant Inactive",
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': "http://www.Akretion.com",
+    'website': "http://www.akretion.com",
     'license': 'AGPL-3',
     'category': 'Product',
-    'version': '0.1',
-    'depends': ['product'],
+    'version': '8.0.0.0.0',
+    'depends': ['stock'],
     'data': [
         'product_variant_view.xml'
     ],
-    'demo': [
-        ],
+    'demo': ['product.product.csv'],
 }
--- product_variant_inactive/product_variant.py
+++ product_variant_inactive/product_variant.py
@@ -1,24 +1,7 @@
+# coding: utf-8
+# © 2015 Abdessamad HILALI <abdessamad.hilali@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) All Rights Reserved 2015 Akretion
-#    @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
 from openerp import models, api
 from lxml import etree
 from openerp.osv import orm
@@ -29,26 +12,24 @@ class ProductProduct(models.Model):
 
     @api.multi
     def button_activate(self):
-        self.active = True
+        self.write({'active': True})
 
     @api.multi
     def button_deactivate(self):
-        self.active = False
+        self.write({'active': False})
 
     @api.model
-    def fields_view_get(self,
-                        view_id=None,
-                        view_type='tree',
+    def fields_view_get(self, view_id=None, view_type='tree',
                         toolbar=False, submenu=False):
         """ Dynamic modification of fields """
         res = super(ProductProduct, self).fields_view_get(
-            view_id=view_id,
-            view_type=view_type,
-            toolbar=toolbar,
-            submenu=submenu)
+            view_id=view_id, view_type=view_type,
+            toolbar=toolbar, submenu=submenu)
         root = etree.fromstring(res['arch'])
         if view_type == 'tree':
             for button in root.findall(".//button"):
+                # search_disable_custom_filters is applied when active_id/ids
+                # defined in addons/web/static/src/js/views.js
                 if 'search_disable_custom_filters' in self.env.context:
                     button.set('invisible', '0')
                     orm.setup_modifiers(button, root)
