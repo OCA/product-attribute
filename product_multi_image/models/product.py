f15e682bcd46cb7042f0b602dc905098edaa4a2c
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-11 17:09:33 +0100

    Fix bugs found in review time, improve README.

--- product_multi_image/models/product.py
+++ product_multi_image/models/product.py
@@ -4,16 +4,17 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+"""Reference core image fields to multi-image variants.
+
+It is needed to use v7 api here because core model fields use the ``multi``
+attribute, that has no equivalent in v8, and it needs to be disabled or
+bad things will happen.
+"""
+
 from openerp.osv import orm, fields
 
 
 class ProductTemplate(orm.Model):
-    """Reference core image fields to multi-image variants.
-
-    It is needed to use v7 api here because core model fields use the ``multi``
-    attribute, that has no equivalent in v8, and it needs to be disabled or
-    bad things will happen.
-    """
     _name = "product.template"
     _inherit = [_name, "base_multi_image.owner"]
     _columns = {
