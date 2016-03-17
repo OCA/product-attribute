1892a98a7dc3cf5837a1e524fcb6e099b86b403a
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-03-17 17:20:35 +0530

    [PORT][9.0] product_m2m_categories/product_multi_category

---
+++ product_multi_category/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2009 Akretion,Guewen Baconnier,Camptocamp,Avanzosc,Sharoon Thomas,Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import product
