f62bf4e0f05cbe68a40bf1243aabb6d2ad1881a7
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-26 13:00:40 +0200

    product_multi_image: Handle images in variants

--- product_multi_image/models/product_template.py
+++ product_multi_image/models/product_template.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
-# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2014-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-"""Reference core image fields to multi-image variants.
-
-It is needed to use v7 api here because core model fields use the ``multi``
-attribute, that has no equivalent in v8, and it needs to be disabled or
-bad things will happen.
-"""
 
 from openerp.osv import orm, fields
 
 
 class ProductTemplate(orm.Model):
+    """Reference core image fields to multi-image variants.
+
+    It is needed to use v7 api here because core model fields use the ``multi``
+    attribute, that has no equivalent in v8, and it needs to be disabled or
+    bad things will happen. For more reference, see
+    https://github.com/odoo/odoo/issues/10799
+    """
     _name = "product.template"
     _inherit = [_name, "base_multi_image.owner"]
     _columns = {
