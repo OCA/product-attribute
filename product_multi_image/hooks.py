1ff0f1073fdd0713a900c77216ccdb57d38333a0
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-26 13:00:40 +0200

    product_multi_image: Handle images in variants

--- product_multi_image/hooks.py
+++ product_multi_image/hooks.py
@@ -2,7 +2,12 @@
 # © 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.addons.base_multi_image.hooks import pre_init_hook_for_submodules
+try:
+    from openerp.addons.base_multi_image.hooks import (
+        pre_init_hook_for_submodules)
+except ImportError:
+    # Don't complain, as this will be solved as dependency when needed
+    pass
 
 
 def pre_init_hook(cr):
