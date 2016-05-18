95378c743d89954be58f49ca5386c8ee0455d580
Luis Felipe Mileo <mileo@kmee.com.br>
2016-05-18 12:46:55 -0300

    Add new module: product_attribute_priority

---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Product attribute priority",
+    "version": "8.0.1.0.0",
+    "depends": [
+        "product",
+    ],
+    "author": "KMEE INFORMATICA LTDA,"
+              "Odoo Community Association (OCA)",
+    "contributors": [
+        "Luis Felipe Miléo <mileo@kmee.com.br>",
+    ],
+    "data": [
+        'views/product_view.xml',
+    ],
+    "license": "AGPL-3",
+    "category": "Product Management",
+    "website": "http://www.kmee.com.br",
+    "installable": True,
+}
