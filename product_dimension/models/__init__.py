5a6e569271f8a95b1df45cbf17072eb50acca4bd
Denis Leemann <denis.leemann@camptocamp.com>
2016-05-26 14:24:42 +0200

    Module structure, views & license headers
    [MIG] Make modules uninstallable
    
    [MIG] Rename manifest files

--- product_dimension/models/__init__.py
+++ product_dimension/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Camptocamp SA
+# © 2016 Denis Leemann
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
+from . import product
