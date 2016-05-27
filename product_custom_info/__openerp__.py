d01e2af6c4e34e7ec4aba91d5a4be69308577128
Jairo Llopis <yajo.sk8@gmail.com>
2016-05-27 09:48:58 +0200

    Fix docs and credits.

--- product_custom_info/__openerp__.py
+++ product_custom_info/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
@@ -17,7 +18,9 @@
         'views/product_view.xml',
     ],
     'author': 'Antiun Ingeniería S.L., '
-              'Incaser Informatica S.L., ',
+              'Incaser Informatica S.L., '
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     'website': 'http://www.antiun.com',
     'license': 'AGPL-3',
     'installable': True,
