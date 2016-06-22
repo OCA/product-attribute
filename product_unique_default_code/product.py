8c15c00f7b7224b3751c017c68b22836b448baf9
ralwafaie <rami.alwafaie@initos.de>
2016-06-22 11:01:08 +0200

    #164 fix Flake8

--- product_unique_default_code/product.py
+++ product_unique_default_code/product.py
@@ -3,7 +3,7 @@
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 from openerp import models, api, _
-from openerp.exceptions import Warning
+from openerp.exceptions import Warning as UserError
 
 
 class Product(models.Model):
@@ -14,4 +14,4 @@ class Product(models.Model):
     def _check_unique(self):
         for obj in self:
             if 1 < self.search_count([('default_code', '=', obj.default_code)]):
-                raise Warning(_("Internal Reference must be unique!"))
+                raise UserError(_("Internal Reference must be unique!"))
