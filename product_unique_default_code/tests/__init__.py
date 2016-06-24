f27ba0e7d5c508fde5d88ffa302198c0ae896371
ralwafaie <rami.alwafaie@initos.de>
2016-06-24 12:44:23 +0200

    #164 add tests

---
+++ product_unique_default_code/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 initOS GmbH.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from . import test_default_code_unique
---
+++ product_unique_default_code/tests/test_product_unique_default_code.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2016 initOS GmbH.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import ValidationError
+
+
+class TestProductUniqueDefaultCode(TransactionCase):
+
+    def setUp(self):
+        super(TestProductUniqueDefaultCode, self).setUp()
+
+        self.model = self.env['product.product']
+
+    def test_1(self):
+        self.product = self.model.create(
+            {
+                'name': 'product1',
+                'default_code': 'product1'
+            }
+        )
+        self.assertEqual(
+            'product1',
+            self.product.default_code
+        )
+
+    def test_2(self):
+        with self.assertRaises(ValidationError):
+            self.product = self.ProdModel.create(
+                {
+                    'name': 'product2',
+                    'default_code': 'product1'
+                }
+            )
