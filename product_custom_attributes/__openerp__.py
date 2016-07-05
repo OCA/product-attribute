e03a89193afee5b0507517642ca5a44ea4033155
florent.thomas <florent.thomas@mind-and-go.com>
2016-07-05 10:42:12 +0200

    [WIP][9.0]Migration for v9 product_custom_attributes

--- product_custom_attributes/__openerp__.py
+++ product_custom_attributes/__openerp__.py
@@ -1,29 +1,11 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#                                                                             #
-#   product_custom_attributes for OpenERP                                      #
-#   Copyright (C) 2011 Akretion Benoît GUILLOT <benoit.guillot@akretion.com>  #
-#                                                                             #
-#   This program is free software: you can redistribute it and/or modify      #
-#   it under the terms of the GNU Affero General Public License as            #
-#   published by the Free Software Foundation, either version 3 of the        #
-#   License, or (at your option) any later version.                           #
-#                                                                             #
-#   This program is distributed in the hope that it will be useful,           #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#   GNU Affero General Public License for more details.                       #
-#                                                                             #
-#   You should have received a copy of the GNU Affero General Public License  #
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-#                                                                             #
-###############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'product_custom_attributes',
-    'version': '0.2.1',
+    'version': '9.0.1.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'description': """This module adds the possibility to easily create custom fields on products.
@@ -36,12 +18,12 @@ In particular it's used by the Magento Magentoerpconnect module to match the EAV
     'depends': ['product', 'base_custom_attributes'],
     'init_xml': [],
     'update_xml': [
-           'product_view.xml',
-           'custom_attributes_view.xml',
+           'views/product_view.xml',
+           'views/custom_attributes_view.xml',
            'wizard/open_product_by_attribute_set.xml',
     ],
     'demo_xml': [],
-    'installable': False,
+    'installable': True,
     'active': False,
 }
 
