bd9c088df49d61e900562482406706fd0154cb81
florent.thomas <florent.thomas@mind-and-go.com>
2016-07-05 10:54:00 +0200

    [WIP][9.0]Migration and cherry pick from bad commit/PR

--- product_categ_attributes/__openerp__.py
+++ product_categ_attributes/__openerp__.py
@@ -1,29 +1,10 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#                                                                             #
-#   product_custom_attributes for OpenERP                                     #
-#   Copyright (C) 2013 Akretion Raphaël Valyi <raphael.valyi@akretion.com>    #
-#                                                                             #
-#   This program is free software: you can redistribute it and/or modify      #
-#   it under the terms of the GNU Affero General Public License as            #
-#   published by the Free Software Foundation, either version 3 of the        #
-#   License, or (at your option) any later version.                           #
-#                                                                             #
-#   This program is distributed in the hope that it will be useful,           #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#   GNU Affero General Public License for more details.                       #
-#                                                                             #
-#   You should have received a copy of the GNU Affero General Public License  #
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-#                                                                             #
-###############################################################################
-
-
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'product_categ_attributes',
-    'version': '0.1',
+    'version': '9.0.1.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'description': """Makes it possible to inherit product attributes from its categories
@@ -33,10 +14,10 @@
     'depends': ['product_custom_attributes', 'product_multi_category'],
     'init_xml': [],
     'update_xml': [
-        "product_view.xml"
+        "views/product_view.xml"
     ],
     'demo_xml': [],
-    'installable': False,
+    'installable': True,
     'active': False,
 }
 
--- product_categ_attributes/models/product.py
+++ product_categ_attributes/models/product.py
@@ -1,19 +1,28 @@
-from openerp.osv.orm import Model
-from openerp.osv import fields
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
 from openerp.osv.osv import except_osv
 from openerp.tools.translate import _
 
 
-class product_category(Model):
+from openerp import models, fields
+from openerp import api
+
+
+class ProductCategory(models.Model):
     _inherit = "product.category"
-    _columns = {
-        'attribute_group_ids': fields.many2many('attribute.group', 'categ_attr_grp_rel', 'categ_id', 'grp_id', 'Attribute Groups'),
-        }
+    
+    attribute_group_ids = fields.Many2many(comodel_name="attribute.group",
+                                            relation="categ_attr_grp_rel", column1="categ_id", column2="grp_id", string="Attribute Group")
+    
+    
 
-
-class product_product(Model):
+class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    @api.v7
     def _attr_grp_ids(self, cr, uid, ids, field_names, arg=None, context=None):
         res = {}
         for product in self.browse(cr, uid, ids, context=context):
