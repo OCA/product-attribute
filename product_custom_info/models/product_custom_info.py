f1c4e2e1f46ba3b302b315aca62f75b17e8240d7
Jairo Llopis <yajo.sk8@gmail.com>
2016-08-03 17:28:53 +0200

    Relicense to LGPL with permission from all orignal authors.

--- product_custom_info/models/product_custom_info.py
+++ product_custom_info/models/product_custom_info.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from openerp import fields, models
 
