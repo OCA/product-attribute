9dfca75f1d17f1ad6bf5875b05eba63045d37a69
eLBati <lorenzo.battistini@agilebg.com>
2016-09-07 11:21:39 +0200

    IMP headers and module description

--- product_pricelist_import/__openerp__.py
+++ product_pricelist_import/__openerp__.py
@@ -1,34 +1,10 @@
-
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
+# -*- coding: utf-8 -*-
 #    Daniel Campos (danielcampos@avanzosc.es) Date: 08/10/2014
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product Pricelist Import",
-    "version": "1.0",
-    "description": """
-    This module allows to load product price list from a file.
-    The file format must have these colummns:
-
-    keys = ['code', 'info', 'price', 'discount_1', 'discount_2', 'retail',
-    'pdv1', 'pdv2']
-    """,
+    "version": "8.0.1.0.0",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza",
