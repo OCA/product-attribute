5340879069ca992f45622d52f15de85af50790b4
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-10-24 18:59:30 +0530

    [MIG][10.0] migrate product_sequence

--- product_sequence/__openerp__.py
+++ product_sequence/__openerp__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2004 Tiny SPRL
-# © 2016 Sodexis
+# Copyright 2004 Tiny SPRL
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Sequence',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
     'website': 'http://www.zikzakmedia.com',
     'license': 'AGPL-3',
@@ -15,9 +15,6 @@
     ],
     'data': [
         'data/product_sequence.xml',
-    ],
-    'demo': [
-        'demo/product_product.xml'
     ],
     'pre_init_hook': 'pre_init_hook',
     'auto_install': False,
