bcfc7903eef19a229f1b47d4610c75c2117cfd88
Dave Lasley <dave@laslabs.com>
2016-11-04 13:48:38 -0700

    [MIG] product_multi_image: Upgrade to v10 * Migrate product_multi_image to v10 * Remove old api from `product.product` * Add test coverage for hooks and variant count * Fix oca depends * Add uninstall hook

--- product_multi_image/models/product_template.py
+++ product_multi_image/models/product_template.py
@@ -3,33 +3,22 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import orm, fields
+from odoo import fields, models
 
 
-class ProductTemplate(orm.Model):
-    """Reference core image fields to multi-image variants.
-
-    It is needed to use v7 api here because core model fields use the ``multi``
-    attribute, that has no equivalent in v8, and it needs to be disabled or
-    bad things will happen. For more reference, see
-    https://github.com/odoo/odoo/issues/10799
-    """
+class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = [_name, "base_multi_image.owner"]
-    _columns = {
-        "image": fields.related(
-            "image_main",
-            type="binary",
-            store=False,
-            multi=False),
-        "image_medium": fields.related(
-            "image_main_medium",
-            type="binary",
-            store=False,
-            multi=False),
-        "image_small": fields.related(
-            "image_main_small",
-            type="binary",
-            store=False,
-            multi=False)
-    }
+
+    image = fields.Binary(
+        related='image_main',
+        store=False,
+    )
+    image_medium = fields.Binary(
+        related='image_main_medium',
+        store=False,
+    )
+    image_small = fields.Binary(
+        related='image_main_small',
+        store=False,
+    )
