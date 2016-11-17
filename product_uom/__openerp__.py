2cdc7384dd1f309c1dbb3b45b3fd65107d5d74c2
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-17 16:35:14 -0600

    [ADD] product_uom

---
+++ product_uom/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Unit of Measures",
+    "summary": "Provide missing uom",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "data/product_uom_categ.xml",
+        "data/product_uom.xml",
+    ],
+}
