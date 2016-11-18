3056f7d380aa4f66857e2b59343dfc9d375bf9f1
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-18 11:00:18 -0600

    [FIX] URL

--- product_uom/__openerp__.py
+++ product_uom/__openerp__.py
@@ -6,7 +6,7 @@
     "summary": "Provide missing uom",
     "version": "9.0.1.0.0",
     "category": "Uncategorized",
-    "website": "https://ursainfosystems.com",
+    "website": "http://ursainfosystems.com",
     "author": "Ursa Information Systems, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
