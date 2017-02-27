693a38cb89160ccbe1f19c248328c378559af040
Andrea Piovesana <andrea.m.piovesana@gmail.com>
2017-02-27 15:04:33 +0100

    Update product.py

--- product_dimension/models/product.py
+++ product_dimension/models/product.py
@@ -4,8 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from openerp import models, fields
-from openerp import api
+from odoo import models, fields
+from odoo import api
 
 
 class Product(models.Model):
