82558ec68a1e17e72d50924b71a55526bcdc5a8c
Florian da Costa <florian.dacosta@akretion.com>
2017-03-08 16:12:26 +0100

    Migrate product_weight to v10

--- product_weight/models/__init__.py
+++ product_weight/models/__init__.py
@@ -3,4 +3,4 @@
 # Copyright (C) 2015 Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import product_weight_update
+from . import bom
