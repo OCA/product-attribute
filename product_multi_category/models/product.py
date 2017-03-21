7ea47b64bc74c55e84ced94ed878e1a771fe8148
Angel Moya Pardo <amoyapardo@gmail.com>
2017-03-21 08:56:24 +0100

    MIG 10.0 product_multi_category
    FIX product_multi_category README.rst

--- product_multi_category/models/product.py
+++ product_multi_category/models/product.py
@@ -2,7 +2,7 @@
 # © 2009 Akretion,Guewen Baconnier,Camptocamp,Avanzosc,Sharoon Thomas,Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
