e4d16bded4080797f0c0f4a439040ccfb174c416
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-05-05 18:07:55 +0200

    [10.0] add new module to manage seasonal products

---
+++ product_life_period/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product life period",
+    "version": "10.0.1.0.0",
+    'author': "Acsone SA/NV, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "http://www.acsone.eu",
+    "depends": [
+        "product_state",
+        "sale"
+    ],
+    "data": [
+        "views/product_life_period_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+}
