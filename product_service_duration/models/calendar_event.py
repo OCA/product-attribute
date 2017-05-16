89a153fce1dfb3e5b8520e5b90886fa0cd89dbf6
Brett Wood <bmwood@lton.io>
2017-05-16 16:42:57 -0700

    [10.0][ADD] product service duration (#251)
    * [ADD] Add module product_service_duration.
    
    * [IMP] product_service_duration: Refactor models and views.
    * Change any time fields to float_time widgets
    * Adjust field names
    * Add domain to service_user_id
    * Abstract calendar_event logic into smaller chunks
    
    * [IMP] product_service_duration: Add new logic checks.
    * Prevent validation errors if event allday
    * Prevent validation errors if event stop date is in the past
    * Add test coverage
    * Move product logic to product.product
    * Move view to product product views
    
    * [FIX] product_service_duration: Fix views, event model.
    * Stop min_duration from being calculated if event all day or event ended
    * Add event min_duration to calendar form popup
    * Add event min_duration to calendar search

---
+++ product_service_duration/models/calendar_event.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+import odoo.addons.decimal_precision as dp
+
+
+class CalendarEvent(models.Model):
+
+    _inherit = 'calendar.event'
+
+    product_ids = fields.Many2many(
+        string='Services',
+        comodel_name='product.product',
+        help='Services involved in '
+             'this event/meeting.',
+        domain="[('type', '=', 'service')]",
+    )
+    min_duration = fields.Float(
+        string='Minimum Duration',
+        digits=dp.get_precision('Product UoM'),
+        compute='_compute_min_duration',
+        store=True,
+        help='Minimum duration, in hours, the meeting can be.'
+    )
+
+    @api.multi
+    @api.depends(
+        'product_ids', 'product_ids.min_service_time', 'stop', 'allday')
+    def _compute_min_duration(self):
+        today_time = fields.Datetime.now()
+        for record in self:
+
+            if record.stop < today_time:
+                continue
+
+            if record.allday:
+                continue
+
+            min_duration = sum(
+                [t.min_service_time for t in record.product_ids]
+            )
+
+            if min_duration < 0:
+                min_duration = 0
+
+            record.min_duration = min_duration
+
+    @api.multi
+    @api.constrains(
+        'product_ids', 'duration', 'start', 'stop', 'min_duration')
+    def _check_product_ids(self):
+        today_time = fields.Datetime.now()
+        for record in self:
+
+            if record.stop < today_time:
+                continue
+
+            if record.allday:
+                continue
+
+            event_hours = record.duration
+
+            if not event_hours:
+                event_hours = record._get_duration(record.start, record.stop)
+
+            if record.min_duration > event_hours:
+                raise ValidationError(
+                    _('The services involved require a %s hour '
+                      'long meeting.') % record.min_duration,
+                )
