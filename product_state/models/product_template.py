e96a9502b83a50f1a7306428d6ca024d7804d148
Cédric Pigeon (ACSONE) <cedric.pigeon@acsone.eu>
2017-05-17 00:36:08 +0200

    re-introduce state field on product (#261)
    * [10.0] re-introduce state field on product
    
    * [ADD] add setup for product state

---
+++ product_state/models/product_template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    state = fields.Selection(selection=[('draft', 'In Development'),
+                                        ('sellable', 'Normal'),
+                                        ('end', 'End of Lifecycle'),
+                                        ('obsolete', 'Obsolete')],
+                             string='Status',
+                             default='sellable',
+                             index=True)
