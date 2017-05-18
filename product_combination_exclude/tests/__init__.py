4801eab09ff8a26869a1403f3221d67c449da997
Graeme Gellatly <gdgellatly@gmail.com>
2017-05-18 12:06:01 +1200

    [REV] Its not the tests

--- product_combination_exclude/tests/__init__.py
+++ product_combination_exclude/tests/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# from . import test_product_combination_exclude
+from . import test_product_combination_exclude
