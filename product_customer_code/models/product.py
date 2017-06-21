6d1b43320005e2dfe61d1dfa735c29ed9308bb7d
Aurélien Bondis <aurelien.bondis@gmail.com>
2017-06-20 20:50:42 -0400

    [#2045] fix: display matching product codes, do not display same code for all matching results, match super.name_get results with current ids

--- product_customer_code/models/product.py
+++ product_customer_code/models/product.py
@@ -19,27 +19,38 @@ class ProductProduct(models.Model):
     def name_get(self):
         product_customer_code_obj = self.env['product.customer.code']
         partner_id = self._context.get('partner_id')
-        if self._context.get('display_default_code', True) and partner_id:
+        display_default_code = self._context.get('display_default_code', True)
+        show_customer_code = self._context.get('show_customer_code', True)
+        if show_customer_code and display_default_code and partner_id:
             no_default_code = self.with_context(display_default_code=False)
+            # get product names without codes
             res = super(ProductProduct, no_default_code).name_get()
+            # res is [(product_id, name)...]
+            dict_res = dict(res)
             new_res = []
-            for (product_id, name) in res:
-                new_name = ''
-                product = self.browse(product_id)
+            for product in self:
+                name = dict_res[product.id]
                 product_code = product_customer_code_obj.search([
                     ('product_id', '=', product.id),
                     ('partner_id', '=', partner_id)
                 ], limit=1)
-                product_code = getattr(product_code, 'product_code', None) or product.code
+                product_code = (
+                    product_code.product_code if product_code
+                    else product.code
+                )
                 if product_code:
-                    new_name = '[{}] - {}'.format(product_code, name)
-                new_res.append((product_id, new_name or name))
+                    name = '[{}] - {}'.format(product_code, name)
+                new_res.append((product.id, name))
             return new_res
         return super(ProductProduct, self).name_get()
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
-        res = super(ProductProduct, self).name_search(
+        """
+        Shows products with matching customer code
+        """
+        no_customer_code = self.with_context(show_customer_code=False)
+        res = super(ProductProduct, no_customer_code).name_search(
             name=name, args=args, operator=operator, limit=limit)
         if (len(res) < limit):
             partner_id = self._context.get('partner_id')
@@ -49,7 +60,8 @@ class ProductProduct(models.Model):
                     ('product_code', operator, name),
                     ('partner_id', '=', partner_id)
                 ], limit=limit)
-                res.extend(product_codes.mapped('product_id').name_get())
-            # import ipdb
-            # ipdb.set_trace()
+                # get product names for all product customer code
+                # matching the query
+                res.extend(
+                    product_codes.mapped('product_id').name_get())
         return res
