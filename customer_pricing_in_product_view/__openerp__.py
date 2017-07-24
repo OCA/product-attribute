5647f2f156f06831583c7075a3894e0c3a32933b
Ronald Portier <ronald@therp.nl>
2017-07-24 03:01:40 +0200

    [8.0][FIX] Use self.env instead of pooler. (#270)
    * [FIX] Use self.env istead of pooler.
    
    * [FIX] Use self.env instead of pooler.
    
    * [FIX] patching ids to [ids] still needed.

--- customer_pricing_in_product_view/__openerp__.py
+++ customer_pricing_in_product_view/__openerp__.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution - module extension
-#    Copyright (C) 2014- O4SB (<http://o4sb.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014 O4SB <http://o4sb.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Display Customer Price in Product View',
     'version': '8.0.1.2.1',
@@ -26,13 +8,12 @@
     'author': "O4SB - Graeme Gellatly,Odoo Community Association (OCA)",
     'website': 'http://www.o4sb.com',
     'license': 'AGPL-3',
-    'depends': ['base', 'product'],
-    'description': '''
-    This module provides:
-        A button on the partner form to show Customer Pricing when
-        viewing a list of products.
-    ''',
-    'data': ['views/res_partner_view.xml'],
+    'depends': [
+        'base',
+        'product',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
     'installable': True,
-    'active': True
 }
--- customer_pricing_in_product_view/models/product_pricelist.py
+++ customer_pricing_in_product_view/models/product_pricelist.py
@@ -1,30 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution - module extension
-#    Copyright (C) 2014- O4SB (<http://o4sb.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import orm
+# © 2014 O4SB <http://o4sb.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import models
 
 
-class ProductPricelist(orm.Model):
+class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
+    # pylint: disable=old-api7-method-defined
     def name_get(self, cr, uid, ids, context=None):
         """
         When using widget=selection the orm seems to take care of converting
