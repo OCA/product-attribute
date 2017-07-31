191591b792ba4873cc55e7bd730af17ded771361
aheficent <ahenriquez@eficent.com>
2017-07-31 15:21:58 +0200

    product_supplierinfo_for_customer to v9

--- product_supplierinfo_for_customer/__openerp__.py
+++ product_supplierinfo_for_customer/__openerp__.py
@@ -1,28 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Use product supplier info for customers too",
-    "version": "8.0.1.0.0",
-    "author": "OdooMRP team,"
-              "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+    "version": "9.0.1.0.0",
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Tecnativa, "
+              "Eficent, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.odoomrp.com",
+    "website": "https://github.com/OCA/product-attribute",
     "category": "Sales Management",
     "license": 'AGPL-3',
     "depends": [
