a68f5066d8a805dfa4eec03309613a87299c0867
MonsieurB <eric@ebii.fr>
2017-09-08 12:51:45 +0200

    FIX static data

--- product_variant_inactive/__manifest__.py
+++ product_variant_inactive/__manifest__.py
@@ -11,7 +11,7 @@
     'version': '10.0.1.0.0',
     'depends': ['stock'],
     'data': [
-        'data/product_variant_view.xml'
+        'views/product_variant_view.xml'
     ],
     'demo': ['data/product.product.csv'],
 }
