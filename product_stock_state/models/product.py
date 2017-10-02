a8ec19e24de042eb133512cc95af27a008e29dcd
Sébastien BEAU <sebastien.beau@akretion.com>
2017-10-02 14:51:15 +0200

    [IMP] mass update with sed for odoo import and odoo UserError

--- product_stock_state/models/product.py
+++ product_stock_state/models/product.py
@@ -3,7 +3,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
