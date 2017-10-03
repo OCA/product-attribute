b7f196b02dbd6c3465f7e1e13af0d821f13987ed
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-10-03 04:30:35 +0200

    [PORT+IMP][10.0] product_stock_state

--- product_stock_state/__manifest__.py
+++ product_stock_state/__manifest__.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
+# Copyright 2017-Today GRAP (http://www.grap.coop).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
+# @author Sylvain LE GAL <https://twitter.com/legalsylvain>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 
 {
     "name": "Product Stock State",
     "summary":
         "Compute the state stock based on"
         "the stock level and sale_ok field",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Uncategorized",
     "website": "www.akretion.com",
-    "author": " Akretion",
+    "author": " Akretion,GRAP",
     "license": "AGPL-3",
     "application": False,
-    'installable': False,
+    'installable': True,
     "external_dependencies": {
         "python": [],
         "bin": [],
@@ -23,9 +26,17 @@
         "sale_stock",
     ],
     "data": [
-        'views/product_view.xml',
+        'security/res_groups.xml',
+        'views/product_template_view.xml',
+        'views/product_product_view.xml',
+        'views/product_category_view.xml',
+        'views/sale_config_settings_view.xml',
     ],
     "demo": [
+        'demo/res_groups.xml',
+        'demo/product_category.xml',
+        'demo/product_product.xml',
+        'demo/product_category.xml',
     ],
     "qweb": [
     ]
--- product_stock_state/models/__init__.py
+++ product_stock_state/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import product
+from . import res_company
+from . import sale_config_settings
+from . import product_category
+from . import product_template
+from . import product_product
+
