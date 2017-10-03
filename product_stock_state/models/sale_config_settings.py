985208b7c7474b3069c6149623ae8ee2694f25af
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-10-03 04:30:35 +0200

    [PORT+IMP][10.0] product_stock_state

---
+++ product_stock_state/models/sale_config_settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-Today GRAP (http://www.grap.coop).
+# @author Sylvain LE GAL <https://twitter.com/legalsylvain>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleConfigSettings(models.TransientModel):
+    _inherit = 'sale.config.settings'
+
+    stock_state_threshold = fields.Float(
+        related='company_id.stock_state_threshold',
+        string="Stock State Threshold *", help="Define custom value"
+        " under wich the stock state will pass from 'In Stock' to 'In Limited"
+        " Stock' State.")
