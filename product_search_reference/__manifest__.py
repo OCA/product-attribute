114d762e8bcf956d3a4e26f994d66ce14272203c
Loïc Faure-Lacroix <lamerstar@gmail.com>
2017-10-16 21:49:56 +0300

    [#2350] Port module product_searc_reference to v10

--- product_search_reference/__manifest__.py
+++ product_search_reference/__manifest__.py
@@ -29,7 +29,5 @@
     'description': '''
     Add search by partner reference on product
     ''',
-    'installable': False,
+    'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
--- product_search_reference/product.py
+++ product_search_reference/product.py
@@ -1,5 +1,5 @@
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
@@ -14,49 +14,68 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import osv, fields
+from odoo import fields, models, api
 
 
-class Product(osv.osv):
+class Product(models.Model):
     _inherit = 'product.product'
 
-    def _partner_ref2(self, cursor, user, ids, name, arg, context=None):
-        res = {}
-        for product in self.browse(cursor, user, ids, context=context):
-            res[product.id] = '\n'.join([x.product_code \
-                    for x in product.seller_ids if x.product_code]) or ''
-        return res
+    def _search_partner_ref2(self, operator, value):
+        suppliferinfo_obj = self.env['product.supplierinfo']
 
-    def _partner_ref2_search(self, cursor, user, obj, name, args, context={}):
-        supplierinfo_obj = self.pool.get('product.supplierinfo')
-        args = args[:]
-        i = 0
-        while i < len(args):
-            args[i] = ('product_code', args[i][1], args[i][2])
-            i += 1
-        supplierinfo_ids = supplierinfo_obj.search(cursor, user, args)
-        product_ids = [ x.product_id.id for x in supplierinfo_obj.browse(cursor, user,
-                supplierinfo_ids) if x.product_id]
+        domain = [('product_code', operator, value)]
+        supplierinfo_ids = suppliferinfo_obj.search(domain)
+
+        product_ids = []
+
+        for vendor in supplierinfo_ids:
+            if vendor.product_id:
+                product_ids.append(vendor.product_id.id)
+            elif vendor.product_tmpl_id:
+                product_ids += vendor.product_tmpl_id.product_variant_ids.ids
+
         return [('id', 'in', product_ids)]
 
-    _columns = {
-        'partner_ref2': fields.function(_partner_ref2, method=True,
-            type='char', string='Customer ref', fnct_search=_partner_ref2_search,
-            select=2),
-    }
+    partner_ref2 = fields.Char(
+        compute="_compute_partner_ref2",
+        search="_search_partner_ref2",
+        string="Customer ref",
+    )
 
-    def name_search(self, cursor, user, name='', args=None, operator='ilike',
-            context=None, limit=80):
-        ids = self.search(cursor, user, [('partner_ref2', '=', name)] + args,
-                limit=limit, context=context)
-        if ids:
-            return self.name_get(cursor, user, ids, context=context)
-        return super(Product, self).name_search(cursor, user, name=name, args=args,
-                operator=operator, context=context, limit=limit)
+    @api.multi
+    @api.depends(
+        'seller_ids',
+        'seller_ids.product_code',
+    )
+    def _compute_partner_ref2(self):
+        for product in self:
+            refs = [
+                supplier.product_code
+                for supplier in product.seller_ids
+                if supplier.product_code
+            ]
+            product.partner_ref2 = "\n".join(refs)
 
-Product()
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    @api.model
+    def name_search(
+        self,
+        name='',
+        args=None,
+        operator='ilike',
+        limit=100
+    ):
+        domain = [('partner_ref2', operator, name)] + args
+        result = self.search(domain, limit=limit)
 
+        if result:
+            return result.name_get()
+
+        return super(Product, self).name_search(
+            name=name,
+            args=args,
+            operator=operator,
+            limit=limit
+        )
