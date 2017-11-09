7b73441d8fa6a1a03fbaa11823b66f53a523586d
Aleksandr <kolushov@it-projects.info>
2017-11-09 13:29:05 +0500

    Update __manifest__.py

--- product_template_tags/__manifest__.py
+++ product_template_tags/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Product Template Tags',
     'summary': """
         This addon allow to add tags on products""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': 'https://www.acsone.eu',
