91dae64e15899c7a19c29647efbc33985c63fc4b
Fanha Giang <fanha99@hotmail.com>
2018-01-07 19:00:50 +0700

    [10][MIG] product_custom_info

--- product_custom_info/__manifest__.py
+++ product_custom_info/__manifest__.py
@@ -8,7 +8,7 @@
     'name': "Product Custom Info",
     'summary': "Add custom field in products",
     'category': 'Customize',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'product',
         'base_custom_info',
