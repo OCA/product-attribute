57d77ca197b752e469652bfac9f342eaf4e092bf
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-01-31 19:26:10 +0530

    [ADD] firmware version on Stock product lot

---
+++ stock_production_lot_firmware_version/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock product lot firmware version",
+    "summary": "Allows to store the current firmware version on an serialized electronic item.",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Stock",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_view.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_production_lot_firmware_version/models/stock.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
+
+    firmware_version = fields.Char(
+        string='Firmware Version'
+    )
