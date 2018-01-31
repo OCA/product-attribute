add46640fe426678e3aeba2a9fb680d36cce3bbf
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-01-31 19:19:49 +0530

    [ADD] firmware version on Product

---
+++ product_firmware_version/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
---
+++ product_firmware_version/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product firmware version",
+    "summary": "Allow to store the revision of a firmware on a part (latest) and his related serial numbers",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["product"],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
