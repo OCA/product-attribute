29219a430051d644806c4e55bf1031f16cc66da1
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-01-31 19:21:19 +0530

    [CHG] copyright header

--- product_firmware_version/__init__.py
+++ product_firmware_version/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012 - TODAY, Open Source Integrators
+# Copyright (C) 2017 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
