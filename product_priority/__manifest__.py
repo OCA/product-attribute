fb5924a07e8a3fbfe258d20f246bf69b198f50a3
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-02 12:43:13 +0530

    [ADD] product_priority

---
+++ product_priority/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Priority",
+    "summary": "Allow user to specify a priority on a product",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["product", "stock"],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
