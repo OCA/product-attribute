0bdf7e3272e1427bf1b98df83f1cb088a01c521a
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-02 12:43:13 +0530

    [ADD] product_priority

---
+++ product_priority/models/product.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    priority = fields.Integer(
+        string='Priority',
+        default=100
+    )
