9bbf8c0bf01853b10e8c1ede8f2926cc78f4cfdc
Antonio Yamuta <ayamuta@ursainfosystems.com>
2018-02-08 08:58:56 -0800

    [ADD] Product End-of-Life

---
+++ product_end_of_life/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product End-Of-Life Date",
+    "summary": "Alert users when products approach EoL Date",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product", "stock", "purchase"],
+    "data": [
+        "views/product_view.xml",
+        "views/purchase_view.xml",
+        "views/res_config_settings_view.xml",
+        "data/ir_cron.xml",
+        "data/mail_channel_data.xml",
+        "views/mail_templates.xml",
+    ],
+    "installable": True,
+}
---
+++ product_end_of_life/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import product
+from . import res_config_settings
