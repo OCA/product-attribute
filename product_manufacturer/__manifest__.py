ce13fbc087d8aec9a44161f0b54d30e3a8c5e790
Lois Rilo <lois.rilo@eficent.com>
2018-02-09 15:20:08 +0100

    [11.0][MIG] product_manufacturer

--- product_manufacturer/__manifest__.py
+++ product_manufacturer/__manifest__.py
@@ -1,34 +1,15 @@
-# coding: utf-8
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Product Manufacturers',
-    'version': '10.0.1.0.0',
-    'author': "OpenERP SA, Odoo Community Association (OCA)",
+    'name': 'Product Manufacturer',
+    'version': '11.0.1.0.0',
+    'summary': 'Adds manufacturers and attributes on the product view.',
+    'author': 'OpenERP SA, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'contributors': ['Acysos SL <info@acysos.com>'],
-    'category': 'Purchase Management',
+    'category': 'Product',
     'depends': ['product'],
-    'demo': [],
     'data': [
-        'views/product_manufacturer_view.xml'
+        'views/product_manufacturer_view.xml',
     ],
     'auto_install': False,
     'installable': True,
--- product_manufacturer/models/product_manufacturer.py
+++ product_manufacturer/models/product_manufacturer.py
@@ -1,31 +1,15 @@
-# coding: utf-8
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    manufacturer = fields.Many2one('res.partner', 'Manufacturer')
-    manufacturer_pname = fields.Char('Manuf. Product Name')
-    manufacturer_pref = fields.Char('Manuf. Product Code')
-    manufacturer_purl = fields.Char('Manuf. Product URL')
+    manufacturer = fields.Many2one(
+        comodel_name='res.partner', string='Manufacturer',
+    )
+    manufacturer_pname = fields.Char(string='Manuf. Product Name')
+    manufacturer_pref = fields.Char(string='Manuf. Product Code')
+    manufacturer_purl = fields.Char(string='Manuf. Product URL')
