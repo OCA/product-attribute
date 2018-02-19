967e6bbb91d0036df0e44437c8f3e6113a2e644e
Alexis de Lattre <alexis.delattre@akretion.com>
2018-02-19 11:28:52 +0100

    Update README.rst to latest standard

--- product_categ_image/__openerp__.py
+++ product_categ_image/__openerp__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#
 # © 2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
