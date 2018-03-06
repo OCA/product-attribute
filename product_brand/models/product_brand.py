62b2afe55e82c90a37c0fd0aa710a2b7360b84e5
Daniel-CA <danielcampos@avanzosc.es>
2018-03-06 17:34:39 +0100

    [MIG] : Migration to 11.0 + Test

--- product_brand/models/product_brand.py
+++ product_brand/models/product_brand.py
@@ -2,8 +2,8 @@
 # © 2009 NetAndCo (<http://www.netandco.net>).
 # © 2011 Akretion Benoît Guillot <benoit.guillot@akretion.com>
 # © 2014 prisnet.ch Seraphine Lantible <s.lantible@gmail.com>
-# © 2016-Today Serpent Consulting Services Pvt. Ltd.
-#    (<http://www.serpentcs.com>)
+# © 2016 Serpent Consulting Services Pvt. Ltd.
+# © 2018 Daniel Campos <danielcampos@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import api, fields, models
@@ -31,10 +31,11 @@ class ProductBrand(models.Model):
         compute='_get_products_count',
     )
 
-    @api.one
+    @api.multi
     @api.depends('product_ids')
     def _get_products_count(self):
-        self.products_count = len(self.product_ids)
+        for brand in self:
+            brand.products_count = len(brand.product_ids)
 
 
 class ProductTemplate(models.Model):
