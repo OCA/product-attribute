cb160df6308871de746a7a4e247ef0edd3447f73
Kumar Aberer <kumar.aberer@braintec-group.com>
2018-03-12 12:37:41 +0100

    [MIG] product_dimension: Migration to 11.0

--- product_dimension/__manifest__.py
+++ product_dimension/__manifest__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
+# © 2018 brain-tec AG (http://www.braintec-group.com)
 # © 2015-2016 Camptocamp SA
 # © 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product Dimension',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Product',
-    'author': 'ADHOC SA, Camptocamp, Odoo Community Association (OCA)',
+    'author': 'brain-tec AG, ADHOC SA, Camptocamp, '
+              'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'depends': ['product'],
+    'depends': ['product', 'stock'],
     'data': ['views/product_view.xml'],
-    'installable': False,
+    'installable': True,
     'images': [],
 }
