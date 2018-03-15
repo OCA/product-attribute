0be1f054e51632645b94fd5d09e077a2b73a2ef7
Kumar Aberer <kumar.aberer@braintec-group.com>
2018-03-15 11:14:57 +0100

    [MIG]: Adding suggested changes

--- product_dimension/models/__init__.py
+++ product_dimension/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2016 Denis Leemann
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import product
