a9d4eaa36b481a094981e58150939f93bb9b98a2
Kumar Aberer <kumar.aberer@braintec-group.com>
2018-03-15 11:14:57 +0100

    [MIG]: Adding suggested changes

--- product_dimension/tests/__init__.py
+++ product_dimension/tests/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-# © 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import test_compute_volume
