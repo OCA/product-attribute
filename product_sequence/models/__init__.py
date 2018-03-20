11843cc7b67d1ff3e3c6d229fde111b9cb3930f7
Lois Rilo <lois.rilo@eficent.com>
2018-03-20 12:10:18 +0100

    [11.0][MIG/IMP] product_sequence: * Migration to v11 * Support sequence by Product Category

--- product_sequence/models/__init__.py
+++ product_sequence/models/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2004 Tiny SPRL
-# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import product_product
+from . import product_category
