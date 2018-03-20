63fb6b65219e16b6d0d22e7175d7a33fb766113f
Lois Rilo <lois.rilo@eficent.com>
2018-03-20 12:10:18 +0100

    [11.0][MIG/IMP] product_sequence: * Migration to v11 * Support sequence by Product Category

--- product_sequence/__openerp__.py
+++ product_sequence/__openerp__.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright 2004 Tiny SPRL
 # Copyright 2016 Sodexis
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Sequence',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
-    'website': 'http://www.zikzakmedia.com',
+    'website': 'https://github.com/OCA/product-attribute',
     'license': 'AGPL-3',
-    'category': 'Generic Modules/Inventory Control',
+    'category': 'Product',
     'depends': [
         'product',
     ],
     'data': [
         'data/product_sequence.xml',
+        'views/product_category.xml',
     ],
     'pre_init_hook': 'pre_init_hook',
     'auto_install': False,
