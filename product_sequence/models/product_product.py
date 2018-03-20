a816dd506a631456b67c9f6184b33ffcd28a7b64
Lois Rilo <lois.rilo@eficent.com>
2018-03-20 12:10:18 +0100

    [11.0][MIG/IMP] product_sequence: * Migration to v11 * Support sequence by Product Category

--- product_sequence/models/product_product.py
+++ product_sequence/models/product_product.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2004 Tiny SPRL
 # Copyright 2016 Sodexis
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, _
@@ -23,18 +24,43 @@ class ProductProduct(models.Model):
     @api.model
     def create(self, vals):
         if 'default_code' not in vals or vals['default_code'] == '/':
-            sequence = self.env.ref('product_sequence.seq_product_auto')
+            categ_id = vals.get("categ_id")
+            template_id = vals.get("product_tmpl_id")
+            categ = sequence = False
+            if categ_id:
+                # Created as a product.product
+                categ = self.env['product.category'].browse(categ_id)
+            elif template_id:
+                # Created from a product.template
+                template = self.env["product.template"].browse(template_id)
+                categ = template.categ_id
+            if categ:
+                sequence = categ.sequence_id
+            if not sequence:
+                sequence = self.env.ref('product_sequence.seq_product_auto')
             vals['default_code'] = sequence.next_by_id()
         return super(ProductProduct, self).create(vals)
 
     @api.multi
     def write(self, vals):
-        for product in self:
-            if product.default_code in [False, '/']:
-                sequence = self.env.ref('product_sequence.seq_product_auto')
-                vals['default_code'] = sequence.next_by_id()
-            super(ProductProduct, product).write(vals)
-        return True
+        """To assign a new internal reference, just write '/' on the field.
+        Note this is up to the user, if the product category is changed,
+        she/he will need to write '/' on the internal reference to force the
+        re-assignment."""
+        if vals.get('default_code', '') in [False, '/']:
+            for product in self:
+                sequence = product.categ_id.sequence_id
+                if not sequence:
+                    sequence = self.env.ref(
+                        'product_sequence.seq_product_auto')
+                ref = sequence.next_by_id()
+                vals['default_code'] = ref
+                super(ProductProduct, product).write(vals)
+                if len(product.product_tmpl_id.product_variant_ids) == 1:
+                    # default code must match for variant and template.
+                    product.product_tmpl_id.default_code = ref
+            return True
+        return super().write(vals)
 
     @api.multi
     def copy(self, default=None):
