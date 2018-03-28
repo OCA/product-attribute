b7e3954310ce70ec43a2cfe56b75156bcc4abe4b
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-28 15:14:27 +0200

    [MIG] product_categ_image from v8 to v10

--- product_categ_image/models/product_category.py
+++ product_categ_image/models/product_category.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-#
-# © 2016 Akretion (http://www.akretion.com)
+# Copyright 2016-2018 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class ProductCategory(models.Model):
