8b7c503ca2ee9a12287f7870bd963230f607aff4
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-28 15:14:27 +0200

    [MIG] product_categ_image from v8 to v10

--- product_categ_image/__manifest__.py
+++ product_categ_image/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
+# Copyright 2016-2018 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 {
     'name': 'Product Category Image',
-    'version': '8.0.0.1.0',
-    'category': 'Sales Management',
+    'version': '10.0.1.0.0',
+    'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Add image on product category',
     'author': 'Akretion,Odoo Community Association (OCA)',
