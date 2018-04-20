b6148a52e6b2359e74b45b2903fb46a7e7f8fa58
Kumar Aberer <kumar.aberer@braintec-group.com>
2018-04-20 13:57:58 -0500

    [MIG] product_dimension: Adding missing po files and improvemnets

--- product_dimension/models/product.py
+++ product_dimension/models/product.py
@@ -3,8 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models, fields
-from odoo import api
+from odoo import models, fields, api
 
 
 class Product(models.Model):
@@ -12,14 +11,16 @@ class Product(models.Model):
 
     @api.onchange('length', 'height', 'width', 'dimensional_uom_id')
     def onchange_calculate_volume(self):
-        if (not self.length or not self.height or not self.width or
-                not self.dimensional_uom_id):
-            return False
-
-        length_m = self.convert_to_meters(self.length, self.dimensional_uom_id)
-        height_m = self.convert_to_meters(self.height, self.dimensional_uom_id)
-        width_m = self.convert_to_meters(self.width, self.dimensional_uom_id)
-        self.volume = length_m * height_m * width_m
+        if self.length and self.height and self.width and \
+                self.dimensional_uom_id:
+            length_m = self.convert_to_meters(
+                self.length, self.dimensional_uom_id)
+            height_m = self.convert_to_meters(
+                self.height, self.dimensional_uom_id)
+            width_m = self.convert_to_meters(
+                self.width, self.dimensional_uom_id)
+            self.volume = length_m * height_m * width_m
+        return False
 
     def convert_to_meters(self, measure, dimensional_uom):
         uom_meters = self.env.ref('product.product_uom_meter')
@@ -51,14 +52,16 @@ class ProductTemplate(models.Model):
 
     @api.onchange('length', 'height', 'width', 'dimensional_uom_id')
     def onchange_calculate_volume(self):
-        if (not self.length or not self.height or not self.width or
-                not self.dimensional_uom_id):
-            return False
-
-        length_m = self.convert_to_meters(self.length, self.dimensional_uom_id)
-        height_m = self.convert_to_meters(self.height, self.dimensional_uom_id)
-        width_m = self.convert_to_meters(self.width, self.dimensional_uom_id)
-        self.volume = length_m * height_m * width_m
+        if self.length and self.height and self.width and \
+                self.dimensional_uom_id:
+            length_m = self.convert_to_meters(
+                self.length, self.dimensional_uom_id)
+            height_m = self.convert_to_meters(
+                self.height, self.dimensional_uom_id)
+            width_m = self.convert_to_meters(
+                self.width, self.dimensional_uom_id)
+            self.volume = length_m * height_m * width_m
+        return False
 
     def convert_to_meters(self, measure, dimensional_uom):
         uom_meters = self.env.ref('product.product_uom_meter')
