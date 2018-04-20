2323db615d79bf129d0b1bda86aee51549c5e5e9
mara1 <raul.martin@braintec-group.com>
2018-04-20 13:09:37 +0200

    [MIG][11.0] Migration of l10n_eu_product_adr_report to v11

---
+++ l10n_eu_product_adr_report/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "ADR Product report",
+    "summary": "",
+    "version": "11.0.1.0.0",
+    "depends": [
+        "product", "sale", "sales_team"
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "brain-tec AG",
+    "category": "Product Management",
+    "website": "http://www.odoomrp.com",
+    'license': 'AGPL-3',
+    "data": [
+        "views/product_view.xml",
+        "report/product_adr_report.xml",
+        "security/ir.model.access.csv",
+    ],
+    'installable': True,
+    'auto_install': False,
+}
