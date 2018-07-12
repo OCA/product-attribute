60c447a10e04066d9e8828870b222b7d8d2e03e6
David <david.vidal@tecnativa.com>
2018-07-12 12:28:24 +0200

    [MIG] Merge product_brand_sale_report into product_brand

--- product_brand/reports/sale_report.py
+++ product_brand/reports/sale_report.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class SaleReport(models.Model):
