b48ebbbbc0e906a7d44ebaf870d386f490d12856
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2018-07-13 09:27:20 +0200

    [11.0][MIG] product_supplier_info_for_customer: Module migration

--- product_customerinfo/__manifest__.py
+++ product_customerinfo/__manifest__.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 OdooMRP team
 # Copyright 2015 AvanzOSC
-# Copyright 2015 Tecnativa
+# Copyright 2015-18 Tecnativa
+# Copyright 2017-18 Eficent
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo for Customers",
     "summary": "Allows to define prices for customers in the products",
-    "version": "10.0.3.0.1",
+    "version": "11.0.1.0.0",
     "author": "AvanzOSC, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
@@ -14,8 +14,6 @@
     "category": "Sales Management",
     "license": 'AGPL-3',
     "depends": [
-        "base",
-        "product",
         "purchase",
     ],
     "data": [
---
+++ product_customerinfo/readme/USAGE.rst
@@ -0,0 +1,6 @@
+There's a new section on *Sales* tab of the product form called "Customers",
+where you can define records for customers with the same structure of the
+suppliers.
+
+There's a new option on pricelist items that allows to get the prices from the
+supplierinfo at the product form.
