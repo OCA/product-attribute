5dc6c1f2191213ad7a66c35f7aa81b5577cdf18a
mreficent <miquel.raich@eficent.com>
2018-07-16 16:49:57 +0200

    [REF] Don't overwrite views and fix view for customers

--- product_supplierinfo_for_customer/models/pricelist.py
+++ product_supplierinfo_for_customer/models/pricelist.py
@@ -9,16 +9,5 @@ from odoo import fields, models
 class ProductPricelistItem(models.Model):
     _inherit = "product.pricelist.item"
 
-    base = fields.Selection([
-        ('list_price', 'Public Price'),
-        ('standard_price', 'Cost'),
-        ('pricelist', 'Other Pricelist'),
-        ('partner', 'Partner Prices on the product form')],
-        default='list_price', required=True,
-        help='Base price for computation.\n'
-             'Public Price: The base price will be the Sale/public Price.\n'
-             'Cost Price : The base price will be the cost price.\n'
-             'Other Pricelist : Computation of the base price based on another'
-             ' Pricelist.'
-             'Partner Prices: Take the price from the customer info on the'
-             ' product form')
+    base = fields.Selection(
+        selection_add=[('partner', 'Partner Prices on the product form')])
