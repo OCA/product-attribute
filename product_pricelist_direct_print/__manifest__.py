b0ed5be9bf921ee18a1e3d424bb93eaf503d0e64
David <david.vidal@tecnativa.com>
2018-07-18 09:44:00 +0200

    [MIG] product_pricelist_direct_print: Migration to 11.0

--- product_pricelist_direct_print/__manifest__.py
+++ product_pricelist_direct_print/__manifest__.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "Product Pricelist Direct Print",
     "summary": "Print price list from menu option, product templates, "
                "products variants or price lists",
-    "version": "9.0.1.1.0",
+    "version": "11.0.1.0.0",
     "category": "Product",
-    "website": "http://www.tecnativa.com",
+    "website": "https://www.github.com/OCA/product-attribute",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
-        "product",
+        "sale",
     ],
     "data": [
+        "data/pricelist_send_action.xml",
         "views/report_product_pricelist.xml",
         "wizards/product_pricelist_print_view.xml",
+        "data/mail_template_data.xml",
     ],
 }
