4c29156760a0dd6e586248bcbc0f47b46223bc5f
David <david.vidal@tecnativa.com>
2018-07-18 09:44:00 +0200

    [MIG] product_pricelist_direct_print: Migration to 11.0

--- product_pricelist_direct_print/wizards/product_pricelist_print.py
+++ product_pricelist_direct_print/wizards/product_pricelist_print.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017 Tecnativa - Carlos Dauden
+# Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import ValidationError
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductPricelistPrint(models.TransientModel):
@@ -64,5 +64,52 @@ class ProductPricelistPrint(models.TransientModel):
                self.show_sale_price):
             raise ValidationError(_(
                 'You must set price list or any show price option.'))
-        return self.env['report'].get_action(
-            self, 'product_pricelist_direct_print.report_product_pricelist')
+        return self.env.ref(
+            'product_pricelist_direct_print.'
+            'action_report_product_pricelist').report_action(self)
+
+    @api.multi
+    def action_pricelist_send(self):
+        self.ensure_one()
+        template_id = self.env.ref(
+            'product_pricelist_direct_print.email_template_edi_pricelist').id
+        compose_form_id = self.env.ref(
+            'mail.email_compose_message_wizard_form').id
+        ctx = {
+            'default_composition_mode': 'comment',
+            'default_res_id': self.id,
+            'default_model': 'product.pricelist.print',
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'partner_to': self.partner_id,
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+    @api.multi
+    def force_pricelist_send(self):
+        if self.env.context.get('active_model') != 'res.partner':
+            return False
+        template_id = self.env.ref(
+            'product_pricelist_direct_print.email_template_edi_pricelist').id
+        composer = self.env['mail.compose.message'].with_context({
+            'default_composition_mode': 'mass_mail',
+            'default_notify': True,
+            'default_res_id': self.id,
+            'default_model': 'product.pricelist.print',
+            'default_template_id': template_id,
+            'active_ids': self.ids,
+            'partner_to': self.partner_id,
+        }).create({})
+        values = composer.onchange_template_id(
+            template_id, 'mass_mail', 'product.pricelist.print',
+            self.id)['value']
+        composer.write(values)
+        composer.send_mail()
