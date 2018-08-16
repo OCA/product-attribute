1dc007e670a2832d3936d34e8b23ac3036111dd7
KolushovAlexandr <kolushov@it-projects.info>
2018-08-16 20:08:54 +0500

    [ADD] tests

---
+++ product_template_tags/tests/test_product_template_tags.py
@@ -0,0 +1,17 @@
+from odoo.tests.common import TransactionCase
+from odoo.api import Environment
+
+
+class TestProductTemplateTag(TransactionCase):
+    pre_install = True
+    post_install = True
+
+    def test_products_count(self):
+        tag = self.env['product.template.tag'].create({
+            'name': 'test_tag',
+        })
+        product = self.env['product.template'].search([], limit=1)
+        product.write({
+            'tag_ids': [(6, 0, [tag.id])],
+        })
+        self.assertEqual(product.tag_ids[0].products_count, 1, 'Error product count does not match')
