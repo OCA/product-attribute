1d6f78f121d6ec1ceb5af7bb56b2ec8478cc95d2
sergio.teruel <sergio.teruel@tecnativa.com>
2018-09-22 00:17:23 +0200

    11.0[WIP]product_pricelist_revision

---
+++ product_pricelist_revision/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Carlos Dauden - Tecnativa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Pricelist Revision",
+    "summary": "Product Pricelist Revision",
+    "version": "11.0.1.0.0",
+    "category": "Product",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa S.L.",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale_management",
+    ],
+    "data": [
+        "views/pricelist_view.xml",
+        "wizards/pricelist_duplicate_wizard_view.xml",
+    ],
+}
