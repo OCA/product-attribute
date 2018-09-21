9e39b6d2d02ee6b9e207311409b73850694de51f
Aleksandr <kolushov@it-projects.info>
2018-09-21 12:28:21 +0500

    [ADD] license to the tests

--- product_template_tags/tests/test_product_template_tags.py
+++ product_template_tags/tests/test_product_template_tags.py
@@ -1,3 +1,7 @@
+# Copyright 2018 Kolushov Alexandr
+# Copyright 2018 <https://it-projects.info/team/KolushovAlexandr>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo.tests.common import TransactionCase
 
 
