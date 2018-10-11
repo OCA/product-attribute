1143635c7001fe2eef8830b5517a2c539ec0a88b
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-10-11 20:13:06 +0200

    [FIX] set standard_price_tax_included a company_dependent field

--- product_standard_price_tax_included/models/product_template.py
+++ product_standard_price_tax_included/models/product_template.py
@@ -13,16 +13,17 @@ class ProductTemplate(models.Model):
     # Column Section
     standard_price_tax_included = fields.Float(
         compute='_compute_standard_price_tax_included',
-        string='Cost Price Tax Included',
-        store=True, digits=dp.get_precision('Product Price'),
+        string='Cost Price Tax Included', company_dependent=True,
+        digits=dp.get_precision('Product Price'),
         help="Cost Price of the product, All Tax Included:\n"
         "This field will be computed with the 'Cost Price', taking into"
         " account Sale Taxes setting.")
 
-    @api.one
+    @api.multi
     @api.depends(
         'standard_price', 'taxes_id', 'taxes_id.type', 'taxes_id.amount')
     def _compute_standard_price_tax_included(self):
-        taxes = self.env['account.tax'].browse(self.taxes_id.ids)
-        info = taxes.compute_all(self.standard_price, 1, force_excluded=True)
-        self.standard_price_tax_included = info['total_included']
+        for template in self:
+            info = template.taxes_id.compute_all(
+                    template.standard_price, 1, force_excluded=True)
+            template.standard_price_tax_included = info['total_included']
