462b37678d1e8a20505798433ae5968fe4be3cb7
chafique delli <chafique.delli@akretion.com>
2018-10-11 20:01:05 +0200

    [10.0][ADD] product_code_unique module (#369)

---
+++ product_code_unique/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from .hooks import pre_init_product_code
---
+++ product_code_unique/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Unique Product Internal Reference',
+    'summary': 'Set Product Internal Reference as Unique',
+    'version': '10.0.1.0.0',
+    'category': 'Product',
+    'website': 'https://github.com/OCA/product-attribute',
+    'author': 'Open Source Integrators, Akretion, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'pre_init_hook': 'pre_init_product_code',
+    'depends': ['product'],
+}
