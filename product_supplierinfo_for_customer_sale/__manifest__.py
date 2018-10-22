830216e42a1be87b773b04089ae92e4dc72f4f39
Moisés López <moylop260@vauxoo.com>
2018-10-22 17:54:42 -0500

    [MIG] product_supplierinfo_for_customer_sale: Migration to 11.0
     - Remove coding comment
     - bump_version
     - 2to3 script
     - view fixing
     - fix the test

--- product_supplierinfo_for_customer_sale/__manifest__.py
+++ product_supplierinfo_for_customer_sale/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Vauxoo (https://www.vauxoo.com) info@vauxoo.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Product supplier info for customer - sale",
-    "version": "8.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Vauxoo, Odoo Community Association (OCA)",
     "website": "http://www.vauxoo.com/",
     "category": "Sales Management",
