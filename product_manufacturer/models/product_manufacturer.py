79024756b1805b3dd19911060908c8c4122c8077
Daniels Andersons <daniels.andersons@avoin.systems>
2018-10-29 17:47:44 +0200

    [MIG] product_manufacturer: Migration to 12.0

--- product_manufacturer/models/product_manufacturer.py
+++ product_manufacturer/models/product_manufacturer.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
 
