99d31c817ff657d58bc6b8f83264ec68dfd7e1c2
Daniels Andersons <daniels.andersons@avoin.systems>
2018-10-29 17:47:44 +0200

    [MIG] product_manufacturer: Migration to 12.0

--- product_manufacturer/__manifest__.py
+++ product_manufacturer/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product Manufacturer',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'summary': 'Adds manufacturers and attributes on the product view.',
     'author': 'OpenERP SA, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
