e0c9e4364ad05cdd1da8d185d423d5cff13be4e8
David <david.vidal@tecnativa.com>
2018-11-20 09:58:07 +0100

    fixup! fixup! fixup! fixup! [ADD] product_weight_through_uom: New module

--- product_weight_through_uom/hooks.py
+++ product_weight_through_uom/hooks.py
@@ -9,8 +9,7 @@ def post_init_hook(cr, registry, vals=None):
     env = api.Environment(cr, SUPERUSER_ID, {})
     # Change only those with no weight already set
     products_init = env['product.product'].search([
-        ('is_weight_uom', '=', True),
         ('weight', '=', 0),
-    ])
+    ]).filtered('is_weight_uom')
     for product in products_init:
         product._onchange_uom()
