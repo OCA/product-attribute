42eee96685419488812ea8f9a60735a09cb81226
Ruchir Shukla <ruchir@bizzappdev.com>
2018-11-25 09:51:17 +0530

    [MIG]product_code_unique: migration from 11 to 12

--- product_code_unique/__manifest__.py
+++ product_code_unique/__manifest__.py
@@ -4,16 +4,12 @@
 {
     "name": "Unique Product Internal Reference",
     "summary": "Set Product Internal Reference as Unique",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "depends": ["product"],
-    "data": [
-    ],
-    'demo': [
-    ],
     "pre_init_hook": 'pre_init_product_code',
     "installable": True,
 }
