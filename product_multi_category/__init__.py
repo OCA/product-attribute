d6db4df54fff5f0d5aef70fbbcfb40134724545b
Sudhir Arya <sudhir@erpharbor.com>
2018-12-04 17:12:51 +0530

    [MIG] Migrated product_multi_category to v12

--- product_multi_category/__init__.py
+++ product_multi_category/__init__.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-# © 2009 Akretion,Guewen Baconnier,Camptocamp,Avanzosc,Sharoon Thomas,Sodexis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import models
