4ef34d546afd055782419f57781ad840d40a2c79
Sudhir Arya <sudhir@erpharbor.com>
2018-12-04 12:18:06 +0530

    [MIG] Migrated product_code_mandatory to v12

--- product_code_mandatory/__init__.py
+++ product_code_mandatory/__init__.py
@@ -1,6 +1,3 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import models
 
 
--- product_code_mandatory/models/product.py
+++ product_code_mandatory/models/product.py
@@ -8,8 +8,7 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     def _get_default_code(self):
-        res = self.env['ir.sequence'].next_by_code('product.default.code')
-        return res
+        return self.env['ir.sequence'].next_by_code('product.default.code')
 
     default_code = fields.Char('Internal Reference', index=True,
                                default=_get_default_code)
