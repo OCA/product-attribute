6feee81266ac7bef2caaed6f01e04fa81b82f3b4
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-12-04 10:02:12 +0200

    [MIG] product_pricelist_direct_print: Migration to 12.0

--- product_brand/tests/test_product_brand.py
+++ product_brand/tests/test_product_brand.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2018 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
 
