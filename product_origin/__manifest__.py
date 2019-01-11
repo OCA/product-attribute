2c6b6c10809352963a97ec435c489da0e710a673
Denis Roussel <denis.roussel@acsone.eu>
2019-01-11 09:07:48 +0100

    [10.0][ADD] product_origin

---
+++ product_origin/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Product Origin',
+    'summary': '''
+        Adds the origin of the product''',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/product-attribute',
+    'maintainers': ['rousseldenis'],
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_template.xml',
+    ],
+}
---
+++ product_origin/readme/USAGE.rst
@@ -0,0 +1,3 @@
+* Go to product form
+* Fill in the country and/or state of origin of the product under the
+  'General Information' tab.
