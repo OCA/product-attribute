3f23cb220ae31602ada260bbbb123f1661350e69
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-29 23:22:53 +0530

    [MIG] product_pricelist_supplierinfo v11 to v12

--- product_pricelist_supplierinfo/__manifest__.py
+++ product_pricelist_supplierinfo/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Supplier info prices in sales pricelists",
     "summary": "Allows to create priceslists based on supplier info",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa,"
