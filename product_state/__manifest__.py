b01d7601eb09544b5ecdaaedf7443d5d687b1241
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-29 22:59:31 +0530

    product_state v11 to v12

--- product_state/__manifest__.py
+++ product_state/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Product State",
-    'description': """
+    'summary': """
         Module introducing a state field on product template""",
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "https://github.com/OCA/product-attribute",
     'category': 'Product',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'product',
