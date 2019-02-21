9cef7bd275644ab39314de5edbf0711630f3659d
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-02-21 01:22:43 +0100

    [11.0][FIX] product_pricelist_supplierinfo: Add template compatibility (#436)
    Previously, only worked fine with product.product.

--- product_pricelist_supplierinfo/models/__init__.py
+++ product_pricelist_supplierinfo/models/__init__.py
@@ -1,4 +1,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import product_product
 from . import product_pricelist
+from . import product_product
+from . import product_template
