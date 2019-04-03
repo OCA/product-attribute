6508a6a11c7575ab882a6d3bfe1af9d31c6bc9a9
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-04-03 18:06:52 +0200

    [FIX] product_supplierinfo_for_customer: Adapt views to variants handling
    The same way Odoo does.

--- product_supplierinfo_for_customer/models/product_template.py
+++ product_supplierinfo_for_customer/models/product_template.py
@@ -1,6 +1,7 @@
 # Copyright 2015 OdooMRP team
 # Copyright 2015 AvanzOSC
 # Copyright 2015 Tecnativa
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
@@ -9,6 +10,9 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     customer_ids = fields.One2many(
+        comodel_name='product.supplierinfo', inverse_name='product_tmpl_id',
+        string='Customer', domain=[('supplierinfo_type', '=', 'customer')])
+    variant_customer_ids = fields.One2many(
         comodel_name='product.supplierinfo', inverse_name='product_tmpl_id',
         string='Customer', domain=[('supplierinfo_type', '=', 'customer')])
     supplier_ids = fields.One2many(
