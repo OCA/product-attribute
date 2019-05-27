fb7b4af0906f307a56411efd1840bce9462ad91b
Denis Roussel <denis.roussel@acsone.eu>
2019-05-27 11:16:07 +0200

    [12.0][MIG] packaging_uom

--- packaging_uom/models/product_packaging.py
+++ packaging_uom/models/product_packaging.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
@@ -9,22 +9,30 @@ class ProductPackaging(models.Model):
 
     @api.model
     def _default_uom_categ_domain_id(self):
+        """
+        Taking value from context depending from which form it was called
+        :return:
+        """
         product_id = self.env.context.get("default_product_id")
-        if not product_id:
-            return self.env['product.uom.categ']
-        uom = self.env['product.product'].browse(product_id).uom_id
+        categ_id = self.env.context.get('get_uom_categ_from_uom')
+        if not product_id and not categ_id:
+            return self.env['uom.category']
+        if categ_id:
+            uom = self.env['uom.uom'].browse(categ_id)
+        if product_id:
+            uom = self.env['product.product'].browse(product_id).uom_id
         return uom.category_id.id
 
     uom_id = fields.Many2one(
-        'product.uom',
-        'Unit of Measure',
+        'uom.uom',
+        'Packaging Unit of Measure',
         help="It must be in the same category than "
              "the default unit of measure.",
         required=False
     )
     uom_categ_domain_id = fields.Many2one(
-        default=_default_uom_categ_domain_id,
-        comodel_name='product.uom.categ'
+        default=lambda self: self._default_uom_categ_domain_id(),
+        comodel_name='uom.category'
     )
     qty = fields.Float(
         compute="_compute_qty",
