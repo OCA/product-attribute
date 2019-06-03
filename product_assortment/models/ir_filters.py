498e67e729a4de928f781c29d7f46a0551e68e8b
Denis Roussel <denis.roussel@acsone.eu>
2019-06-03 16:12:01 +0200

    [12.0][MIG] product_assortment

--- product_assortment/models/ir_filters.py
+++ product_assortment/models/ir_filters.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 ACSONE SA/NV
+# Copyright 2018-2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models, _
 from odoo.osv import expression
