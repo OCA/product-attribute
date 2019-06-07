21bf8758669dde1b6566aa77e80c5a71278c2269
mreficent <miquel.raich@eficent.com>
2019-06-07 12:25:45 +0200

    [MIG] product_restricted_type: Migration to 12.0

--- product_restricted_type/__manifest__.py
+++ product_restricted_type/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Product Restricted Type',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "Eficent,"
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/product-attribute',
