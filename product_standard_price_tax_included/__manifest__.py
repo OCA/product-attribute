880a8fa3e8765ca317da32c866af80e7a20b8380
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-07 17:59:20 +0200

    [PORT] product_standard_price_tax_included

--- product_standard_price_tax_included/__manifest__.py
+++ product_standard_price_tax_included/__manifest__.py
@@ -1,11 +1,10 @@
-# coding: utf-8
 # Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Product - Cost Price Tax Included',
-    'version': '8.0.2.1.0',
+    'version': '12.0.1.0.0',
     'category': 'Product',
     'summary': 'Brings a Cost Price Field Tax Included on Product Model',
     'author': 'GRAP,Odoo Community Association (OCA)',
@@ -14,19 +13,15 @@
     'development_status': 'Beta',
     'maintainers': ['legalsylvain'],
     'depends': [
-        'product',
         'sale',
-        'account',
     ],
     'data': [
-        'data/product_price_type.xml',
-        'views/view_product_template.xml',
+        'views/view_product_product.xml',
     ],
     'demo': [
         'demo/res_groups.xml',
         'demo/product_pricelist.xml',
-        'demo/res_partner.xml',
         'demo/account_tax.xml',
-        'demo/product_template.xml',
+        'demo/product_product.xml',
     ],
 }
--- product_standard_price_tax_included/models/__init__.py
+++ product_standard_price_tax_included/models/__init__.py
@@ -1,2 +1,4 @@
-# coding: utf-8
-from . import product_template
+from . import account_tax
+from . import product_pricelist
+from . import product_pricelist_item
+from . import product_product
