c10473892c475b5e058aa87d380d29f19fe0540f
Florian da Costa <florian.dacosta@akretion.com>
2019-08-19 17:17:40 +0200

    [FIX] product_weight: Fix bom weight calculation in case of recursive (phantom) bom

--- product_weight/models/__init__.py
+++ product_weight/models/__init__.py
@@ -1,5 +1,3 @@
 # Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # Copyright (C) 2015 Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import bom
