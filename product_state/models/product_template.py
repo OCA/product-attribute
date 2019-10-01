a2a5f5734e814eae900b6e291ce0578a7bc3b076
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-10-01 22:29:56 +0530

    [MIG] Migration product_state v12 to v13

--- product_state/models/product_template.py
+++ product_state/models/product_template.py
@@ -1,18 +1,20 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    state = fields.Selection(selection=[
-        ('draft', 'In Development'),
-        ('sellable', 'Normal'),
-        ('end', 'End of Lifecycle'),
-        ('obsolete', 'Obsolete')],
-        string='Status',
-        default='sellable',
-        index=True
+    state = fields.Selection(
+        selection=[
+            ("draft", "In Development"),
+            ("sellable", "Normal"),
+            ("end", "End of Lifecycle"),
+            ("obsolete", "Obsolete"),
+        ],
+        string="Status",
+        default="sellable",
+        index=True,
     )
