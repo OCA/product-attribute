83f2f2975aa695d03625bfe04a4cde9d27f1fe01
Kaushal Prajapati <kbprajapati@live.com>
2019-10-15 19:44:40 +0530

    [MIG][13.0] product_brand

--- product_brand/readme/CONTRIBUTORS.rst
+++ product_brand/readme/CONTRIBUTORS.rst
@@ -10,3 +10,5 @@
 * `Tecnativa <https://www.tecnativa.com>`_
 
   * David Vidal <david.vidal@tecnativa.com>
+
+* Kaushal Prajapati <kbprajapati@live.com>
--- product_brand/reports/account_invoice_report.py
+++ product_brand/reports/account_invoice_report.py
@@ -1,32 +1,25 @@
 # Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    product_brand_id = fields.Many2one(
-        comodel_name='product.brand',
-        string='Brand',
-    )
+    product_brand_id = fields.Many2one(comodel_name="product.brand",
+                                       string="Brand")
 
+    @api.model
     def _select(self):
-        select_str = super()._select()
+        select_str = super(AccountInvoiceReport, self)._select()
         select_str += """
-            , sub.product_brand_id as product_brand_id
+            , template.product_brand_id as product_brand_id
             """
         return select_str
 
-    def _sub_select(self):
-        select_str = super()._sub_select()
-        select_str += """
-            , pt.product_brand_id
-            """
-        return select_str
-
+    @api.model
     def _group_by(self):
-        group_by_str = super()._group_by()
-        group_by_str += ", pt.product_brand_id"
+        group_by_str = super(AccountInvoiceReport, self)._group_by()
+        group_by_str += ", template.product_brand_id"
         return group_by_str
