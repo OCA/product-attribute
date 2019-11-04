381577e2dbfd677f8d8d9b56750165905924671b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-04 10:00:22 +0100

    [FIX] product_m2mcategories: RST syntax

--- product_m2mcategories/__openerp__.py
+++ product_m2mcategories/__openerp__.py
@@ -25,13 +25,14 @@
     "category": "Product Management",
     "depends": ['base', 'product'],
     "description": """
-    This module Extends the existing functionality of Open ERP Products
-    (One product - One Catgory) to One product -> Many Categories
+This module Extends the existing functionality of Open ERP Products
+(One product - One Catgory) to One product -> Many Categories
 
-    *Note: This module was built generically but in focus of the Magento
-    Open ERP connector
-    **Note: The additional categories are only for classification and does
-    not affect other operations
+**Note**: This module was built generically but in focus of the Magento
+Open ERP connector.
+  
+**Note 2**: The additional categories are only for classification and does
+not affect other operations.
     """,
     "data": [
             'views/product_view.xml'
