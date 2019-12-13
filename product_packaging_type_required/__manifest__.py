be8e21f08f0681d5f6be4ecfab65eaa1f7d3511f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-13 16:45:55 +0100

    Migrate product_packaging_type_required to 13.0

--- product_packaging_type_required/__manifest__.py
+++ product_packaging_type_required/__manifest__.py
@@ -2,23 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Packaging Type Required',
-    'version': '12.0.1.0.0',
-    'development_status': "Beta",
-    'category': 'Product',
-    'summary': "Product Packaging Type Required",
-    'author': 'Camptocamp, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-attribute',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
-        'product_packaging_type',
-        ],
-    'data': [
-        'data/cron.xml',
-        'views/product_packaging_type_view.xml',
-    ],
-    'installable': True,
-    'auto_install': False
+    "name": "Product Packaging Type Required",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Product",
+    "summary": "Product Packaging Type Required",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "depends": ["product", "product_packaging_type"],
+    "data": ["data/cron.xml", "views/product_packaging_type_view.xml"],
+    "installable": True,
+    "auto_install": False,
 }
