b0652829d6bc857e75578e7d27939732ad762025
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-13 16:44:58 +0100

    Migrate product_packaging_type to 13.0

--- product_packaging_type/__manifest__.py
+++ product_packaging_type/__manifest__.py
@@ -2,25 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Packaging Type',
-    'version': '12.0.1.0.0',
-    'development_status': "Beta",
-    'category': 'Product',
-    'summary': "Product Packagin Type",
-    'author': 'Camptocamp, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-attribute',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
-        'stock',
-        ],
-    'data': [
-        'data/product_packaging_type.xml',
-        'security/ir.model.access.csv',
-        'views/product_packaging_type_view.xml',
-        'views/product_packaging_view.xml',
+    "name": "Product Packaging Type",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Product",
+    "summary": "Product Packaging Type",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "depends": ["product", "stock"],
+    "data": [
+        "data/product_packaging_type.xml",
+        "security/ir.model.access.csv",
+        "views/product_packaging_type_view.xml",
+        "views/product_packaging_view.xml",
     ],
-    'installable': True,
-    'auto_install': False
+    "installable": True,
+    "auto_install": False,
 }
