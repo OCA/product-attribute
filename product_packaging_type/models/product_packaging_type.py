4520b69f7e98c289483a7ce0ee234bd29e9299b5
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-13 16:44:58 +0100

    Migrate product_packaging_type to 13.0

--- product_packaging_type/models/product_packaging_type.py
+++ product_packaging_type/models/product_packaging_type.py
@@ -1,6 +1,7 @@
 # Copyright 2019 Camptocamp (<http://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from collections import OrderedDict
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -8,7 +9,7 @@ from odoo.exceptions import ValidationError
 class ProductPackagingType(models.Model):
     _name = "product.packaging.type"
     _description = "Type management for product.packaging"
-    _order = 'sequence, code'
+    _order = "sequence, code"
 
     name = fields.Char(required=True, translate=True)
     code = fields.Char(required=True)
@@ -17,30 +18,25 @@ class ProductPackagingType(models.Model):
     active = fields.Boolean(default=True)
     is_default = fields.Boolean()
 
-    @api.constrains('is_default')
+    @api.constrains("is_default")
     def _check_is_default(self):
         msg = False
-        default_count = self.search_count([('is_default', '=', True)])
+        default_count = self.search_count([("is_default", "=", True)])
         if default_count == 0:
-            msg = _(
-                'There must be one product packaging type set as "Is Default".'
-            )
+            msg = _('There must be one product packaging type set as "Is Default".')
         elif default_count > 1:
-            msg = _(
-                'Only one product packaging type can be set as "Is Default".'
-            )
+            msg = _('Only one product packaging type can be set as "Is Default".')
         if msg:
             raise ValidationError(msg)
 
 
 class ProductPackaging(models.Model):
     _inherit = "product.packaging"
-    _order = 'product_id, type_sequence'
+    _order = "product_id, type_sequence"
 
-    @api.model
     def default_packaging_type_id(self):
-        return self.env['product.packaging.type'].search(
-            [('is_default', '=', True)], limit=1
+        return self.env["product.packaging.type"].search(
+            [("is_default", "=", True)], limit=1
         )
 
     packaging_type_id = fields.Many2one(
@@ -49,8 +45,7 @@ class ProductPackaging(models.Model):
         ondelete="restrict",
         default=lambda p: p.default_packaging_type_id(),
     )
-    type_has_gtin = fields.Boolean(related="packaging_type_id.has_gtin",
-                                   readonly=True)
+    type_has_gtin = fields.Boolean(related="packaging_type_id.has_gtin", readonly=True)
     type_sequence = fields.Integer(
         string="Type sequence",
         related="packaging_type_id.sequence",
@@ -58,26 +53,27 @@ class ProductPackaging(models.Model):
         store=True,
     )
     qty_per_type = fields.Html(
-        compute='_compute_qty_per_type', string='Qty per package type',
+        compute="_compute_qty_per_type", string="Qty per package type"
     )
 
     @api.depends(
-        'product_id', 'product_id.packaging_ids', 'packaging_type_id',
-        'packaging_type_id.code'
+        "product_id",
+        "product_id.packaging_ids",
+        "packaging_type_id",
+        "packaging_type_id.code",
     )
     def _compute_qty_per_type(self):
         for packaging in self:
             product = packaging.product_id
             if not product:
+                packaging.qty_per_type = ""
                 continue
 
             smaller_product_packagings = product.packaging_ids.filtered(
                 lambda p: p.id != packaging.id and packaging.qty > p.qty > 0.0
             )
             res = OrderedDict()
-            for p_pack in smaller_product_packagings.sorted(
-                lambda p: p.qty
-            ):
+            for p_pack in smaller_product_packagings.sorted(lambda p: p.qty):
                 res[p_pack.packaging_type_id.code] = p_pack.qty
             packaging.qty_per_type = packaging._format_qty_per_type(res)
 
@@ -89,8 +85,8 @@ class ProductPackaging(models.Model):
             if not new_qty.is_integer():
                 new_qty_int = int(new_qty)
                 new_qty_decimals = new_qty - new_qty_int
-                new_qty = '%s<span style="color: red;">%s</span>' % (
-                    new_qty_int, str(new_qty_decimals).lstrip('0')
+                new_qty = '{}<span style="color: red;">{}</span>'.format(
+                    new_qty_int, str(new_qty_decimals).lstrip("0")
                 )
-            res.append('%s %s' % (new_qty, code))
-        return '; '.join(res)
+            res.append("{} {}".format(new_qty, code))
+        return "; ".join(res)
