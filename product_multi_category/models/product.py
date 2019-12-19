b77c0eb287dc27ab6e9dab456aca414e90f2eed3
ps-tubtim <pimolnat.s@outlook.com>
2019-12-19 17:26:49 +0700

    [MIG] product_multi_category: Migration to 13.0

--- product_multi_category/models/product.py
+++ product_multi_category/models/product.py
@@ -1,4 +1,4 @@
-# © 2009 Akretion,Guewen Baconnier,Camptocamp,Avanzosc,Sharoon Thomas,Sodexis
+# Copyright 2009 Akretion,Guewen Baconnier,Camptocamp,Avanzosc,Sharoon Thomas,Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -7,11 +7,10 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    categ_id = fields.Many2one(string="Pricing/Primary Category")
     categ_ids = fields.Many2many(
         comodel_name="product.category",
         relation="product_categ_rel",
         column1="product_id",
         column2="categ_id",
-        string="Extra categories",
+        string="Extra Categories",
     )
