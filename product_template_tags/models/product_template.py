491ea80c2c4e2b46e4f6328b861d143648d79b0a
ps-tubtim <pimolnat.s@outlook.com>
2019-12-20 17:23:07 +0700

    [IMP] product_template_tags: black, isort

--- product_template_tags/models/product_template.py
+++ product_template_tags/models/product_template.py
@@ -6,9 +6,12 @@ from odoo import fields, models
 
 class ProductTemplate(models.Model):
 
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     tag_ids = fields.Many2many(
-        comodel_name='product.template.tag', string="Product Tags",
-        relation='product_template_product_tag_rel',
-        column1='product_tmpl_id', column2='tag_id')
+        comodel_name="product.template.tag",
+        string="Product Tags",
+        relation="product_template_product_tag_rel",
+        column1="product_tmpl_id",
+        column2="tag_id",
+    )
