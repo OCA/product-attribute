6f16be8ef161d93f5b22f187df70be346e5eae2f
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-23 08:34:48 +0100

    [IMP] product_restricted_type: black, isort

--- product_restricted_type/models/product.py
+++ product_restricted_type/models/product.py
@@ -1,16 +1,15 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
     restricted_product_type = fields.Selection(
         string="Restricted Product Type",
-        selection=lambda self: self.env['product.template']._fields[
-            'type'].selection,
+        selection=lambda self: self.env["product.template"]._fields["type"].selection,
     )
 
     @api.constrains("restricted_product_type")
@@ -18,13 +17,18 @@ class ProductCategory(models.Model):
         """Check if any product in the category has different type"""
         for categ in self:
             if categ.restricted_product_type:
-                products = self.env['product.template'].search(
-                    [('categ_id', '=', categ.id),
-                     ('type', '!=', categ.restricted_product_type)],
+                products = self.env["product.template"].search(
+                    [
+                        ("categ_id", "=", categ.id),
+                        ("type", "!=", categ.restricted_product_type),
+                    ],
                     limit=1,
                 )
                 if products:
-                    raise ValidationError(_(
-                        'A product (or multiple products) in the category '
-                        'have different type than the selected restricted '
-                        'product type'))
+                    raise ValidationError(
+                        _(
+                            "A product (or multiple products) in the category "
+                            "have different type than the selected restricted "
+                            "product type"
+                        )
+                    )
