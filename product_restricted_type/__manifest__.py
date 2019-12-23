af2802d5220fab06bac529b20b20114ab326aeb4
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-23 08:34:48 +0100

    [IMP] product_restricted_type: black, isort

--- product_restricted_type/__manifest__.py
+++ product_restricted_type/__manifest__.py
@@ -3,19 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Restricted Type',
-    'version': '12.0.1.0.0',
-    'author': "Eficent,"
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/product-attribute',
-    'license': 'AGPL-3',
-    'category': 'Product',
-    'depends': [
-        'product',
-    ],
-    'data': [
-        'views/product_views.xml',
-    ],
-    'auto_install': False,
-    'installable': True,
+    "name": "Product Restricted Type",
+    "version": "12.0.1.0.0",
+    "author": "Eficent," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "category": "Product",
+    "depends": ["product"],
+    "data": ["views/product_views.xml"],
+    "auto_install": False,
+    "installable": True,
 }
--- product_restricted_type/models/product_template.py
+++ product_restricted_type/models/product_template.py
@@ -1,30 +1,38 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    @api.onchange('categ_id')
+    @api.onchange("categ_id")
     def _onchange_categ_id(self):
         if self.categ_id and self.categ_id.restricted_product_type:
             self.type = self.categ_id.restricted_product_type
 
-    @api.onchange('type')
+    @api.onchange("type")
     def _onchange_type(self):
         if self.type:
-            return {'domain': {'categ_id': [
-                ('restricted_product_type', 'in', [self.type, False])]}}
+            return {
+                "domain": {
+                    "categ_id": [("restricted_product_type", "in", [self.type, False])]
+                }
+            }
         else:
-            return {'domain': {'categ_id': []}}
+            return {"domain": {"categ_id": []}}
 
     @api.constrains("type")
     def _check_product_type(self):
         for product in self:
-            if product.categ_id.restricted_product_type and product.type != \
-                    product.categ_id.restricted_product_type:
-                raise ValidationError(_(
-                    'The product type must be equal to the restricted '
-                    'product type defined in the product category'))
+            if (
+                product.categ_id.restricted_product_type
+                and product.type != product.categ_id.restricted_product_type
+            ):
+                raise ValidationError(
+                    _(
+                        "The product type must be equal to the restricted "
+                        "product type defined in the product category"
+                    )
+                )
