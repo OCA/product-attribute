c57b67be7b7fd414a4735a7478d90d9ec48a3caa
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-03 13:25:40 +0100

    [ADD] product_product_template_navigation:
    This module adds a button in the product form to view the product template.

---
+++ product_product_template_navigation/models/product_product.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Alexandre Díaz - Tecnativa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    def action_open_product_template(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "product.template",
+            "res_id": self.product_tmpl_id.id,
+            "views": [[False, "form"]],
+        }
