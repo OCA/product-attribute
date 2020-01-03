4d090ab5df8643928d9c4788ade2d57eb1145fc2
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-03 13:25:40 +0100

    [ADD] product_product_template_navigation:
    This module adds a button in the product form to view the product template.

---
+++ product_product_template_navigation/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Alexandre Díaz - Tecnativa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Template Navigation",
+    "summary": "Adds a button in product to view the template",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "category": "Product",
+    "version": "13.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": ["views/product_product_views.xml"],
+    "installable": True,
+}
---
+++ product_product_template_navigation/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds a button in the product form to view the product template.
+
+.. image:: ../static/img/template_navigation.gif
