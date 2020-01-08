08ae52687942ee1fa65dfe7a48c07274015d0c82
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-01-08 13:46:32 +0100

    [IMP] product_packaging_type_pallet: black, isort

--- product_packaging_type_pallet/models/product_packaging.py
+++ product_packaging_type_pallet/models/product_packaging.py
@@ -4,17 +4,15 @@ from odoo import fields, models
 
 
 class ProductPackagingType(models.Model):
-    _inherit = 'product.packaging.type'
+    _inherit = "product.packaging.type"
 
     is_pallet = fields.Boolean()
 
 
 class ProductPackaging(models.Model):
 
-    _inherit = 'product.packaging'
+    _inherit = "product.packaging"
 
-    type_is_pallet = fields.Boolean(
-        related='packaging_type_id.is_pallet'
-    )
+    type_is_pallet = fields.Boolean(related="packaging_type_id.is_pallet")
     layers = fields.Integer()
-    packs_per_layer = fields.Integer(help='number of boxes/bags on a layer')
+    packs_per_layer = fields.Integer(help="number of boxes/bags on a layer")
