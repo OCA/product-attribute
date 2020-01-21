0393409392fefe05fc629d6467ff1ea50932821e
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-21 21:36:58 +0100

    [IMP] product_sequence: black, isort

--- product_sequence/models/product_product.py
+++ product_sequence/models/product_product.py
@@ -8,25 +8,25 @@ from odoo import _, api, fields, models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     default_code = fields.Char(
         required=True,
-        default='/',
-        track_visibility='onchange',
+        default="/",
+        track_visibility="onchange",
         help="Set to '/' and save if you want a new internal reference "
-             "to be proposed."
+        "to be proposed.",
     )
 
     @api.model
     def create(self, vals):
-        if 'default_code' not in vals or vals['default_code'] == '/':
+        if "default_code" not in vals or vals["default_code"] == "/":
             categ_id = vals.get("categ_id")
             template_id = vals.get("product_tmpl_id")
             categ = sequence = False
             if categ_id:
                 # Created as a product.product
-                categ = self.env['product.category'].browse(categ_id)
+                categ = self.env["product.category"].browse(categ_id)
             elif template_id:
                 # Created from a product.template
                 template = self.env["product.template"].browse(template_id)
@@ -34,8 +34,8 @@ class ProductProduct(models.Model):
             if categ:
                 sequence = categ.sequence_id
             if not sequence:
-                sequence = self.env.ref('product_sequence.seq_product_auto')
-            vals['default_code'] = sequence.next_by_id()
+                sequence = self.env.ref("product_sequence.seq_product_auto")
+            vals["default_code"] = sequence.next_by_id()
         return super().create(vals)
 
     @api.multi
@@ -44,19 +44,18 @@ class ProductProduct(models.Model):
         Note this is up to the user, if the product category is changed,
         she/he will need to write '/' on the internal reference to force the
         re-assignment."""
-        if vals.get('default_code', '') == '/':
-            product_category_obj = self.env['product.category']
+        if vals.get("default_code", "") == "/":
+            product_category_obj = self.env["product.category"]
             for product in self:
-                category_id = vals.get('categ_id', product.categ_id.id)
+                category_id = vals.get("categ_id", product.categ_id.id)
                 category = product_category_obj.browse(category_id)
                 sequence = category.exists() and category.sequence_id
                 if not sequence:
-                    sequence = self.env.ref(
-                        'product_sequence.seq_product_auto')
+                    sequence = self.env.ref("product_sequence.seq_product_auto")
                 ref = sequence.next_by_id()
-                vals['default_code'] = ref
+                vals["default_code"] = ref
                 if len(product.product_tmpl_id.product_variant_ids) == 1:
-                    product.product_tmpl_id.write({'default_code': ref})
+                    product.product_tmpl_id.write({"default_code": ref})
                 super(ProductProduct, product).write(vals)
             return True
         return super().write(vals)
@@ -65,8 +64,6 @@ class ProductProduct(models.Model):
     def copy(self, default=None):
         if default is None:
             default = {}
-        if self.default_code and 'default_code' not in default:
-            default.update({
-                'default_code': self.default_code + _('-copy'),
-            })
+        if self.default_code and "default_code" not in default:
+            default.update({"default_code": self.default_code + _("-copy")})
         return super().copy(default)
