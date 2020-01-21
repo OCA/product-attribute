452f47e077a1e925ffe4eb8f9c6b213e988d1ef3
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-21 21:36:58 +0100

    [IMP] product_sequence: black, isort

--- product_sequence/__manifest__.py
+++ product_sequence/__manifest__.py
@@ -5,20 +5,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Sequence',
-    'version': '12.0.1.0.1',
-    'author': "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/product-attribute',
-    'license': 'AGPL-3',
-    'category': 'Product',
-    'depends': [
-        'product',
-        'product_code_unique',
-    ],
-    'data': [
-        'data/product_sequence.xml',
-        'views/product_category.xml',
-    ],
-    'pre_init_hook': 'pre_init_hook',
-    'installable': True,
+    "name": "Product Sequence",
+    "version": "12.0.1.0.1",
+    "author": "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "category": "Product",
+    "depends": ["product", "product_code_unique"],
+    "data": ["data/product_sequence.xml", "views/product_category.xml"],
+    "pre_init_hook": "pre_init_hook",
+    "installable": True,
 }
