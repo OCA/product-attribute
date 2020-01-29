5db1d81c39a23ac46c94ea04fdd0f1c027a59dc3
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-29 22:33:51 +0100

    [NEW] stock_account_product_cost_security: New module to hide cost elements according to product cost security

---
+++ stock_account_product_cost_security/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Account Product Cost Security",
+    "summary": "Product cost security restriction view",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "maintainers": ["sergio-teruel"],
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": ["stock_account", "product_cost_security"],
+    "data": ["views/product_views.xml", "wizard/stock_change_standard_price_views.xml"],
+}
