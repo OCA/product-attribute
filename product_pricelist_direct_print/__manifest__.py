4bef760ec0bef852e2394f41b5be8a2c36c96686
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-01 20:21:15 +0100

    [IMP] product_pricelist_direct_print: black, isort

--- product_pricelist_direct_print/__manifest__.py
+++ product_pricelist_direct_print/__manifest__.py
@@ -3,17 +3,13 @@
 {
     "name": "Product Pricelist Direct Print",
     "summary": "Print price list from menu option, product templates, "
-               "products variants or price lists",
+    "products variants or price lists",
     "version": "12.0.1.0.0",
     "category": "Product",
     "website": "https://www.github.com/OCA/product-attribute",
-    "author":
-        "Tecnativa, "
-        "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": [
-        "sale",
-    ],
+    "depends": ["sale"],
     "data": [
         "views/report_product_pricelist.xml",
         # 'mail_template_data' has to be after 'report_product_pricelist'
