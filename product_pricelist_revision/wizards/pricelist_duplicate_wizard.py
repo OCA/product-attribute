f423a3e58555519a0bc4755e58d028b423499676
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-03 19:19:49 +0100

    [IMP] product_pricelist_revision: black, isort

--- product_pricelist_revision/wizards/pricelist_duplicate_wizard.py
+++ product_pricelist_revision/wizards/pricelist_duplicate_wizard.py
@@ -4,39 +4,41 @@
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models
+
 from odoo.addons import decimal_precision as dp
 
 
 class ProductPricelistItemDuplicateWizard(models.TransientModel):
-    _name = 'product.pricelist.item.duplicate.wizard'
-    _description = 'Wizard Product Pricelist Item Duplicate'
+    _name = "product.pricelist.item.duplicate.wizard"
+    _description = "Wizard Product Pricelist Item Duplicate"
 
     date_start = fields.Date(required=True)
     date_end = fields.Date()
     variation_percent = fields.Float(
-        digits=dp.get_precision('Product Price'),
-        string='Variation %',
+        digits=dp.get_precision("Product Price"), string="Variation %"
     )
 
     @api.multi
     def action_apply(self):
-        PricelistItem = self.env['product.pricelist.item']
+        PricelistItem = self.env["product.pricelist.item"]
         new_items = PricelistItem
-        for item in PricelistItem.browse(self.env.context['active_ids']):
-            new_items |= item.copy({
-                'date_start': self.date_start,
-                'date_end': self.date_end,
-                'previous_item_id': item.id,
-                'fixed_price': item.fixed_price * (
-                    1.0 + self.variation_percent / 100.0),
-            })
+        for item in PricelistItem.browse(self.env.context["active_ids"]):
+            new_items |= item.copy(
+                {
+                    "date_start": self.date_start,
+                    "date_end": self.date_end,
+                    "previous_item_id": item.id,
+                    "fixed_price": item.fixed_price
+                    * (1.0 + self.variation_percent / 100.0),
+                }
+            )
             item.date_end = self.date_start - relativedelta(days=1)
 
         action = self.env.ref(
-            'product_pricelist_revision.product_pricelist_item_action'
+            "product_pricelist_revision.product_pricelist_item_action"
         ).read()[0]
         if len(new_items) > 0:
-            action['domain'] = [('id', 'in', new_items.ids)]
+            action["domain"] = [("id", "in", new_items.ids)]
         else:
-            action = {'type': 'ir.actions.act_window_close'}
+            action = {"type": "ir.actions.act_window_close"}
         return action
