933b1126bb68fe4a3063303bc83497e21600e894
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-03 19:19:49 +0100

    [IMP] product_pricelist_revision: black, isort

--- product_pricelist_revision/__manifest__.py
+++ product_pricelist_revision/__manifest__.py
@@ -10,11 +10,6 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "sale_management",
-    ],
-    "data": [
-        "views/pricelist_view.xml",
-        "wizards/pricelist_duplicate_wizard_view.xml",
-    ],
+    "depends": ["sale_management"],
+    "data": ["views/pricelist_view.xml", "wizards/pricelist_duplicate_wizard_view.xml"],
 }
