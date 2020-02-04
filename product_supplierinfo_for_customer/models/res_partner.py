12900c3bd2012a5786effb7ecf740f938dc644f1
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-04 21:17:44 +0100

    [IMP] product_supplierinfo_for_customer: black, isort

--- product_supplierinfo_for_customer/models/res_partner.py
+++ product_supplierinfo_for_customer/models/res_partner.py
@@ -6,15 +6,17 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def default_get(self, fields):
         res = super(ResPartner, self).default_get(fields)
-        select_type = self.env.context.get('select_type', False)
+        select_type = self.env.context.get("select_type", False)
         if select_type:
-            res.update({
-                'customer': select_type == 'customer',
-                'supplier': select_type == 'supplier',
-            })
+            res.update(
+                {
+                    "customer": select_type == "customer",
+                    "supplier": select_type == "supplier",
+                }
+            )
         return res
