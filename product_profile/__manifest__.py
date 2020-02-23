dea1e1a35cb4f4cc54333d1804bd644733d68421
kevinkhao <kevin.khao@akretion.com.br>
2020-02-23 22:14:37 +0100

    [MIG][12.0] product_profile: Migration to 12.0

--- product_profile/__manifest__.py
+++ product_profile/__manifest__.py
@@ -1,22 +1,21 @@
-# coding: utf-8
 # © 2015 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Product Profile",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "summary": "Allow to configure a product in 1 click",
     "category": "product",
-    "depends": ["sale"],
-    "website": "http://www.akretion.com/",
+    "depends": ["sale_management"],
+    "website": "https://github.com/oca/product-attribute",
     "data": [
         "security/group.xml",
         "views/product_view.xml",
         "views/config_view.xml",
         "security/ir.model.access.csv",
     ],
-    "demo": ["demo/product.profile.csv"],
     "installable": True,
     "license": "AGPL-3",
+    "maintainers": ["bealdav", "sebastienbeau", "kevinkhao"],
 }
