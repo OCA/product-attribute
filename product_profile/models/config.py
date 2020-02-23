e090a6acd3cf0362b26940394111048fe9462a6a
kevinkhao <kevin.khao@akretion.com.br>
2020-02-23 22:13:04 +0100

    [IMP] product_profile: black, file structure change

--- product_profile/models/config.py
+++ product_profile/models/config.py
@@ -7,10 +7,11 @@ from .product import PROFILE_MENU
 
 
 class BaseConfigSettings(models.TransientModel):
-    _inherit = 'base.config.settings'
+    _inherit = "base.config.settings"
 
     group_product_profile = fields.Boolean(
         string="Display Product Profile fields",
-        implied_group='product_profile.group_product_profile',
+        implied_group="product_profile.group_product_profile",
         help="Display fields computed by product profile "
-             "module.\nFor debugging purpose see menu\n%s" % PROFILE_MENU)
+        "module.\nFor debugging purpose see menu\n%s" % PROFILE_MENU,
+    )
