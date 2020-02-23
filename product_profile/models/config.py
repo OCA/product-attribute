7170d9d2a7236ade3f602bf2196b80398739b093
kevinkhao <kevin.khao@akretion.com.br>
2020-02-23 22:14:37 +0100

    [MIG][12.0] product_profile: Migration to 12.0

--- product_profile/models/config.py
+++ product_profile/models/config.py
@@ -1,17 +1,16 @@
-# coding: utf-8
 # © 2015 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
-from .product import PROFILE_MENU
+from .product_profile import PROFILE_MENU
 
 
-class BaseConfigSettings(models.TransientModel):
-    _inherit = "base.config.settings"
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
 
     group_product_profile = fields.Boolean(
         string="Display Product Profile fields",
-        implied_group="product_profile.group_product_profile",
+        implied_group="product_profile.group_product_profile_user",
         help="Display fields computed by product profile "
         "module.\nFor debugging purpose see menu\n%s" % PROFILE_MENU,
     )
