908429d31d483d3b45cdac3e0ee0d3ad5481b0d7
kevinkhao <kevin.khao@akretion.com.br>
2020-02-23 22:13:04 +0100

    [IMP] product_profile: black, file structure change

--- product_profile/__manifest__.py
+++ product_profile/__manifest__.py
@@ -3,24 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Product Profile',
-    'version': '10.0.1.0.0',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'summary': "Allow to configure a product in 1 click",
-    'category': 'product',
-    'depends': [
-        'sale',
+    "name": "Product Profile",
+    "version": "10.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "summary": "Allow to configure a product in 1 click",
+    "category": "product",
+    "depends": ["sale"],
+    "website": "http://www.akretion.com/",
+    "data": [
+        "security/group.xml",
+        "views/product_view.xml",
+        "views/config_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'website': 'http://www.akretion.com/',
-    'data': [
-        'security/group.xml',
-        'views/product_view.xml',
-        'views/config_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'demo': [
-        'demo/product.profile.csv',
-    ],
-    'installable': True,
-    'license': 'AGPL-3',
+    "demo": ["demo/product.profile.csv"],
+    "installable": True,
+    "license": "AGPL-3",
 }
