1ee3bbc80b431c9fd6e4a5f783d5d8bb226dc0c4
Kevin Khao <kevinkhao@gmail.com>
2020-03-06 16:42:23 +0100

    [12.0][ADD] product_stock_state: reformat for OCA, misc. improvements and cleanup

--- product_stock_state/models/__init__.py
+++ product_stock_state/models/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 from . import res_company
-from . import sale_config_settings
+from . import res_config_settings
 from . import product_category
 from . import product_template
 from . import product_product
