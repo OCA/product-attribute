dd9e1af701af7135fb11cee620f71bbafd56962e
emagdalena <emagdalena@c2i.es>
2020-03-07 16:07:55 +0100

    [FIX] rename length to product_length

--- product_dimension/__manifest__.py
+++ product_dimension/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product Dimension',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': 'Product',
     'author': 'brain-tec AG, ADHOC SA, Camptocamp SA, '
               'Odoo Community Association (OCA)',
