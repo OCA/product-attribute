aebafaf4a4cb9818be80b86a2e2af0f2134ebb4e
emagdalena <emagdalena@c2i.es>
2020-03-07 16:07:55 +0100

    [FIX] rename length to product_length

---
+++ product_dimension/migrations/12.0.2.0.0/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2019 C2i Change 2 improve - Eduardo Magdalena <emagdalena@c2i.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+field_renames = [
+    ('product.template', 'product_template', 'length', 'product_length'),
+    ('product.template', 'product_template', 'heigth', 'product_heigth'),
+    ('product.template', 'product_template', 'width', 'product_width'),
+]
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    openupgrade.rename_fields(env, field_renames)
