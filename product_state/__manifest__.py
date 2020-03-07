87a8109d0d64b91eb954b82df55ba8971feedfea
emagdalena <emagdalena@c2i.es>
2020-03-07 15:39:56 +0100

    [IMP] product_state

--- product_state/__manifest__.py
+++ product_state/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Product State",
-    'summary': """
+    "name": "Product State",
+    "summary": """
         Module introducing a state field on product template""",
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "https://github.com/OCA/product-attribute",
@@ -11,9 +11,14 @@
     'license': 'AGPL-3',
     'depends': [
         'product',
+        'sale',
     ],
     'data': [
+        'data/product_state_data.xml',
+        'security/ir.model.access.csv',
         'views/product_template.xml',
     ],
     'application': True,
+    "maintainers": ["emagdalenaC2i"],
+    "post_init_hook": "post_init_hook",
 }
