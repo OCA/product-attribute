a8363148cd41b891c976ff84bddf811924c44f75
emagdalena <emagdalena@c2i.es>
2020-03-07 15:39:56 +0100

    [IMP] product_state

--- product_state/__init__.py
+++ product_state/__init__.py
@@ -1,3 +1,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
+
+
+from odoo import api, SUPERUSER_ID
+
+
+def post_init_hook(cr, registry):
+    """ This hook is used to add a state on existing products
+    when module product_state is installed.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    product_without_state = env["product.template"].search(
+        [("product_state_id", "=", False), ("state", "!=", False),]
+    )
+    product_without_state._inverse_product_state()
