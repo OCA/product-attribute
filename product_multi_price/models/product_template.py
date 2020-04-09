07811bc94aa5d5f4c2940951d004ea3a585201cb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-09 10:56:00 +0200

    [IMP] product_multi_price: Several improvements
    * Adjusted terminology
    * Better positioning of the prices list
    * Adjusted README
    * Remove useless constraint
    * Make multi price name translatable
    * Apply some guidelines
    * Remove compute proxy field as not finally used, and thus the ORM cache, but
      needed then to override initial computation.

--- product_multi_price/models/product_template.py
+++ product_multi_price/models/product_template.py
@@ -1,7 +1,6 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
-from odoo.addons import decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
@@ -10,13 +9,8 @@ class ProductTemplate(models.Model):
     price_ids = fields.One2many(
         comodel_name='product.multi.price',
         compute='_compute_price_ids',
-        inverse='_set_price_ids',
-    )
-    multi_price = fields.Float(
-        digits=dp.get_precision('Product Price'),
-        compute='_compute_multi_price',
-        groups="base.group_user",
-        readonly=True,
+        inverse='_inverse_price_ids',
+        string="Other Prices",
     )
 
     @api.depends('product_variant_ids',
@@ -26,7 +20,7 @@ class ProductTemplate(models.Model):
             if len(p.product_variant_ids) == 1:
                 p.price_ids = p.product_variant_ids.price_ids
 
-    def _set_price_ids(self):
+    def _inverse_price_ids(self):
         for p in self:
             if len(p.product_variant_ids) == 1:
                 p.product_variant_ids.price_ids = p.price_ids
@@ -37,18 +31,6 @@ class ProductTemplate(models.Model):
                 self.product_variant_ids._get_multiprice_pricelist_price(rule))
         return 0
 
-    @api.depends('product_variant_ids', 'product_variant_ids.multi_price')
-    def _compute_multi_price(self):
-        """Use multi_price field as the frontend for any of the registered
-           multi price fields on the product passing it by context"""
-        multi_price_field = self.env.context.get('multi_price_field', False)
-        if not multi_price_field:
-            self.update({'multi_price': 0})
-            return
-        for p in self:
-            if len(p.product_variant_ids) == 1:
-                p.multi_price = p.product_variant_ids.multi_price
-
     @api.model
     def create(self, vals):
         product = super().create(vals)
@@ -57,3 +39,14 @@ class ProductTemplate(models.Model):
                 'price_ids': vals.get('price_ids'),
             })
         return product
+
+    def price_compute(self, price_type, uom=False, currency=False,
+                      company=False):
+        """Return temporary prices when computation is done for multi price for
+        avoiding error on super method. We will later fill these with the
+        correct values.
+        """
+        if price_type == 'multi_price':
+            return dict.fromkeys(self.ids, 1.0)
+        return super().price_compute(
+            price_type, uom=uom, currency=currency, company=company)
