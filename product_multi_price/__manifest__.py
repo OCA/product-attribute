f0932ab40c06e2b08a2bb1313aa53633e5cc58a5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-09 10:56:00 +0200

    [IMP] product_multi_price: Several improvements
    * Adjusted terminology
    * Better positioning of the prices list
    * Adjusted README
    * Remove useless constraint
    * Make multi price name translatable
    * Apply some guidelines
    * Remove compute proxy field as not finally used, and thus the ORM cache, but
      needed then to override initial computation.

--- product_multi_price/__manifest__.py
+++ product_multi_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product Multi Price",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/product-attribute',
--- product_multi_price/models/product_multi_price.py
+++ product_multi_price/models/product_multi_price.py
@@ -1,7 +1,6 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import _, api, fields, models, tools
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
 
@@ -11,6 +10,7 @@ class ProductMultiPrice(models.Model):
     name = fields.Many2one(
         comodel_name='product.multi.price.name',
         required=True,
+        translate=True,
     )
     product_id = fields.Many2one(
         comodel_name='product.product',
@@ -56,43 +56,3 @@ class ProductMultiPriceName(models.Model):
         ('multi_price_name_uniq', 'unique(name, company_id)',
          'Prices Names must be unique per company'),
     ]
-
-    @api.model
-    @tools.ormcache()
-    def _get_field_names(self):
-        return set([x.name for x in self.search([])])
-
-    @api.model
-    def create(self, vals):
-        res = super().create(vals)
-        self.clear_caches()
-        return res
-
-    def write(self, vals):
-        res = super().write(vals)
-        if 'name' in vals:
-            self.clear_caches()
-        return res
-
-    def unlink(self):
-        res = super().unlink()
-        self.clear_caches()
-        return res
-
-    @api.constrains('name')
-    def _check_name(self):
-        """The target is to use multi price as if they were virtual fields
-           so we want to constrain the naming to the same rules"""
-        product_fields = list(self.env['product.product']._fields)
-        product_fields += list(self.env['product.template']._fields)
-        for field in self:
-            try:
-                models.check_pg_name(field.name)
-            except ValidationError:
-                msg = _(
-                    "Field names can only contain characters, "
-                    "digits and underscores (up to 63).")
-                raise ValidationError(msg)
-            if field in set(product_fields):
-                raise ValidationError(_(
-                    "The field name is used in the model, try another"))
