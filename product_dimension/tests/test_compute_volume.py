1020730d4ab8ad141a15ec3ba3c6c5fc9d9915c6
Emīls Goško <emils.gosko@gmail.com>
2020-04-15 10:37:26 +0300

    [IMP] product_dimension: black, isort, prettier

--- product_dimension/tests/test_compute_volume.py
+++ product_dimension/tests/test_compute_volume.py
@@ -4,64 +4,50 @@ from odoo.tests.common import TransactionCase
 
 
 class TestComputeVolumeOnProduct(TransactionCase):
-
     def test_it_computes_volume_in_cm(self):
-        self.product.product_length = 10.
-        self.product.product_height = 200.
-        self.product.product_width = 100.
+        self.product.product_length = 10.0
+        self.product.product_height = 200.0
+        self.product.product_width = 100.0
         self.product.dimensional_uom_id = self.uom_cm
         self.product.onchange_calculate_volume()
-        self.assertAlmostEqual(
-            0.2,
-            self.product.volume
-        )
+        self.assertAlmostEqual(0.2, self.product.volume)
 
     def test_it_computes_volume_in_meters(self):
-        self.product.product_length = 6.
-        self.product.product_height = 2.
-        self.product.product_width = 10.
+        self.product.product_length = 6.0
+        self.product.product_height = 2.0
+        self.product.product_width = 10.0
         self.product.dimensional_uom_id = self.uom_m
         self.product.onchange_calculate_volume()
-        self.assertAlmostEqual(
-            120,
-            self.product.volume
-        )
+        self.assertAlmostEqual(120, self.product.volume)
 
     def setUp(self):
         super(TestComputeVolumeOnProduct, self).setUp()
 
-        self.product = self.env['product.product'].new()
-        self.uom_m = self.env['uom.uom'].search([('name', '=', 'm')])
-        self.uom_cm = self.env['uom.uom'].search([('name', '=', 'cm')])
+        self.product = self.env["product.product"].new()
+        self.uom_m = self.env["uom.uom"].search([("name", "=", "m")])
+        self.uom_cm = self.env["uom.uom"].search([("name", "=", "cm")])
 
 
 class TestComputeVolumeOnTemplate(TransactionCase):
-
     def test_it_computes_volume_in_cm(self):
-        self.template.product_length = 10.
-        self.template.product_height = 200.
-        self.template.product_width = 100.
+        self.template.product_length = 10.0
+        self.template.product_height = 200.0
+        self.template.product_width = 100.0
         self.template.dimensional_uom_id = self.uom_cm
         self.template.onchange_calculate_volume()
-        self.assertAlmostEqual(
-            0.2,
-            self.template.volume
-        )
+        self.assertAlmostEqual(0.2, self.template.volume)
 
     def test_it_computes_volume_in_meters(self):
-        self.template.product_length = 6.
-        self.template.product_height = 2.
-        self.template.product_width = 10.
+        self.template.product_length = 6.0
+        self.template.product_height = 2.0
+        self.template.product_width = 10.0
         self.template.dimensional_uom_id = self.uom_m
         self.template.onchange_calculate_volume()
-        self.assertAlmostEqual(
-            120,
-            self.template.volume
-        )
+        self.assertAlmostEqual(120, self.template.volume)
 
     def setUp(self):
         super(TestComputeVolumeOnTemplate, self).setUp()
 
-        self.template = self.env['product.template'].new()
-        self.uom_m = self.env['uom.uom'].search([('name', '=', 'm')])
-        self.uom_cm = self.env['uom.uom'].search([('name', '=', 'cm')])
+        self.template = self.env["product.template"].new()
+        self.uom_m = self.env["uom.uom"].search([("name", "=", "m")])
+        self.uom_cm = self.env["uom.uom"].search([("name", "=", "cm")])
