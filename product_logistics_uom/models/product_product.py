cd6ee77cfdfd17b0138456fb2a3d7ea4568729a0
hparfr <raph@hpar.fr>
2020-04-28 15:12:00 +0200

    Add module product_logistics_uom

---
+++ product_logistics_uom/models/product_product.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Akretion (https://www.akretion.com).
+# @author Raphaël Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    volume = fields.Float(
+        help="The volume of the contents, not including any packaging, etc."
+    )
