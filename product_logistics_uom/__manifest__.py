68863c878d2a727661e001ffa9a84cdbee0777fe
hparfr <raph@hpar.fr>
2020-04-28 15:12:00 +0200

    Add module product_logistics_uom

---
+++ product_logistics_uom/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Akretion (https://www.akretion.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product logistics UoM",
+    "summary": "Configure product weights and volume UoM",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": " Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["hparfr"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/res_config_settings.xml",
+        "views/product.xml",
+    ],
+}
