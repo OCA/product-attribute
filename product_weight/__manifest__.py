db43cbb0ddde5332fc7f05496276becbcccaa3ce
Tony Galmiche <tony.galmiche@infosaone.com>
2020-05-23 10:12:27 +0200

    [IMP] product_weight: black, isort, prettier

--- product_weight/__manifest__.py
+++ product_weight/__manifest__.py
@@ -3,19 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Product Weight Calculation',
-    'version': '12.0.1.0.0',
-    'author': 'Savoir-faire Linux,Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-attribute',
-    'license': 'AGPL-3',
-    'category': 'Warehouse',
-    'summary': 'Allows to calculate products weight from its components.',
-    'depends': [
-        'mrp',
-    ],
-    'data': [
-        'wizard/product_weight_update_view.xml',
-        'views/product_view.xml',
-    ],
-    'installable': True,
+    "name": "Product Weight Calculation",
+    "version": "13.0.1.0.0",
+    "author": "Savoir-faire Linux,Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "category": "Warehouse",
+    "summary": "Allows to calculate products weight from its components.",
+    "depends": ["mrp"],
+    "data": ["wizard/product_weight_update_view.xml", "views/product_view.xml"],
+    "installable": True,
 }
