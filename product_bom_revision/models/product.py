abae6e6ec5a04a28f43c9549fd9ba23b287820eb
emagdalena <emagdalena@c2i.es>
2020-05-23 02:29:40 +0200

    [IMP] product_bom_revision: black, isort, prettier

--- product_bom_revision/models/product.py
+++ product_bom_revision/models/product.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     product_revision = fields.Char(string="Revision")
