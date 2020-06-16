0bbe31986ac9755fd58c64953b7763ce90f6e05b
Marc Poch <mpoch@planetatic.com>
2020-06-16 18:09:46 +0200

    [FIX] product_application_custom_info: Application name is computed Application name is a concatenation of its values.

--- product_application_custom_info/models/product_application.py
+++ product_application_custom_info/models/product_application.py
@@ -1,7 +1,7 @@
 # Copyright 2020 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductApplication(models.Model):
@@ -12,3 +12,18 @@ class ProductApplication(models.Model):
         context={"default_model": _name})
     custom_info_ids = fields.One2many(
         context={"default_model": _name})
+
+    name = fields.Char(
+        string='Application Name',
+        compute='_get_application_name',
+        required=False
+    )
+
+    @api.model
+    def _get_application_name(self):
+        for app in self:
+            items = []
+            for tmpl in app.custom_info_template_id:
+                for prop in tmpl.property_ids:
+                    items.append(str(prop.default_value))
+            app.name = ' | '.join(items)
