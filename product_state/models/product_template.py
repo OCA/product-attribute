7305bcb71401be3f73fa89d9fa1e5683ba59bd4d
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-17 16:13:01 +0200

    [CHG] some code cleaning to make it v13 compliant

--- product_state/models/product_template.py
+++ product_state/models/product_template.py
@@ -1,47 +1,12 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields
-
-
-class ProductState(models.Model):
-    _name = "product.state"
-    _description = "Product State"
-    _order = "sequence, id"
-
-    name = fields.Char("State Name", required=True, translate=True)
-    code = fields.Char("State Code", required=True)
-    sequence = fields.Integer("Sequence", help="Used to order the States", default=25)
-    description = fields.Text(translate=True)
-    product_ids = fields.One2many(
-        "product.template", "product_state_id", string="State Products",
-    )
-    products_count = fields.Integer(
-        string="Number of products", compute="_compute_products_count",
-    )
-
-    _sql_constraints = [
-        ("code_unique", "UNIQUE(code)", "Product State Code must be unique.")
-    ]
-
-    @api.depends("product_ids")
-    def _compute_products_count(self):
-        data = self.env["product.template"].read_group(
-            [("product_state_id", "in", self.ids)],
-            ["product_state_id"],
-            ["product_state_id"],
-        )
-        mapped_data = {
-            record["product_state_id"][0]: record["product_state_id_count"]
-            for record in data
-        }
-        for state in self:
-            state.products_count = mapped_data.get(state.id, 0)
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
 
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     state = fields.Char(
         string="Product Status",
@@ -54,6 +19,7 @@ class ProductTemplate(models.Model):
         comodel_name="product.state",
         string="State",
         help="Select a state for this product",
+        group_expand="_read_group_state_id",
     )
 
     @api.depends("product_state_id")
@@ -70,3 +36,7 @@ class ProductTemplate(models.Model):
             if product_tmpl.state and not product_state:
                 product_state = ProductState.create({"name": product_tmpl.state})
             product_tmpl.product_state_id = product_state.id
+
+    @api.model
+    def _read_group_state_id(self, states, domain, order):
+        return states.search([])
