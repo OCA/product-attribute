a5a5a241b3facfa9b44e749f3db8f0721d12172c
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-17 16:13:01 +0200

    [CHG] some code cleaning to make it v13 compliant

--- product_state/__manifest__.py
+++ product_state/__manifest__.py
@@ -4,21 +4,19 @@
     "name": "Product State",
     "summary": """
         Module introducing a state field on product template""",
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': "https://github.com/OCA/product-attribute",
-    'category': 'Product',
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
-        'sale',
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "category": "Product",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["product", "sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/product_state_data.xml",
+        "security/ir.model.access.csv",
+        "views/product_template.xml",
     ],
-    'data': [
-        'data/product_state_data.xml',
-        'security/ir.model.access.csv',
-        'views/product_template.xml',
-    ],
-    'application': True,
+    "application": True,
     "maintainers": ["emagdalenaC2i"],
     "post_init_hook": "post_init_hook",
 }
