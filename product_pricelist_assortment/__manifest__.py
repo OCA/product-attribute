b9f715fd41a2734df8abae73e8337d4754950555
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-17 11:59:00 +0200

    [ADD] product_pricelist_assortment

---
+++ product_pricelist_assortment/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Pricelist Assortment",
+    "summary": """
+        Product assortment and pricelist""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product_assortment"],
+    "data": [
+        "security/product_pricelist_assortment_item.xml",
+        "data/ir_cron.xml",
+        "views/product_pricelist.xml",
+        "views/product_pricelist_assortment_item.xml",
+        "views/product_pricelist_item.xml",
+    ],
+    "demo": [],
+}
---
+++ product_pricelist_assortment/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Denis Roussel <denis.roussel@acsone.eu>
+* Cédric Pigeon <cedric.pigeon@acsone.eu>
+* François Honoré <francois.honore@acsone.eu>
