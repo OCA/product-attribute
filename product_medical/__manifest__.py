6b57a4dded8222d64402894517a9faf974caeb0d
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-06-23 11:55:26 +0300

    [ADD] product_medical: create

---
+++ product_medical/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Iryna Vyshnevska,Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Medical",
+    "summary": "Base  structure to handle medical products",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "category": "Product",
+    "depends": ["product"],
+    "data": [
+        "data/medical_data.xml",
+        "security/ir.model.access.csv",
+        "views/product_template.xml",
+    ],
+    "installable": True,
+}
