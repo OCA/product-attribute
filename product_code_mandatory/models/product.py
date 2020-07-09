9c509b9cba898c11bd53cafd457ab566e00d40f0
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-07-09 12:46:30 +0200

    [IMP] product_code_mandatory: black, isort, prettier

--- product_code_mandatory/models/product.py
+++ product_code_mandatory/models/product.py
@@ -5,10 +5,11 @@ from odoo import fields, models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     def _get_default_code(self):
-        return self.env['ir.sequence'].next_by_code('product.default.code')
+        return self.env["ir.sequence"].next_by_code("product.default.code")
 
-    default_code = fields.Char('Internal Reference', index=True,
-                               default=_get_default_code)
+    default_code = fields.Char(
+        "Internal Reference", index=True, default=_get_default_code
+    )
