48e78b6c6912bff5b641304626fe5f2f751ef529
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-07-16 10:45:26 +0200

    Create module product_total_weight_from_packaging

---
+++ product_total_weight_from_packaging/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Product Total Weight From Packaging",
+    "summary": "Compute estimated weight based on product's packaging weights",
+    "version": "13.0.1.3.0",
+    "development_status": "Alpha",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["stock_packaging_calculator", "product_packaging_dimension"],
+}
