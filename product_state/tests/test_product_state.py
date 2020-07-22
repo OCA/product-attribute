ca10faef873e0482ebcdba6401d6151dc88867a6
c2cdidier <didier.donze@camptocamp.com>
2020-07-22 11:17:20 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: product-attribute-13.0/product-attribute-13.0-product_state
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-13-0/product-attribute-13-0-product_state/fr/
    
    Translated using Weblate (French)
    
    Currently translated at 55.2% (16 of 29 strings)
    
    Translation: product-attribute-13.0/product-attribute-13.0-product_state
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-13-0/product-attribute-13-0-product_state/fr/

---
+++ product_state/tests/test_product_state.py
@@ -0,0 +1,21 @@
+from odoo.tests.common import SavepointCase
+
+
+class TestProductState(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.ProductState = cls.env["product.state"]
+        cls.state = cls.ProductState.create(
+            {
+                "name": "State Name",
+                "code": "Code"
+            }
+        )
+
+    def test_01_product_state(self):
+        print(self.state.products_count)
+
+    
+
+
