194141f4c8f13336c8e8bd96c904ee32a3062b45
Denis Roussel <denis.roussel@acsone.eu>
2020-07-30 10:53:31 +0200

    [10.0][ADD] product_state_history

---
+++ product_state_history/models/product_template.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    state = fields.Selection(
+        track_visibility="onchange",
+        inverse="_inverse_state_history",
+    )
+
+    @api.multi
+    def _prepare_product_state_history_values(self):
+        self.ensure_one()
+        vals = {
+            "product_template_id": self.id,
+            "product_state": self.state,
+        }
+        return vals
+
+    @api.multi
+    def _inverse_state_history(self):
+        history_obj = self.env["product.state.history"]
+        for template in self:
+            history_obj.create(
+                template._prepare_product_state_history_values())
+
+    @api.multi
+    def action_product_state_history(self):
+        action = self.env.ref(
+            'product_state_history.product_state_history_act_window')
+        result = action.read()[0]
+        result.update({
+            "domain": [("product_template_id", "in", self.ids)]
+        })
+
+        return result
---
+++ product_state_history/models/report_product_state_history.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ReportProductStateHistory(models.AbstractModel):
+
+    _name = 'report.product_state_history.report_product_state_history'
+
+    @api.model
+    def render_html(self, docids, data=None):
+        data = data if data is not None else {}
+        history_ids = self.env['product.state.history'].browse(
+            data.get('ids', docids))
+        docargs = {
+            'doc_ids': data.get('ids', data.get('active_ids')),
+            'doc_model': 'product.state.history',
+            'docs': history_ids,
+            'data': dict(
+                data,
+            ),
+        }
+        return self.env['report'].render(
+            'product_state_history.report_product_state_history', docargs)
