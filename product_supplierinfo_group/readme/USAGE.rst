0c8e5fd0bedf4325ae5bc91877a641a03ffab726
Kevin Khao <kevinkhao@gmail.com>
2020-08-20 18:08:08 +0200

    [12.0][ADD] product_supplierinfo_group

---
+++ product_supplierinfo_group/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Go to the Product page, under the Purchase tab. Enter product prices with the new interface. It is mostly the same as before,
+except you don't need to repeat information on all price lines.
---
+++ product_supplierinfo_group/tests/test_product_supplierinfo_group.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Akretion France (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+from copy import deepcopy
+
+
+class TestProductSupplierinfoGroup(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product_sofa = cls.env.ref("product.consu_delivery_01_product_template")
+        cls.vendor_gemini = cls.env.ref("base.res_partner_3")
+        cls.supplierinfo_vals = {
+            "name": cls.vendor_gemini.id,
+            "product_tmpl_id": cls.product_sofa.id,
+            "product_name": "aProductName",
+            "product_code": "aProductCode",
+            "min_qty": 5.0,
+            "price": 10.0,
+            "delay": 1,
+        }
+
+    def test_no_group(self):
+        """
+        If we try to create a supplierinfo and there is no group yet,
+        create a group
+        """
+        group_before = self.env["product.supplierinfo.group"].search([])
+        self.env["product.supplierinfo"].create(self.supplierinfo_vals)
+        group = self.env["product.supplierinfo.group"].search(
+            [("id", "not in", group_before.ids)]
+        )
+        self.assertTrue(group)
+
+    def test_has_group(self):
+        """
+        If we try to create a supplierinfo and there is already a group,
+        just add a new line to that group
+        """
+        group_before = self.env["product.supplierinfo.group"].search([])
+        self.env["product.supplierinfo"].create(
+            [self.supplierinfo_vals, self.supplierinfo_vals]
+        )
+        group = self.env["product.supplierinfo.group"].search(
+            [("id", "not in", group_before.ids)]
+        )
+        self.assertEqual(len(group.ids), 1)
+        self.assertEqual(len(group.supplierinfo_ids.ids), 2)
+
+    def test_price_note(self):
+        """
+        Test our price note (Char field display to inform user) is correct
+        """
+        group_before = self.env["product.supplierinfo.group"].search([])
+        self.env["product.supplierinfo"].create([self.supplierinfo_vals])
+        group = self.env["product.supplierinfo.group"].search(
+            [("id", "not in", group_before.ids)]
+        )
+        self.assertEqual(
+            ">= 5.0 : 10.0", group.unit_price_note,
+        )
+        min_50 = deepcopy(self.supplierinfo_vals)
+        min_50.update({"min_qty": 50.0, "price": 8.0})
+        min_500 = deepcopy(self.supplierinfo_vals)
+        min_500.update({"min_qty": 500.0, "price": 6.0})
+        # randomize order of supplierinfo prices/qties
+        self.env["product.supplierinfo"].create([min_500, min_50])
+        self.assertEqual(
+            "5.0 - 49.999 : 10.0\n50.0 - 499.999 : 8.0\n>= 500.0 : 6.0",
+            group.unit_price_note,
+        )
