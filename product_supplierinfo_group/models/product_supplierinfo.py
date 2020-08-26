d539a243aa1ee9a410d67b0653b818a6b07cc4d2
Kevin Khao <kevinkhao@gmail.com>
2020-08-26 11:16:58 +0200

    [IMP] rename "name" to "partner_id" on product_supplierinfo_group

--- product_supplierinfo_group/models/product_supplierinfo.py
+++ product_supplierinfo_group/models/product_supplierinfo.py
@@ -4,22 +4,24 @@
 from odoo import api, fields, models
 from copy import deepcopy
 
-FIELDS_RELATED = [
-    "product_tmpl_id",
-    "name",
-    "product_id",
-    "product_name",
-    "product_code",
-    "sequence",
-]
+# format: field_from_supplierinfo:field_from_group
 
-FIELDS_MATCH_GROUP = [
-    "product_tmpl_id",
-    "name",
-    "product_id",
-    "product_name",
-    "product_code",
-]
+MAPPING_RELATED = {
+    "product_tmpl_id": "product_tmpl_id",
+    "name": "partner_id",
+    "product_id": "product_id",
+    "product_name": "product_name",
+    "product_code": "product_code",
+    "sequence": "sequence",
+}
+
+MAPPING_MATCH_GROUP = {
+    "product_tmpl_id": "product_tmpl_id",
+    "name": "partner_id",
+    "product_id": "product_id",
+    "product_name": "product_name",
+    "product_code": "product_code",
+}
 
 
 class ProductSupplierinfo(models.Model):
@@ -27,18 +29,24 @@ class ProductSupplierinfo(models.Model):
 
     supplierinfo_group_id = fields.Many2one("product.supplierinfo.group", required=True)
     product_tmpl_id = fields.Many2one(related="supplierinfo_group_id.product_tmpl_id")
-    name = fields.Many2one(related="supplierinfo_group_id.name")
+    name = fields.Many2one(related="supplierinfo_group_id.partner_id")
     product_id = fields.Many2one(related="supplierinfo_group_id.product_id")
     product_name = fields.Char(related="supplierinfo_group_id.product_name")
     product_code = fields.Char(related="supplierinfo_group_id.product_code")
     sequence = fields.Integer(related="supplierinfo_group_id.sequence")
 
     def _find_or_create_supplierinfo_group(self, vals):
-        domain = [(field, "=", vals.get(field)) for field in FIELDS_MATCH_GROUP]
+        domain = [
+            (field_group, "=", vals.get(field_supplierinfo))
+            for field_supplierinfo, field_group in MAPPING_MATCH_GROUP.items()
+        ]
         group = self.env["product.supplierinfo.group"].search(domain)
         if not group:
             group = self.env["product.supplierinfo.group"].create(
-                {field: vals.get(field) for field in FIELDS_MATCH_GROUP}
+                {
+                    field_group: vals.get(field_supplierinfo)
+                    for field_supplierinfo, field_group in MAPPING_MATCH_GROUP.items()
+                }
             )
         return group
 
@@ -48,9 +56,9 @@ class ProductSupplierinfo(models.Model):
             new_val.get("supplierinfo_group_id")
         ) or self._find_or_create_supplierinfo_group(new_val)
         new_val["supplierinfo_group_id"] = group.id
-        for field in FIELDS_RELATED:
-            if field in new_val:
-                del new_val[field]
+        for field_supplierinfo in MAPPING_RELATED.keys():
+            if field_supplierinfo in new_val:
+                del new_val[field_supplierinfo]
         return new_val
 
     @api.model_create_multi
