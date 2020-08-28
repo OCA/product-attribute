10255c0fac5b6a01d313667af65ca49dfc43d911
Kevin Khao <kevinkhao@gmail.com>
2020-08-28 13:46:58 +0200

    [IMP] GPL->AGPL, exclude incompatible module product_supplierinfo_for_customer

--- product_supplierinfo_group/models/product_supplierinfo_group.py
+++ product_supplierinfo_group/models/product_supplierinfo_group.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2015-Today GRAP (http://www.grap.coop)
 # Copyright (C) 2020-Today Akretion (http://www.akretion.com)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License GPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
