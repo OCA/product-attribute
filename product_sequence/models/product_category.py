4ff3a414af902c1671e80941efc10f18c1c25c23
mreficent <miquel.raich@eficent.com>
2020-10-01 12:56:45 +0200

    [UPD] Eficent -> ForgeFlow

--- product_sequence/models/product_category.py
+++ product_sequence/models/product_category.py
@@ -1,5 +1,5 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2018 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
