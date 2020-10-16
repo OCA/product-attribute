b23c28b56db8cba92c173001d7062889c8343eb5
Quentin Groulard <quentin.groulard@acsone.eu>
2020-10-16 14:27:09 +0200

    [IMP] product_multi_price: black, isort, prettier

--- product_multi_price/models/product_template.py
+++ product_multi_price/models/product_template.py
@@ -7,14 +7,13 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     price_ids = fields.One2many(
-        comodel_name='product.multi.price',
-        compute='_compute_price_ids',
-        inverse='_inverse_price_ids',
+        comodel_name="product.multi.price",
+        compute="_compute_price_ids",
+        inverse="_inverse_price_ids",
         string="Other Prices",
     )
 
-    @api.depends('product_variant_ids',
-                 'product_variant_ids.price_ids')
+    @api.depends("product_variant_ids", "product_variant_ids.price_ids")
     def _compute_price_ids(self):
         for p in self:
             if len(p.product_variant_ids) == 1:
@@ -27,8 +26,7 @@ class ProductTemplate(models.Model):
 
     def _get_multiprice_pricelist_price(self, rule):
         if len(self.product_variant_ids) == 1:
-            return (
-                self.product_variant_ids._get_multiprice_pricelist_price(rule))
+            return self.product_variant_ids._get_multiprice_pricelist_price(rule)
         return 0
 
     @api.model
@@ -37,19 +35,17 @@ class ProductTemplate(models.Model):
         one variant), after the variant creation, that is performed in super.
         """
         template = super().create(vals)
-        if vals.get('price_ids'):
-            template.write({
-                'price_ids': vals.get('price_ids'),
-            })
+        if vals.get("price_ids"):
+            template.write({"price_ids": vals.get("price_ids")})
         return template
 
-    def price_compute(self, price_type, uom=False, currency=False,
-                      company=False):
+    def price_compute(self, price_type, uom=False, currency=False, company=False):
         """Return temporary prices when computation is done for multi price for
         avoiding error on super method. We will later fill these with the
         correct values.
         """
-        if price_type == 'multi_price':
+        if price_type == "multi_price":
             return dict.fromkeys(self.ids, 1.0)
         return super().price_compute(
-            price_type, uom=uom, currency=currency, company=company)
+            price_type, uom=uom, currency=currency, company=company
+        )
