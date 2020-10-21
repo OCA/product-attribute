d8193b833ec94758538dc8b0d4b988bfe3fa1601
Michael Michot <michael.michot@pharmasimple.com>
2020-10-21 13:47:03 +0200

    [IMP] add unicity constraint on name and company_id

--- product_template_tags/tests/test_product_template_tags.py
+++ product_template_tags/tests/test_product_template_tags.py
@@ -1,7 +1,10 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from psycopg2 import IntegrityError
+
 from odoo.tests.common import SavepointCase
+from odoo.tools import mute_logger
 
 
 class TestProductTemplateTagBase(SavepointCase):
@@ -19,3 +22,19 @@ class TestProductTemplateTag(TestProductTemplateTagBase):
         )
         product_tmpl_tag._compute_products_count()
         self.assertEqual(product_tmpl_tag.products_count, 1)
+
+    def test_product_template_tag_uniq(self):
+        product_tmpl_tag = self.env["product.template.tag"].create({"name": "Test Tag"})
+        self.assertTrue(product_tmpl_tag)
+        # test same tag and same company
+        with mute_logger("odoo.sql_db"):
+            with self.assertRaises(IntegrityError):
+                with self.cr.savepoint():
+                    self.env["product.template.tag"].create({"name": "Test Tag"})
+
+        # test same tag and different company
+        company = self.env["res.company"].create({"name": "Test"})
+        same_product_tmpl_tag_diff_company = self.env["product.template.tag"].create(
+            {"name": "Test Tag", "company_id": company.id}
+        )
+        self.assertTrue(same_product_tmpl_tag_diff_company)
