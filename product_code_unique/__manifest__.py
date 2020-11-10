251018b4c3ee029e2fcf5df492737fcf00b2f161
watthanun <watthanun_t@hotmail.com>
2020-11-10 10:26:27 +0700

    [MIG] product_code_unique: Migration to 14.0

--- product_code_unique/__manifest__.py
+++ product_code_unique/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Unique Product Internal Reference",
     "summary": "Set Product Internal Reference as Unique",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Product",
