f3a981bc2fab4cb71d23dc4d7d24b85ae3f86e93
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-11-11 19:11:17 -0600

    [MIG] product_secondary_unit: Migration to 14.0

--- product_secondary_unit/__manifest__.py
+++ product_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product Secondary Unit",
     "summary": "Set a secondary unit per product",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
