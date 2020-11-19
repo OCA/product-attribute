3cef54e19a3edf0cf1ffb52906b0c7e4bc76851f
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2020-11-19 14:00:43 +0100

    import module product_uom_updatable from external source

---
+++ product_uom_updatable/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_product_uom_update
---
+++ product_uom_updatable/tests/test_product_uom_update.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Acsone SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestProductUomUpdate(TransactionCase):
+    def setUp(self):
+        super(TestProductUomUpdate, self).setUp()
+        self.uom_unit = self.env.ref("product.product_uom_unit")
+        self.product = self.env.ref("product.product_delivery_01")
+        self.product_tmpl_id = self.ref("product.product_delivery_01_product_template")
+        self.partner_id = self.ref("base.res_partner_4")
+        self.picking_type_id = self.ref("stock.picking_type_in")
+        self.location_id = self.ref("stock.stock_location_suppliers")
+        self.location_dest_id = self.ref("stock.stock_location_stock")
+
+    def test_update_uom(self):
+        self.picking_in = self.env["stock.picking"].create(
+            {
+                "picking_type_id": self.picking_type_id,
+                "partner_id": self.partner_id,
+                "location_id": self.location_id,
+                "location_dest_id": self.location_dest_id,
+            }
+        )
+
+        self.env["stock.move"].create(
+            {
+                "name": self.product.name,
+                "product_id": self.product.id,
+                "product_uom_qty": 2,
+                "product_uom": self.product.uom_id.id,
+                "picking_id": self.picking_in.id,
+                "location_id": self.location_id,
+                "location_dest_id": self.location_dest_id,
+            }
+        )
+        self.new_uom = self.env["product.uom"].create(
+            {"name": "new unit", "category_id": self.uom_unit.category_id.id}
+        )
+        # verify that the product has stock_moves
+        self.assertTrue(self.product.stock_move_ids)
+        self.assertEqual(self.product.uom_id, self.uom_unit)
+        self.assertEqual(self.uom_unit.category_id, self.new_uom.category_id)
+        self.assertEqual(self.uom_unit.factor, self.new_uom.factor)
+        # uom is changed with another uom with the same category
+        self.product.update({"uom_id": self.new_uom.id})
+        self.assertEqual(self.product.uom_id, self.new_uom)
+        # uom_po can also be changed with another uom with the same category
+        self.assertEqual(self.product.uom_po_id, self.uom_unit)
+        self.product.update({"uom_po_id": self.new_uom.id})
+        self.assertEqual(self.product.uom_po_id, self.new_uom)
