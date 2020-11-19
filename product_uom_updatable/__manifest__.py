8afc3e02d5244ae520d3867e5cb417d756a1c02a
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2020-11-19 14:00:43 +0100

    import module product_uom_updatable from external source

---
+++ product_uom_updatable/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Acsone SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Uom Updatable",
+    "description": """
+        allows products uom to be modified after be used in a stock picking if the product uom is of the same category""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Acsone SA/NV",
+    "website": "www.acsone.eu",
+    "depends": ["stock"],
+    "data": [],
+    "demo": [],
+}
