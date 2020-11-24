9f61a9082759b88d519d25194fbb44c3ab96baa7
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2020-11-24 11:32:22 +0100

    [MIG][13.0] product_uom_updatable

--- product_uom_updatable/__manifest__.py
+++ product_uom_updatable/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Acsone SA/NV
+# Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product Uom Updatable",
-    "description": """
-        allows products uom to be modified after be used in a stock picking if the product uom is of the same category""",
-    "version": "10.0.1.0.0",
+    "summary": """
+        allows products uom to be modified after be used in a stock picking if \
+         the product uom is of the same category""",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Acsone SA/NV",
-    "website": "www.acsone.eu",
+    "author": "ACSONE SA/NV, Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
     "depends": ["stock"],
-    "data": [],
-    "demo": [],
 }
