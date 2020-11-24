eaead326df0c73208c9f3fd203fe74b371658874
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2020-11-24 11:32:22 +0100

    [MIG][13.0] product_uom_updatable

--- product_uom_updatable/models/product_template.py
+++ product_uom_updatable/models/product_template.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Acsone SA/NV
+# Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from itertools import groupby
 
-from odoo import _, api, models
+from psycopg2 import sql
+
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
@@ -12,58 +14,37 @@ class ProductTemplate(models.Model):
 
     _inherit = "product.template"
 
-    @api.multi
     def write(self, vals):
         uom_id = vals.pop("uom_id", False)
         uom_po_id = vals.pop("uom_po_id", False)
-        res = super(ProductTemplate, self).write(vals)
         if uom_id:
-            self._update_uom(uom_id)
+            self._update_uom(uom_id, "uom_id")
         if uom_po_id:
-            self._update_uom_po(uom_po_id)
+            self._update_uom(uom_po_id, "uom_po_id")
+        res = super().write(vals)
         return res
 
-    @api.multi
-    def _update_uom(self, uom_id):
-        uom_obj = self.env["product.uom"]
-        for key, products_group in groupby(self, key=lambda r: r.uom_id):
-            product_list = list(products_group)
-            product_id_list = []
-            for product in product_list:
-                product_id_list.append(product.id)
+    def _update_uom(self, uom_id, field_name):
+        uom_obj = self.env["uom.uom"]
+        sorted_items = sorted(self, key=lambda r: r[field_name])
+        for key, products_group in groupby(sorted_items, key=lambda r: r[field_name]):
+            product_ids = [product.id for product in products_group]
             new_uom = uom_obj.browse(uom_id)
-            if key.category_id == new_uom.category_id and key.factor == new_uom.factor:
-                self.env.cr.execute(
-                    "UPDATE product_template SET uom_id = %(uom)s WHERE id in %(product_id)s",
-                    {"uom": new_uom.id, "product_id": tuple(product_id_list)},
-                )
-            else:
-                raise UserError(
-                    _(
-                        "You can not change the unit of measure of a product to a new unit that doesn't have the same category and factor"
-                    )
-                )
-
-    @api.multi
-    def _update_uom_po(self, uom_po_id):
-        uom_obj = self.env["product.uom"]
-        for key, products_group in groupby(self, key=lambda r: r.uom_po_id):
-            product_list = list(products_group)
-            product_id_list = []
-            for product in product_list:
-                product_id_list.append(product.id)
-            new_uom_po = uom_obj.browse(uom_po_id)
             if (
-                key.category_id == new_uom_po.category_id
-                and key.factor == new_uom_po.factor
+                key.category_id == new_uom.category_id
+                and key.factor_inv == new_uom.factor_inv
             ):
-                self.env.cr.execute(
-                    "UPDATE product_template SET uom_po_id = %(uom)s WHERE id in %(product_id)s",
-                    {"uom": new_uom_po.id, "product_id": tuple(product_id_list)},
-                )
+                # pylint: disable=sql-injection
+                query = sql.SQL(
+                    "UPDATE product_template SET {field} " " = %s WHERE id in %s"
+                ).format(field=sql.Identifier(field_name))
+                self.env.cr.execute(query, (new_uom.id, tuple(product_ids)))
+                self.invalidate_cache(fnames=[field_name], ids=product_ids)
             else:
                 raise UserError(
                     _(
-                        "You can not change the purchase unit of measure of a product to a new unit that doesn't have the same category and factor"
+                        "You can not change the unit of measure of a product "
+                        "to a new unit that doesn't have the same category "
+                        "and factor"
                     )
                 )
--- product_uom_updatable/tests/test_product_uom_update.py
+++ product_uom_updatable/tests/test_product_uom_update.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Acsone SA/NV
+# Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
+from odoo.tests.common import SavepointCase
 
 
-class TestProductUomUpdate(TransactionCase):
+class TestProductUomUpdate(SavepointCase):
     def setUp(self):
         super(TestProductUomUpdate, self).setUp()
-        self.uom_unit = self.env.ref("product.product_uom_unit")
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        self.uom_unit = self.env.ref("uom.product_uom_unit")
+        self.uom_day = self.env.ref("uom.product_uom_day")
         self.product = self.env.ref("product.product_delivery_01")
-        self.product_tmpl_id = self.ref("product.product_delivery_01_product_template")
+        self.product_tmpl_id = self.env.ref(
+            "product.product_delivery_01_product_template"
+        )
         self.partner_id = self.ref("base.res_partner_4")
         self.picking_type_id = self.ref("stock.picking_type_in")
         self.location_id = self.ref("stock.stock_location_suppliers")
@@ -37,18 +42,42 @@ class TestProductUomUpdate(TransactionCase):
                 "location_dest_id": self.location_dest_id,
             }
         )
-        self.new_uom = self.env["product.uom"].create(
-            {"name": "new unit", "category_id": self.uom_unit.category_id.id}
+        self.new_uom = self.env["uom.uom"].create(
+            {
+                "name": "new unit",
+                "category_id": self.uom_unit.category_id.id,
+                "uom_type": "smaller",
+            }
+        )
+
+        self.new_uom_other_category = self.env["uom.uom"].create(
+            {
+                "name": "new unit 2",
+                "category_id": self.uom_day.category_id.id,
+                "uom_type": "smaller",
+            }
         )
         # verify that the product has stock_moves
         self.assertTrue(self.product.stock_move_ids)
         self.assertEqual(self.product.uom_id, self.uom_unit)
         self.assertEqual(self.uom_unit.category_id, self.new_uom.category_id)
-        self.assertEqual(self.uom_unit.factor, self.new_uom.factor)
+        self.assertEqual(
+            self.uom_day.category_id, self.new_uom_other_category.category_id
+        )
+        self.assertEqual(self.uom_unit.factor_inv, self.new_uom.factor_inv)
+        self.assertEqual(
+            self.uom_day.factor_inv, self.new_uom_other_category.factor_inv
+        )
         # uom is changed with another uom with the same category
-        self.product.update({"uom_id": self.new_uom.id})
-        self.assertEqual(self.product.uom_id, self.new_uom)
+        self.product_tmpl_id.update({"uom_id": self.new_uom.id})
+        self.assertEqual(self.product_tmpl_id.uom_id, self.new_uom)
         # uom_po can also be changed with another uom with the same category
-        self.assertEqual(self.product.uom_po_id, self.uom_unit)
-        self.product.update({"uom_po_id": self.new_uom.id})
-        self.assertEqual(self.product.uom_po_id, self.new_uom)
+        self.assertEqual(self.product_tmpl_id.uom_po_id, self.uom_unit)
+        self.product_tmpl_id.update({"uom_po_id": self.new_uom.id})
+        self.assertEqual(self.product_tmpl_id.uom_po_id, self.new_uom)
+        # uom is changed with another uom from different category
+        with self.assertRaises(UserError):
+            self.product_tmpl_id.update({"uom_id": self.new_uom_other_category.id})
+        # uom_po is changed with another uom from different category
+        with self.assertRaises(UserError):
+            self.product_tmpl_id.update({"uom_po_id": self.new_uom_other_category.id})
