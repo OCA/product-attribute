311b300314d6fda8c6670a1a7eb368b266012b9e
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-12-13 09:59:01 +0100

    [ADD] 12.0 : product_active_propagate : new module to avoid having inconsistent states between the active field of the variant and the active field of the template

---
+++ product_active_propagate/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product - Propagate Active Value',
+    'summary': "Propagate active value from product variants"
+    " to product templates.",
+    'version': "12.0.1.0.1",
+    'category': 'Products',
+    'author': 'GRAP,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/product-attribute',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'installable': True,
+}
