af65a0dc2ba8e4bf43d0b5a804a54862532df848
Carlos Roca <carlos.roca@tecnativa.com>
2020-12-23 08:57:36 +0100

    [IMP] product_assortment: added partners to products assortments

--- product_assortment/__manifest__.py
+++ product_assortment/__manifest__.py
@@ -5,12 +5,12 @@
     "name": "Product Assortment",
     "summary": """
         Adds the ability to manage products assortment""",
-    "version": "13.0.1.0.1",
+    "version": "13.0.2.0.0",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "depends": ["base", "product"],
-    "data": ["views/product_assortment.xml"],
+    "data": ["views/product_assortment.xml", "views/res_partner_view.xml"],
     "installable": True,
 }
--- product_assortment/readme/CONTRIBUTORS.rst
+++ product_assortment/readme/CONTRIBUTORS.rst
@@ -1,2 +1,5 @@
 * Denis Roussel <denis.roussel@acsone.eu>
 * Cédric Pigeon <cedric.pigeon@acsone.eu>
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+  * Carlos Roca
