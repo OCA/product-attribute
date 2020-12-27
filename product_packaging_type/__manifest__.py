ad3e13cc817da3e8652f3cf96caba71e46ae8025
Tran Thanh Phuc <phuc@trobz.com>
2020-12-27 19:14:41 +0700

    [MIG] product_packaging_type: Migration to 14.0

--- product_packaging_type/__manifest__.py
+++ product_packaging_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Packaging Type",
-    "version": "13.0.1.2.1",
+    "version": "14.0.0.1.0",
     "development_status": "Beta",
     "category": "Product",
     "summary": "Product Packaging Type",
