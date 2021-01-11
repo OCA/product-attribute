db31f1b55642f0ff5de5bef65d12c9fbc5cec235
hparfr <raph@hpar.fr>
2021-01-11 17:05:43 +0100

    [MIG] product_manufacturer: Migration to 14.0

--- product_manufacturer/__manifest__.py
+++ product_manufacturer/__manifest__.py
@@ -2,8 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Manufacturer",
-    "version": "13.0.2.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Adds manufacturers and attributes on the product view.",
+    "website": "https://github.com/OCA/product-attribute",
     "author": "OpenERP SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Product",
