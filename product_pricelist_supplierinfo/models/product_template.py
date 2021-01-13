78d449d3193c1a60615a25a602cf8edf03ec864b
teodoralexandru@nexterp.ro <teodoralexandru@nexterp.ro>
2021-01-13 12:18:52 +0200

    [IMP] product_pricelist_supplierinfo: black, isort, prettier

--- product_pricelist_supplierinfo/models/product_template.py
+++ product_pricelist_supplierinfo/models/product_template.py
@@ -7,10 +7,11 @@ from odoo import fields, models, tools
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     def _get_supplierinfo_pricelist_price(
-            self, rule, date=None, quantity=None, product_id=None):
+        self, rule, date=None, quantity=None, product_id=None
+    ):
         """Method for getting the price from supplier info."""
         self.ensure_one()
         price = 0.0
@@ -23,33 +24,31 @@ class ProductTemplate(models.Model):
         # active, so we must ensure variant exists or _select_seller fails.
         if product:
             seller = product._select_seller(
-                partner_id=rule.filter_supplier_id,
-                quantity=quantity,
-                date=date)
+                partner_id=rule.filter_supplier_id, quantity=quantity, date=date
+            )
             if seller:
                 price = seller._get_supplierinfo_pricelist_price()
         if price:
             # We need to convert the price if the pricelist and seller have
             # different currencies so the price have the pricelist currency
             if rule.currency_id != seller.currency_id:
-                convert_date = date or self.env.context.get(
-                    'date', fields.Date.today())
+                convert_date = date or self.env.context.get("date", fields.Date.today())
                 price = seller.currency_id._convert(
-                    price, rule.currency_id, seller.company_id, convert_date)
+                    price, rule.currency_id, seller.company_id, convert_date
+                )
 
             # We have to replicate this logic in this method as pricelist
             # method are atomic and we can't hack inside.
             # Verbatim copy of part of product.pricelist._compute_price_rule.
-            qty_uom_id = self._context.get('uom') or self.uom_id.id
-            price_uom = self.env['uom.uom'].browse([qty_uom_id])
-            convert_to_price_uom = (
-                lambda price: self.uom_id._compute_price(
-                    price, price_uom))
+            qty_uom_id = self._context.get("uom") or self.uom_id.id
+            price_uom = self.env["uom.uom"].browse([qty_uom_id])
+            convert_to_price_uom = lambda price: self.uom_id._compute_price(
+                price, price_uom
+            )
             price_limit = price
             price = (price - (price * (rule.price_discount / 100))) or 0.0
             if rule.price_round:
-                price = tools.float_round(
-                    price, precision_rounding=rule.price_round)
+                price = tools.float_round(price, precision_rounding=rule.price_round)
             if rule.price_surcharge:
                 price_surcharge = convert_to_price_uom(rule.price_surcharge)
                 price += price_surcharge
@@ -61,13 +60,13 @@ class ProductTemplate(models.Model):
                 price = min(price, price_limit + price_max_margin)
         return price
 
-    def price_compute(self, price_type, uom=False, currency=False,
-                      company=False):
+    def price_compute(self, price_type, uom=False, currency=False, company=False):
         """Return dummy not falsy prices when computation is done from supplier
         info for avoiding error on super method. We will later fill these with
         correct values.
         """
-        if price_type == 'supplierinfo':
+        if price_type == "supplierinfo":
             return dict.fromkeys(self.ids, 1.0)
         return super().price_compute(
-            price_type, uom=uom, currency=currency, company=company)
+            price_type, uom=uom, currency=currency, company=company
+        )
