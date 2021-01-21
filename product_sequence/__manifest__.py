3669fa1b0cb1c1c7818fd0a967342a1653f1b742
Kay Häusler <17731016+khaeusler@users.noreply.github.com>
2021-01-21 18:05:50 +0100

    Use parent categories to determine the prefix if the category has no settings for the prefix.

--- product_sequence/__manifest__.py
+++ product_sequence/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Product Sequence',
-    'version': '12.0.1.0.1',
+    'version': '12.0.2.0.1',
     'author': "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/product-attribute',
     'license': 'AGPL-3',
@@ -18,6 +18,7 @@
     'data': [
         'data/product_sequence.xml',
         'views/product_category.xml',
+        'views/res_config_settings_views.xml',
     ],
     'pre_init_hook': 'pre_init_hook',
     'installable': True,
