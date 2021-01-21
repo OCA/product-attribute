e21e1dd3285046e79ff1950a26333c3314bb85dd
sergio-teruel <sergio.teruel@tecnativa.com>
2021-01-21 14:44:16 +0100

    [FIX] product_supplierinfo_for_customer: Crash when the price is computed for a template instead of variant. TT27887

---
+++ product_supplierinfo_for_customer/models/product_customerinfo_mixin.py
@@ -0,0 +1,58 @@
+# Copyright 2021 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ProductSupplierInfoMixin(models.AbstractModel):
+    _name = 'product.customerinfo.mixin'
+    _description = 'Common methods to compute prices based on partners'
+
+    def _get_price_from_customerinfo(self, partner_id):
+        self.ensure_one()
+        if not partner_id:
+            return 0.0
+        if self._name == "product.template":
+            domain = [
+                ("name", "=", partner_id),
+                ("product_tmpl_id", "=", self.id),
+            ]
+        else:
+            domain = [
+                ("name", "=", partner_id),
+                "|",
+                ("product_id", "=", self.id),
+                "&",
+                ("product_tmpl_id", "=", self.product_tmpl_id.id),
+                ("product_id", "=", False),
+            ]
+        customerinfo = self.env["product.customerinfo"].search(
+            domain, limit=1, order="product_id, sequence",)
+        if customerinfo:
+            return customerinfo.price
+        return 0.0
+
+    def get_customerinfo_price(
+            self, uom=False, currency=False, company=False):
+        partner_id = self.env.context.get('partner_id', False) or \
+            self.env.context.get('partner', False)
+        if partner_id and isinstance(partner_id, models.BaseModel):
+            partner_id = partner_id.id
+        prices = self.price_compute('list_price', uom, currency, company)
+        for product in self:
+            price = product._get_price_from_customerinfo(partner_id)
+            if not price:
+                continue
+            prices[product.id] = price
+            if not uom and self._context.get('uom'):
+                uom = self.env['uom.uom'].browse(self._context['uom'])
+            if not currency and self._context.get('currency'):
+                currency = self.env['res.currency'].browse(
+                    self._context['currency'])
+            if uom:
+                prices[product.id] = product.uom_id._compute_price(
+                    prices[product.id], uom)
+            if currency:
+                date = self.env.context.get('date', fields.Datetime.now())
+                prices[product.id] = product.currency_id._convert(
+                    prices[product.id], currency, company, date)
+        return prices
--- product_supplierinfo_for_customer/models/product_product.py
+++ product_supplierinfo_for_customer/models/product_product.py
@@ -2,13 +2,13 @@
 # Copyright 2015 AvanzOSC
 # Copyright 2015 Tecnativa
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-import datetime
 
 from odoo import api, models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = ['product.product', 'product.customerinfo.mixin']
+    _name = 'product.product'
 
     @api.multi
     def name_get(self):
@@ -47,54 +47,9 @@ class ProductProduct(models.Model):
         return res
 
     @api.multi
-    def _get_price_from_customerinfo(self, partner_id):
-        self.ensure_one()
-        if not partner_id:
-            return 0.0
-        customerinfo = self.env["product.customerinfo"].search(
-            [
-                ("name", "=", partner_id),
-                "|",
-                ("product_id", "=", self.id),
-                "&",
-                ("product_tmpl_id", "=", self.product_tmpl_id.id),
-                ("product_id", "=", False),
-            ],
-            limit=1,
-            order="product_id, sequence",
-        )
-        if customerinfo:
-            return customerinfo.price
-        return 0.0
-
-    @api.multi
     def price_compute(
             self, price_type, uom=False, currency=False, company=False):
         if price_type == 'partner':
-            partner_id = self.env.context.get('partner_id', False) or \
-                self.env.context.get('partner', False)
-            if partner_id and isinstance(partner_id, models.BaseModel):
-                partner_id = partner_id.id
-            prices = super(ProductProduct, self).price_compute(
-                'list_price', uom, currency, company)
-            for product in self:
-                price = product._get_price_from_customerinfo(partner_id)
-                if not price:
-                    continue
-                prices[product.id] = price
-                if not uom and self._context.get('uom'):
-                    uom = self.env['uom.uom'].browse(self._context['uom'])
-                if not currency and self._context.get('currency'):
-                    currency = self.env['res.currency'].browse(
-                        self._context['currency'])
-                if uom:
-                    prices[product.id] = product.uom_id._compute_price(
-                        prices[product.id], uom)
-                if currency:
-                    date = self.env.context.get('date',
-                                                datetime.datetime.now())
-                    prices[product.id] = product.currency_id._convert(
-                        prices[product.id], currency, company, date)
-            return prices
-        return super(ProductProduct, self).price_compute(
-            price_type, uom, currency, company)
+            return self.get_customerinfo_price(uom, currency, company)
+        return super().price_compute(
+            price_type, uom=uom, currency=currency, company=company)
