cc7ff240da2833c3345c445c1dfdcc68e5047a2f
Kay Häusler <17731016+khaeusler@users.noreply.github.com>
2021-01-22 13:06:33 +0100

    Use parent categories to determine the prefix if the category has no settings for the prefix.
    [UPD] Update product_sequence.pot
    
    [UPD] README.rst
    
    product_sequence 14.0.2.0.1
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: product-attribute-14.0/product-attribute-14.0-product_sequence
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-14-0/product-attribute-14-0-product_sequence/
    
    Translated using Weblate (Dutch)
    
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: product-attribute-14.0/product-attribute-14.0-product_sequence
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-14-0/product-attribute-14-0-product_sequence/nl/

---
+++ product_sequence/models/ir_sequence.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class IrSequence(models.Model):
+    _inherit = "ir.sequence"
+
+    @api.model
+    def get_category_sequence_id(self, category=False):
+        if self.env.user.company_id.use_parent_categories_to_determine_prefix:
+            while not category.sequence_id and category.parent_id:
+                category = category.parent_id
+        return category.sequence_id or self.env.ref("product_sequence.seq_product_auto")
