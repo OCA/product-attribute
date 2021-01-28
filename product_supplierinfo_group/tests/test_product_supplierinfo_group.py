18e998ac4eb5c84c3e4dbb23c744d10e665353fd
Mourad <mourad.elhadj.mimoune@akretion.com>
2021-01-28 15:45:36 +0100

    [IMP] product_supplierinfo_group: black, isort, prettier

--- product_supplierinfo_group/tests/test_product_supplierinfo_group.py
+++ product_supplierinfo_group/tests/test_product_supplierinfo_group.py
@@ -1,8 +1,9 @@
 # Copyright 2020 Akretion France (http://www.akretion.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
 from copy import deepcopy
+
+from odoo.tests import common
 
 
 class TestProductSupplierinfoGroup(common.SavepointCase):
@@ -58,10 +59,12 @@ class TestProductSupplierinfoGroup(common.SavepointCase):
             [("id", "not in", group_before.ids)]
         )
         self.assertIn(
-            '<td class="table_price_note_cell">5.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">5.0</td>',
+            group.unit_price_note,
         )
         self.assertIn(
-            '<td class="table_price_note_cell">10.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">10.0</td>',
+            group.unit_price_note,
         )
         min_50 = deepcopy(self.supplierinfo_vals)
         min_50.update({"min_qty": 50.0, "price": 8.0})
@@ -69,14 +72,18 @@ class TestProductSupplierinfoGroup(common.SavepointCase):
         min_500.update({"min_qty": 500.0, "price": 6.0})
         self.env["product.supplierinfo"].create([min_500, min_50])
         self.assertIn(
-            '<td class="table_price_note_cell">50.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">50.0</td>',
+            group.unit_price_note,
         )
         self.assertIn(
-            '<td class="table_price_note_cell">8.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">8.0</td>',
+            group.unit_price_note,
         )
         self.assertIn(
-            '<td class="table_price_note_cell">500.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">500.0</td>',
+            group.unit_price_note,
         )
         self.assertIn(
-            '<td class="table_price_note_cell">6.0</td>', group.unit_price_note,
+            '<td class="table_price_note_cell">6.0</td>',
+            group.unit_price_note,
         )
