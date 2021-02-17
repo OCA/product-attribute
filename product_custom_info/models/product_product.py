1c02a3903de2911429e0796340870e04f858653b
Rad0van <radovan@skolnik.info>
2021-02-17 11:16:15 +0100

    [MIG] product_custom_info: Migration to 13.0

--- product_custom_info/models/product_product.py
+++ product_custom_info/models/product_product.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
@@ -10,13 +10,12 @@ class ProductProduct(models.Model):
 
     custom_info_template_id = fields.Many2one(context={"default_model": _name},)
     custom_info_ids = fields.One2many(context={"default_model": _name},)
-    product_tmpl_custom_info_ids = fields.Many2many(
+    product_tmpl_custom_info_ids = fields.One2many(
         comodel_name="custom.info.value",
         compute="_compute_product_tmpl_custom_info_ids",
         string="Product template info",
     )
 
-    @api.multi
     def _compute_product_tmpl_custom_info_ids(self):
         value_obj = self.env["custom.info.value"]
         for product in self:
@@ -27,7 +26,6 @@ class ProductProduct(models.Model):
                 ]
             )
 
-    @api.multi
     def open_product_template(self):
         # HACK: Method is duplicated on core and the second one misses this
         res = super(ProductProduct, self).open_product_template()
