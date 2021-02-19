04bcf9e39782076527819b8f56130368d1e45ac7
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-19 12:24:05 +0100

    [MIG] product_category_active: Migration to v13.0

--- product_category_active/__manifest__.py
+++ product_category_active/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "product_category_archive",
-    "version": "12.0.1.0.1",
+    "name": "Product Category Active",
+    "version": "13.0.1.0.0",
     "category": "Product",
     "summary": "Add option to archive product categories",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "license": "AGPL-3",
-    "depends": ["product",],
-    "data": ["views/product_views.xml",],
+    "depends": ["product"],
+    "data": ["views/product_views.xml"],
 }
