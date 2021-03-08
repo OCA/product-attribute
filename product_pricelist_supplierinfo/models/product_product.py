3800e3b71114c71cae39ad7cb14a54bbf6a26a10
Carlos Roca <carlos.roca@tecnativa.com>
2021-03-08 16:00:56 +0100

    [IMP] product_pricelist_supplierinfo: black, isort

--- product_pricelist_supplierinfo/models/product_product.py
+++ product_pricelist_supplierinfo/models/product_product.py
@@ -7,20 +7,20 @@ from odoo import models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
-    def _get_supplierinfo_pricelist_price(
-            self, rule, date=None, quantity=None):
+    def _get_supplierinfo_pricelist_price(self, rule, date=None, quantity=None):
         return self.product_tmpl_id._get_supplierinfo_pricelist_price(
-            rule, date=date, quantity=quantity, product_id=self.id)
+            rule, date=date, quantity=quantity, product_id=self.id
+        )
 
-    def price_compute(self, price_type, uom=False, currency=False,
-                      company=False):
+    def price_compute(self, price_type, uom=False, currency=False, company=False):
         """Return dummy not falsy prices when computation is done from supplier
         info for avoiding error on super method. We will later fill these with
         correct values.
         """
-        if price_type == 'supplierinfo':
+        if price_type == "supplierinfo":
             return dict.fromkeys(self.ids, 1.0)
         return super().price_compute(
-            price_type, uom=uom, currency=currency, company=company)
+            price_type, uom=uom, currency=currency, company=company
+        )
