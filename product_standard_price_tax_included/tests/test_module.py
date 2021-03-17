a6a3ab1360d2584efb0c43ce2bf28af029fe8a84
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-03-17 15:47:45 +0100

    [FIX] product_standard_price_tax_included: handle case where pricelist items has discount (and surcharge, ...) + ensure that tests are not failing if currency are not correctly set

--- product_standard_price_tax_included/tests/test_module.py
+++ product_standard_price_tax_included/tests/test_module.py
@@ -22,25 +22,49 @@ class TestProductStandardPriceTaxIncluded(TransactionCase):
             'product_standard_price_tax_included.product_product')
         self.pricelist = self.env.ref(
             'product_standard_price_tax_included.pricelist_price_tax_included')
+        self.pricelist_item = self.env.ref(
+            "product_standard_price_tax_included."
+            "pricelist_item_price_tax_included")
+        # ensure that the currency of the price list is the same as
+        # the main company to avoid error due to undesired conversion
+        self.pricelist.currency_id = self.env.ref("base.main_company").currency_id
+
+        # Create an Order
+        self.order = self.SaleOrder.create({
+            'partner_id': self.partner.id,
+            'pricelist_id': self.pricelist.id,
+        })
 
     # Test Section
     def test_01_correct_tax_compute(self):
         """Test if the total of a sale order is correct with price
         based on Price List Tax Included."""
 
-        # Create an Order
-        order = self.SaleOrder.create({
-            'partner_id': self.partner.id,
-            'pricelist_id': self.pricelist.id,
-        })
-
         # Create an Order line with a product with Tax Included
         self.SaleOrderLine.create({
-            'order_id': order.id,
+            'order_id': self.order.id,
             'product_id': self.product.id,
             'product_uom_qty': 1,
             'product_uom': self.uom.id,
         })
         self.assertEquals(
-            order.amount_total, 12.0,
+            self.order.amount_total, 12.0,
             "Computation of Price based on Cost Price Tax Included incorrect.")
+
+    def test_02_correct_tax_compute_with_discount(self):
+        """Test if the total of a sale order is correct with price
+        based on Price List Tax Included with discount on the pricelist"""
+
+        self.pricelist_item.price_discount = 50
+
+        # Create an Order line with a product with Tax Included
+        self.SaleOrderLine.create({
+            'order_id': self.order.id,
+            'product_id': self.product.id,
+            'product_uom_qty': 1,
+            'product_uom': self.uom.id,
+        })
+        self.assertEquals(
+            self.order.amount_total, 6.0,
+            "Computation of Price based on Cost Price Tax Included incorrect."
+            " when discount is set")
