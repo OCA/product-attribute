95584f21b633b996a1ba73b27b0c4ca0281fbdc7
Mourad <mourad.elhadj.mimoune@akretion.com>
2021-03-23 15:34:19 +0100

    [FIX] Make sure all rule records are fetched at once at put in cache

--- product_pricelist_supplierinfo/models/product_pricelist.py
+++ product_pricelist_supplierinfo/models/product_pricelist.py
@@ -17,7 +17,7 @@ class ProductPricelist(models.Model):
             products_qty_partner, date, uom_id
         )
         # Make sure all rule records are fetched at once at put in cache
-        rule_obj.browse(x[1] for x in result.values()).mapped("price_discount")
+        rule_obj.browse([x[1] for x in result.values()]).mapped("price_discount")
         for product, qty, _partner in products_qty_partner:
             rule = rule_obj.browse(result[product.id][1])
             if rule.compute_price == "formula" and rule.base == "supplierinfo":
