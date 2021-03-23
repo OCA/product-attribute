32de9c19a22e58674094cce1dfc30df35e0681d4
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-03-23 11:34:52 +0100

    [IMP] product_life_period: black, isort, prettier

--- product_life_period/models/product_template.py
+++ product_life_period/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models
@@ -6,10 +5,8 @@ from odoo import fields, models
 
 class ProductTemplate(models.Model):
 
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     product_life_period_id = fields.Many2one(
-        'product.life.period',
-        string='Life Period',
-        index=True
+        "product.life.period", string="Life Period", index=True
     )
