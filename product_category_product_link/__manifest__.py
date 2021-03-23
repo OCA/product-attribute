773c891fe26a36a6540c92cdcfb85e5640332dbf
Denis Roussel <denis.roussel@acsone.eu>
2021-03-23 17:36:36 +0100

    [14.0][ADD] product_category_product_link

---
+++ product_category_product_link/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Category Product Link",
+    "summary": """
+        Allows to get products from a category""",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+}
