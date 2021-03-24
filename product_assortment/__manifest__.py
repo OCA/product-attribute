43f9878fe19486d8a3a49ad11738823a79ab17e3
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-03-24 17:08:40 +0100

    [14.0][MIG] product_assortment

--- product_assortment/__manifest__.py
+++ product_assortment/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2018-2019 ACSONE SA/NV
+# Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product Assortment",
     "summary": """
         Adds the ability to manage products assortment""",
-    "version": "13.0.2.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
