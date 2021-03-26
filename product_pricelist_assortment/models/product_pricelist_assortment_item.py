3e6247867674e5a9668c3ad3d4bbadb7ce9f018c
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-03-26 09:30:48 +0100

    [MIG][14.0] product_pricelist_assortement

--- product_pricelist_assortment/models/product_pricelist_assortment_item.py
+++ product_pricelist_assortment/models/product_pricelist_assortment_item.py
@@ -1,4 +1,4 @@
-# Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2021 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
--- product_pricelist_assortment/models/product_pricelist_item.py
+++ product_pricelist_assortment/models/product_pricelist_item.py
@@ -1,4 +1,4 @@
-# Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2021 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
