f9fd95d4012ab553ce9075e11e09887c2d7c2593
Marc Poch <mpoch@planetatic.com>
2021-04-06 16:29:14 +0200

    [FIX] product_application: Removed many2one product_tmpl_id field from model product_application. Now using many2many product_tmpl_ids.

--- product_application/models/product_application.py
+++ product_application/models/product_application.py
@@ -16,23 +16,3 @@ class ProductApplication(models.Model):
         column2='product_tmpl_id',
         string='Product Templates',
     )
-
-    # FIXME: Delete field product_tmpl_id:
-    @api.model
-    def _default_product_template(self):
-        product_obj = self.env['product.product']
-
-        if self._context.get('default_template_id', False):
-            return self._context['default_template_id']
-        elif self._context.get('default_product_id', False):
-            product = product_obj.browse(self._context['default_product_id'])
-            return product.product_tmpl_id.id
-        return 0
-
-    product_tmpl_id = fields.Many2one(
-        'product.template',
-        'Product Template',
-        help="Product Template",
-        required=False,
-        default=_default_product_template
-    )
