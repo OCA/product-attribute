a1f84f8782540956cea0897c4ccdd73b835d4d04
Denis Roussel <denis.roussel@acsone.eu>
2021-04-07 17:38:23 +0200

    [14.0][ADD] product_category_code

---
+++ product_category_code/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Category Code",
+    "summary": """
+        Allows to define a code on product categories""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["rousseldenis"],
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_category.xml",
+    ],
+}
