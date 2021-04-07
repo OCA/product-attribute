68de9f398bf79347de096fb5f4acfe273dec6b2d
Denis Roussel <denis.roussel@acsone.eu>
2021-04-07 18:20:37 +0200

    [14.0][ADD] product_category_code_unique

---
+++ product_category_code_unique/tests/test_code_unique.py
@@ -0,0 +1,29 @@
+# Copyright 2021 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from psycopg2 import IntegrityError
+
+import odoo.tests.common as common
+from odoo.tools import mute_logger
+
+
+class TestProductCode(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        vals = {
+            "name": "Category Test",
+            "code": "TEST",
+        }
+        cls.category = cls.env["product.category"].create(vals)
+
+    @mute_logger("odoo.sql_db")
+    def test_category_code_unique(self):
+        vals = {
+            "name": "Category Test duplicate",
+            "code": "TEST",
+        }
+        with self.assertRaises(IntegrityError), self.env.cr.savepoint():
+            self.category_dup = self.env["product.category"].create(vals)
+
+        vals.update({"code": "TEST1"})
+        self.category_dup = self.env["product.category"].create(vals)
