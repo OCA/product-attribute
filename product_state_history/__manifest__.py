23beff2900fbb508aba108ae764caea8e2978409
Denis Roussel <denis.roussel@acsone.eu>
2021-04-12 09:32:07 +0200

    [IMP] product_state_history: black, isort, prettier

--- product_state_history/__manifest__.py
+++ product_state_history/__manifest__.py
@@ -1,25 +1,24 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product State History',
-    'summary': """
+    "name": "Product State History",
+    "summary": """
         Allows to store product state history for reporting purpose""",
-    'version': '10.0.1.0.1',
-    'license': 'AGPL-3',
-    'category': 'Sales',
-    'maintainers': ['rousseldenis'],
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-attribute',
-    'depends': [
-        'product_state',
+    "version": "10.0.1.0.1",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": [
+        "product_state",
     ],
-    'data': [
-        'security/product_state_history.xml',
-        'views/product_state_history.xml',
-        'views/product_template.xml',
-        'wizards/product_state_history_wizard.xml',
-        'report/report_product_state_history.xml',
+    "data": [
+        "security/product_state_history.xml",
+        "views/product_state_history.xml",
+        "views/product_template.xml",
+        "wizards/product_state_history_wizard.xml",
+        "report/report_product_state_history.xml",
     ],
 }
