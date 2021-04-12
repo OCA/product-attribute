e6fc5c58c9265b0002027b043c234572878afe20
Denis Roussel <denis.roussel@acsone.eu>
2021-04-12 09:32:07 +0200

    [IMP] product_state_history: black, isort, prettier

--- product_state_history/wizards/product_state_history_wizard.py
+++ product_state_history/wizards/product_state_history_wizard.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
@@ -6,20 +5,20 @@ from odoo import api, fields, models
 
 class ProductStateHistoryWizard(models.TransientModel):
 
-    _name = 'product.state.history.wizard'
-    _description = 'Product State History Report'
+    _name = "product.state.history.wizard"
+    _description = "Product State History Report"
 
     pivot_date = fields.Datetime(
         default=lambda self: fields.Datetime.now(),
     )
     product_state = fields.Selection(
         selection=[
-            ('draft', 'In Development'),
-            ('sellable', 'Normal'),
-            ('end', 'End of Lifecycle'),
-            ('obsolete', 'Obsolete')
+            ("draft", "In Development"),
+            ("sellable", "Normal"),
+            ("end", "End of Lifecycle"),
+            ("obsolete", "Obsolete"),
         ],
-        string='Product Status',
+        string="Product Status",
         required=True,
     )
 
@@ -28,19 +27,20 @@ class ProductStateHistoryWizard(models.TransientModel):
         # Get product history for the actual product state
         self.ensure_one()
         return [
-            ('state_date', '>=', self.pivot_date),
-            ('product_state', '=', self.product_state),
-            ('product_template_id.state', '=', self.product_state),
-            ('product_template_id.active', '=', True),
+            ("state_date", ">=", self.pivot_date),
+            ("product_state", "=", self.product_state),
+            ("product_template_id.state", "=", self.product_state),
+            ("product_template_id.active", "=", True),
         ]
 
     @api.multi
     def print_report(self):
         for wizard in self:
-            history_obj = self.env['product.state.history']
+            history_obj = self.env["product.state.history"]
             products = list()
-            histories = self.env['product.state.history'].search(
-                wizard._get_product_domain())
+            histories = self.env["product.state.history"].search(
+                wizard._get_product_domain()
+            )
             # As product state history is ordered by id desc, we take
             # the first one by product
             history_report = history_obj.browse()
@@ -51,12 +51,17 @@ class ProductStateHistoryWizard(models.TransientModel):
                     history_report |= history
 
             datas = {
-                'ids': history_report.ids,
-                'model': 'product.state.history',
-                'form': {'pivot_date': wizard.pivot_date}
+                "ids": history_report.ids,
+                "model": "product.state.history",
+                "form": {"pivot_date": wizard.pivot_date},
             }
 
-            return self.env['report'].with_context(landscape=True).get_action(
-                history_report.ids,
-                'product_state_history.report_product_state_history',
-                data=datas)
+            return (
+                self.env["report"]
+                .with_context(landscape=True)
+                .get_action(
+                    history_report.ids,
+                    "product_state_history.report_product_state_history",
+                    data=datas,
+                )
+            )
