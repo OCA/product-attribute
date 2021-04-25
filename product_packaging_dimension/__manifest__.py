24c773cb7c0099bf2f6ac11509e1aaa6781cd03b
Kévin Roche <kevin.roche@akretion.com>
2021-04-25 22:37:54 +0200

    [MIG] product_packaging_dimension: Migration to 14.0

--- product_packaging_dimension/__manifest__.py
+++ product_packaging_dimension/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
     "name": "Product Packaging Dimension",
     "summary": "Manage packaging dimensions and weight",
     "version": "14.0.1.0.0",
     "category": "Product",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Camptocamp, Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "product",
+        "product_logistics_uom",
     ],
     "website": "https://github.com/OCA/product-attribute",
     "data": [
