340fb14c9b38fc8efc10357786de09f7f5ce5a7a
Pablo Paez <pablopaezps97@gmail.com>
2021-04-28 12:31:13 -0300

    [13.0][FIX]product_code_unique:check unique code in all companys
    The default_code have to be unique across all products
    no matters the company their belong

--- product_code_unique/models/product.py
+++ product_code_unique/models/product.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, api
 
 
 class ProductProduct(models.Model):
@@ -14,3 +14,15 @@ class ProductProduct(models.Model):
             "Internal Reference must be unique across the database!",
         )
     ]
+
+    @api.constrains('default_code', 'active')
+    def check_unique_company_and_default_code(self):
+        for rec in self:
+            if rec.active and rec.default_code:
+                filters = [
+                    ('default_code', '=', rec.default_code),
+                    ('active', '=', True)]
+                products = self.search(filters)
+                if len(products) > 1:
+                    raise ValidationError(_(
+                        'There can not be two active products with the same Reference code.'))
