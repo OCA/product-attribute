536efe75d38dc4a91538a93b063d71c49b0d9bac
Denis Roussel <denis.roussel@acsone.eu>
2021-05-18 14:29:43 +0200

    [14.0][IMP] product_assortment: Fallback to create() for model_id

--- product_assortment/models/ir_filters.py
+++ product_assortment/models/ir_filters.py
@@ -1,7 +1,6 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
 from odoo.osv import expression
 
 
@@ -9,19 +8,30 @@ class IrFilters(models.Model):
     _inherit = "ir.filters"
 
     @api.model
-    def _get_default_model(self):
-        if self.env.context.get("product_assortment", False):
-            model = self.env.ref("product.model_product_product")
-            return model.model
-        raise ValidationError(_("Error no product assortment in context"))
-
-    @api.model
     def _get_default_is_assortment(self):
         if self.env.context.get("product_assortment", False):
             return True
         return False
 
-    model_id = fields.Selection(default=lambda x: x._get_default_model())
+    @api.model
+    def _update_assortment_default_values(self, vals_list):
+        """
+        If we create the filter through the assortment, we need to force
+        model_id to product.product
+        """
+        product_assortment = self.env.context.get("product_assortment", False)
+        if not product_assortment:
+            return vals_list
+        model = self.env.ref("product.model_product_product")
+        for vals in vals_list:
+            if "model_id" not in vals:
+                vals.update({"model_id": model.model})
+        return vals_list
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        self._update_assortment_default_values(vals_list=vals_list)
+        return super().create(vals_list=vals_list)
 
     partner_ids = fields.Many2many(
         comodel_name="res.partner",
