ea471aef19bfd33f583f0d40398cf4c2784b2e63
Chandresh Thakkar OSI <cthakkar@opensourceintegrators.com>
2021-05-21 01:39:47 +0530

    [NEW] product_tier_validation: New module to validate the product.

---
+++ product_tier_validation/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Tier Validation",
+    "summary": "Support a tier validation process for Products",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/OCA/product-attribute",
+    "category": "Products",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "maintainer": ["dreispt"],
+    "development_status": "Alpha",
+    "depends": ["product_state", "base_tier_validation"],
+    "data": [
+        "data/tier_definition.xml",
+        "data/product_state_data.xml",
+        "views/product_template_view.xml",
+    ],
+}
---
+++ product_tier_validation/models/product_template.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ProductTemplate(models.Model):
+    _name = "product.template"
+    _inherit = ["product.template", "tier.validation"]
+    _tier_validation_manual_config = False
+
+    @api.model
+    def _is_tier_validated_active(self, state_code):
+        return state_code != "draft"
+
+    @api.model
+    def create(self, vals):
+        new = super().create(vals)
+        if new.need_validation and self._is_tier_validated_active(new.state):
+            new.active = False
+        return new
+
+    def write(self, vals):
+        """
+        Default `active` is False.
+        It is set to True when State changes to confirmed.
+        """
+        if "state" in vals:
+            vals["active"] = self._is_tier_validated_active(vals["state"])
+        return super().write(vals)
+
+    @api.model
+    def _get_default_product_state_id(self):
+        return self.env.ref(
+            "product_state.product_state_draft", raise_if_not_found=False
+        )
