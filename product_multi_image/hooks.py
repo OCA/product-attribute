0ef0029d4f6a8772e071a184ecf92456aea34db9
Hai Lang <hailn@trobz.com>
2021-05-21 14:53:34 +0700

    [IMP] product_multi_image: black, isort, prettier

--- product_multi_image/hooks.py
+++ product_multi_image/hooks.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -18,7 +17,12 @@ def pre_init_hook(cr):
 
 def uninstall_hook(cr, registry):
     """Remove multi images for models that no longer use them."""
-    uninstall_hook_for_submodules(cr, registry, "product.template",
-                                  field="image", field_medium="image_medium",
-                                  field_small="image_small")
+    uninstall_hook_for_submodules(
+        cr,
+        registry,
+        "product.template",
+        field="image",
+        field_medium="image_medium",
+        field_small="image_small",
+    )
     uninstall_hook_for_submodules(cr, registry, "product.product")
--- product_multi_image/models/product_template.py
+++ product_multi_image/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -11,31 +10,31 @@ class ProductTemplate(models.Model):
     _inherit = [_name, "base_multi_image.owner"]
 
     image = fields.Binary(
-        related='image_main',
+        related="image_main",
         store=False,
     )
     image_medium = fields.Binary(
-        related='image_main_medium',
+        related="image_main_medium",
         store=False,
     )
     image_small = fields.Binary(
-        related='image_main_small',
+        related="image_main_small",
         store=False,
     )
 
     @api.multi
     def write(self, vals):
-        if set(['image', 'image_medium', 'image_small']) & set(vals.keys()):
+        if {"image", "image_medium", "image_small"} & set(vals.keys()):
             # medium/small image is saved as large
             # on load image will be medium/small resized on the fly
-            if 'image' in vals:
+            if "image" in vals:
                 pass
-            elif 'image_medium' in vals:
-                vals['image'] = vals['image_medium']
-                del vals['image_medium']
-            elif 'image_small' in vals:
-                vals['image'] = vals['image_small']
-                del vals['image_small']
+            elif "image_medium" in vals:
+                vals["image"] = vals["image_medium"]
+                del vals["image_medium"]
+            elif "image_small" in vals:
+                vals["image"] = vals["image_small"]
+                del vals["image_small"]
         return super(ProductTemplate, self).write(vals)
 
     @api.multi
