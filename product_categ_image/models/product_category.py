f85c7863f8ebad45b13718963be6e4ec2d573208
Hai Lang <hailn@trobz.com>
2021-05-24 20:36:24 +0700

    [IMP] product_categ_image: black, isort, prettier

--- product_categ_image/models/product_category.py
+++ product_categ_image/models/product_category.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2018 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,6 +6,6 @@ from odoo import fields, models
 
 
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
-    image = fields.Binary(string='Image', attachment=True)
+    image = fields.Binary(string="Image", attachment=True)
