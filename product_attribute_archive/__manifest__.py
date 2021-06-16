b9f055d0033e46374d26c2e9cbf03ce6e3435332
KevinKhao <kevinkhao@gmail.com>
2021-06-16 08:03:44 +0200

    product_attribute_archive

---
+++ product_attribute_archive/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Attribute Archive",
+    "summary": """
+        Add an active field on product attributes""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "depends": ["product"],
+    "data": [
+        "views/product_attribute.xml",
+    ],
+    "website": "https://github.com/OCA/product-attribute",
+}
