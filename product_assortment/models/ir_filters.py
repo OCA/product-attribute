04c282d027b2f4cbeb621698bfff3bf982c0376f
Kevin.roche <kevin.roche@akretion.com>
2021-06-18 00:06:48 +0200

    fix ir_filters

--- product_assortment/models/ir_filters.py
+++ product_assortment/models/ir_filters.py
@@ -24,7 +24,7 @@ class IrFilters(models.Model):
             return vals_list
         model = self.env.ref("product.model_product_product")
         for vals in vals_list:
-            if "model_id" not in vals:
+            if not vals.get("model_id"):
                 vals.update({"model_id": model.model})
         return vals_list
 
@@ -64,7 +64,10 @@ class IrFilters(models.Model):
 
         return res
 
-    @api.depends("domain", "blacklist_product_ids", "whitelist_product_ids")
+    # Original : @api.depends("domain", "blacklist_product_ids", "whitelist_product_ids")
+    # TODO : Resolve bug if depending of blacklist_product_ids and
+    # whitelist_product_ids when we add some products to black / whitelist.
+    @api.depends("domain")
     def _compute_record_count(self):
         for record in self:
             if record.model_id not in self.env:
