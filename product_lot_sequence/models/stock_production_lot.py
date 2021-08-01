29f5410167037fe05436f703f87f84e88e3833ce
remi-filament <30716308+remi-filament@users.noreply.github.com>
2021-08-01 20:27:02 +0200

    [IMP] product_lot_sequence: Change name on create and update translations

--- product_lot_sequence/models/stock_production_lot.py
+++ product_lot_sequence/models/stock_production_lot.py
@@ -11,3 +11,12 @@ class ProductionLot(models.Model):
     def onchange_product_id(self):
         if self.product_id and self.product_id.product_tmpl_id.lot_sequence_id:
             self.name = self.product_id.product_tmpl_id.lot_sequence_id._next()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for lot_vals in vals_list:
+            if "name" not in lot_vals:
+                product = self.env["product.product"].browse(lot_vals["product_id"])
+                if product and product.product_tmpl_id.lot_sequence_id:
+                    lot_vals["name"] = product.product_tmpl_id.lot_sequence_id._next()
+        return super(ProductionLot, self).create(vals_list)
