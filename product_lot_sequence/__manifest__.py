3d050700d496a4ff7e7ffaa097eb5fa68347cf51
remi-filament <30716308+remi-filament@users.noreply.github.com>
2021-08-01 20:26:32 +0200

    [MIG] product_lot_sequence: Migration to v14.0

--- product_lot_sequence/__manifest__.py
+++ product_lot_sequence/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Product Lot Sequence",
     "summary": """
         Adds ability to define a lot sequence from the product""",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
-    "website": "https://www.github.com/OCA/product-attribute",
+    "website": "https://github.com/OCA/product-attribute",
     "depends": ["stock"],
     "data": ["views/product_views.xml"],
 }
