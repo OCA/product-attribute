b1d80479a6e424e45aa9edadaca474ca093cd913
Simone Orsi <simahawk@gmail.com>
2021-08-05 12:52:08 +0200

    [MIG] product_barcode_required: Migration to 14.0

--- product_barcode_required/__manifest__.py
+++ product_barcode_required/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Product barcode required",
     "summary": "Make product barcode required when enabled",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
+    "maintainers": ["simahawk"],
     "development_status": "Beta",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Camptocamp, Odoo Community Association (OCA)",
