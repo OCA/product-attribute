6156c3a6331d109fdbbbf3d7b52c73abc6f58a49
Lindsay <lindsay.marion@acsone.eu>
2021-09-01 14:48:56 +0200

    [MIG] Backport product total weight from packaging

--- product_total_weight_from_packaging/models/product.py
+++ product_total_weight_from_packaging/models/product.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
 from odoo import models
 
 
--- product_total_weight_from_packaging/tests/test_product_total_weight_from_packaging.py
+++ product_total_weight_from_packaging/tests/test_product_total_weight_from_packaging.py
@@ -1,28 +1,37 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo.tests import SavepointCase
+from odoo.tests.common import SavepointCase
 
 
 class TestProductTotalWeightFromPackaging(SavepointCase):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
+        super(TestProductTotalWeightFromPackaging, cls).setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.product = cls.env.ref("product.product_product_20")
         cls.product.weight = 5
         cls.env["product.packaging"].create(
-            {"name": "pair", "product_id": cls.product.id, "qty": 2, "max_weight": 12.5}
+            {
+                "name": "pair",
+                "product_tmpl_id": cls.product.product_tmpl_id.id,
+                "qty": 2,
+                "max_weight": 12.5,
+            }
         )
         cls.env["product.packaging"].create(
             {
                 "name": "cardbox",
-                "product_id": cls.product.id,
+                "product_tmpl_id": cls.product.product_tmpl_id.id,
                 "qty": 10,
                 "max_weight": 55,
             }
         )
         cls.env["product.packaging"].create(
-            {"name": "pallet", "product_id": cls.product.id, "qty": 200}
+            {
+                "name": "pallet",
+                "product_tmpl_id": cls.product.product_tmpl_id.id,
+                "qty": 200,
+            }
         )
 
     def test_weight_from_packaging(self):
