29e4bd52f8677d669dfab6885da565aa94b14025
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-09-01 09:46:29 +0200

    [FIX] product_list_pricelist_price: bump version
    to solve a pypi upload issue

--- product_list_pricelist_price/__manifest__.py
+++ product_list_pricelist_price/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product List Pricelist Price",
     "summary": "Display pricelist prices for products in list view.",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
