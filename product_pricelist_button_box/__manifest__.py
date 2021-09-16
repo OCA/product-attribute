9abeb71b67c6c1bad947f0df4dff6c06d33d1c88
Denis Roussel <denis.roussel@acsone.eu>
2021-09-16 17:28:06 +0200

    [14.0][ADD] product_pricelist_button_box

---
+++ product_pricelist_button_box/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Pricelist Button Box",
+    "summary": """
+        Allows to define a button_box section on pricelist form""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "maintainers": ["rousseldenis"],
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_pricelist.xml",
+    ],
+}
