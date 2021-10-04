41b02ac6ea2c390e4e67652491ddee831eee8c45
nguyenhk <nguyenhk@trobz.com>
2021-10-04 17:05:22 +0700

    [14.0][ADD] uom_extra_data: have mm, ml, week, work week uom

---
+++ uom_extra_data/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "uom_extra_data",
+    "summary": "Have mm, ml, week, work_week uom",
+    "version": "14.0.1.0.1",
+    "category": "Product",
+    "author": "Camptocamp, Trobz, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "uom",
+    ],
+    "website": "https://github.com/OCA/product-attribute",
+    "data": [
+        "data/uom.xml",
+    ],
+    "installable": True,
+}
