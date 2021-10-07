10e73beacece13632c46f6bfd1b8b7cb6a8ebe31
Marco Colombo <marco.colombo@gmail.com>
2021-10-07 17:01:47 +0200

    [IMP] product_model_viewer: black, isort, prettier

--- product_model_viewer/models/product.py
+++ product_model_viewer/models/product.py
@@ -1,23 +1,29 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
     gltf_3d_model = fields.Binary(
-        "3D model", attachment=True, help="Load a model in GLTF 2.0 format")
+        "3D model", attachment=True, help="Load a model in GLTF 2.0 format"
+    )
 
 
 class Product(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
     gltf_3d_model_variant = fields.Binary(
-        "Variant 3D model", attachment=True,
-        help="This field holds the 3D model used for the product variant.")
+        "Variant 3D model",
+        attachment=True,
+        help="This field holds the 3D model used for the product variant.",
+    )
     gltf_3d_model = fields.Binary(
-        "3D model", compute='_compute_gltf_3d_model', inverse='_set_gltf_3d_model',
-        help="3D model of the product variant.")
+        "3D model",
+        compute="_compute_gltf_3d_model",
+        inverse="_set_gltf_3d_model",
+        help="3D model of the product variant.",
+    )
 
     @api.multi
-    @api.depends('gltf_3d_model_variant', 'product_tmpl_id.gltf_3d_model')
+    @api.depends("gltf_3d_model_variant", "product_tmpl_id.gltf_3d_model")
     def _compute_gltf_3d_model(self):
         for p in self:
             p.gltf_3d_model = p.gltf_3d_model_variant
