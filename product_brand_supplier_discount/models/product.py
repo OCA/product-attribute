c74f30bd68b59103cc9edf63af9d412e983d1d91
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2021-10-13 16:21:46 +0200

    [FIX]product_brand_supplier_discount: Allows to force fixed price

--- product_brand_supplier_discount/models/product.py
+++ product_brand_supplier_discount/models/product.py
@@ -8,12 +8,12 @@ class SupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
     fixed_discount = fields.Float(string="Fixed Discount", readonly=True)
+    set_fixed_discount = fields.Boolean(string="Set fixed discount")
     discount = fields.Float(string='Discount', compute="_compute_discount",
                             inverse="_set_discount", store=True)
 
     @api.depends(
         'name',
-        'sale_price',
         'product_tmpl_id.product_brand_id.supplier_discount_ids',
         'product_tmpl_id.product_brand_id.supplier_discount_ids.partner_id',
         'product_tmpl_id.product_brand_id.supplier_discount_ids.discount',
@@ -23,23 +23,22 @@ class SupplierInfo(models.Model):
     def _compute_discount(self):
         # computes the discount suggested by supplier and brand.
         for supplierinfo in self:
-            apply_branch_discount = False
             brand = False
-            if supplierinfo.product_tmpl_id:
-                brand = supplierinfo.product_tmpl_id.product_brand_id
-            elif supplierinfo.product_id:
-                brand = supplierinfo.product_id.product_brand_id
-            if brand:
-                for supplier_discount in brand.supplier_discount_ids:
-                    if supplier_discount.partner_id == supplierinfo.name:
-                        supplierinfo.discount = supplier_discount.discount
-                        supplierinfo.fixed_discount = 0.0
-                        apply_branch_discount = True
-            if not apply_branch_discount:
+            if supplierinfo.set_fixed_discount:
                 supplierinfo.discount = supplierinfo.fixed_discount
+            else:
+                if supplierinfo.product_tmpl_id:
+                    brand = supplierinfo.product_tmpl_id.product_brand_id
+                elif supplierinfo.product_id:
+                    brand = supplierinfo.product_id.product_brand_id
+                if brand:
+                    for supplier_discount in brand.supplier_discount_ids:
+                        if supplier_discount.partner_id == supplierinfo.name:
+                            supplierinfo.discount = supplier_discount.discount
 
     @api.multi
     def _set_discount(self):
         # user can edit discount of a supplierinfo:
         self.ensure_one()
         self.fixed_discount = self.discount
+        self.set_fixed_discount = True
