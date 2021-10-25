7cddb82f4d2f85f6f80c0891f050b1be8481444f
Telmo Santos <telmo.santos@camptocamp.com>
2021-10-25 10:25:25 +0200

    [MIG] product_sale_manufactured_for: Migration to 15.0

--- product_sale_manufactured_for/__manifest__.py
+++ product_sale_manufactured_for/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Product Manufactured for Customer",
     "summary": "Allows to indicate in products that they were made "
     "specifically for some customers.",
-    "version": "14.0.1.0.0",
+    "version": ".0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Camptocamp, Odoo Community Association (OCA)",
