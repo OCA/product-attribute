c80080ef78d24334c12c21be3318f0d85376a03b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-11-09 21:18:53 +0100

    [ADD] product_pricelist_by_contact: Pricelist per contact
    This module allows to set a different pricelist per each contact, so that such
    pricelist is used in the sales flow.
    
    Usage
    =====
    
    * Go to *Contacts* (you need to install module `contacts` for having this menu entry).
    * Select one contact that is not the parent one.
    * On the tab "Sales & Purchases", you will be able to select a different pricelist
      from its parent contact.
    * If you change the pricelist in the parent contact, the pricelist of the children
      contacts will be overwritten with this one, no matter the previous value.
    
    Known issues / Roadmap
    ======================
    
    * Not requiring to override any sales behavior is possible due to the way Odoo
      retrieves the pricelist directly associated to the contact, but no code is put
      here for assuring that such behavior is preserved across time.

---
+++ product_pricelist_by_contact/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Pricelist Per Contact",
+    "version": "13.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["product"],
+    "data": ["views/res_partner_views.xml"],
+}
---
+++ product_pricelist_by_contact/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to set a different pricelist per each contact, so that such
+pricelist is used in the sales flow.
