28e367dcb3d4b99424fbafdf819fbef995ed8719
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-10 13:26:00 +0100

    [MIG] product_pricelist_revision: Migration to 14.0 TT31815

--- product_pricelist_revision/__manifest__.py
+++ product_pricelist_revision/__manifest__.py
@@ -4,12 +4,16 @@
 {
     "name": "Product Pricelist Revision",
     "summary": "Product Pricelist Revision",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["sale_management"],
-    "data": ["views/pricelist_view.xml", "wizards/pricelist_duplicate_wizard_view.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/pricelist_view.xml",
+        "wizards/pricelist_duplicate_wizard_view.xml",
+    ],
 }
