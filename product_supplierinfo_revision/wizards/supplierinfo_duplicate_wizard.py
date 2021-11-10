ae22fcaafac116df65ea4556b2aa00ba8a5a9dad
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-10 14:12:25 +0100

    [MIG] product_supplierinfo_revision: Migration to 14.0 TT31817

--- product_supplierinfo_revision/wizards/supplierinfo_duplicate_wizard.py
+++ product_supplierinfo_revision/wizards/supplierinfo_duplicate_wizard.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from dateutil.relativedelta import relativedelta
@@ -19,22 +19,26 @@ class ProductSupplierInfoDuplicateWizard(models.TransientModel):
 
     def action_apply(self):
         Supplierinfo = self.env["product.supplierinfo"]
-        supplierinfo_news = Supplierinfo
-        for item in Supplierinfo.browse(self.env.context["active_ids"]):
-            supplierinfo_news |= item.copy(
-                {
-                    "date_start": self.date_start,
-                    "date_end": self.date_end,
-                    "previous_info_id": item.id,
-                    "price": item.price * (1.0 + self.variation_percent / 100.0),
-                }
+        new_ids = list()
+        for item in Supplierinfo.browse(self.env.context.get("active_ids")):
+            new_ids.append(
+                item.copy(
+                    {
+                        "date_start": self.date_start,
+                        "date_end": self.date_end,
+                        "previous_info_id": item.id,
+                        "price": item.price * (1.0 + self.variation_percent / 100.0),
+                    }
+                ).id
             )
             item.date_end = fields.Date.from_string(self.date_start) - relativedelta(
                 days=1
             )
-        action = self.env.ref("product.product_supplierinfo_type_action").read()[0]
-        if len(supplierinfo_news) > 0:
-            action["domain"] = [("id", "in", supplierinfo_news.ids)]
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "product.product_supplierinfo_type_action"
+        )
+        if len(new_ids) > 0:
+            action["domain"] = [("id", "in", new_ids)]
         else:  # pragma: no cover
             action = {"type": "ir.actions.act_window_close"}
         return action
