b0803c2b1fd11f79d6db532435007cc1744e23cc
Denis Roussel <denis.roussel@acsone.eu>
2021-11-25 10:15:28 +0100

    [14.0][ADD] product_state_active

---
+++ product_state_active/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product State Active",
+    "summary": """
+        Allows to define option on product state to activate or inactivate product""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["rousseldenis"],
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": [
+        "product_state",
+    ],
+    "data": [
+        "views/product_state.xml",
+    ],
+}
---
+++ product_state_active/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to set options on product states that will trigger
+the activation or deactivation (Archive) of the products.
