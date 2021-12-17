d849bcd29e990c54726a2a139956a6b40560613a
Denis Roussel <denis.roussel@acsone.eu>
2021-12-17 16:03:50 +0100

    [14.0][IMP] product_tier_validation: Take into account state Many2one write

--- product_tier_validation/models/product_template.py
+++ product_tier_validation/models/product_template.py
@@ -8,3 +8,34 @@ class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = ["product.template", "tier.validation"]
     _tier_validation_manual_config = False
+
+    def _check_state_conditions(self, vals):
+        """
+        As product state write is done on product_state_id field if
+        changed through interface. We need to get the correspondong state
+        """
+        self.ensure_one()
+        if "product_state_id" in vals:
+            new_state = self.env["product.state"].browse(vals["product_state_id"]).code
+            return (
+                getattr(self, self._state_field) in self._state_from
+                and new_state in self._state_to
+            )
+        else:
+            return super()._check_state_conditions(vals)
+
+    def _check_tier_state_transition(self, vals):
+        """
+        As product state write is done on product_state_id field if
+        changed through interface. We need to get the correspondong state
+        """
+        self.ensure_one()
+        if "product_state_id" in vals:
+            new_state = self.env["product.state"].browse(vals["product_state_id"]).code
+            return getattr(
+                self, self._state_field
+            ) in self._state_from and new_state not in (
+                self._state_to + [self._cancel_state]
+            )
+        else:
+            return super()._check_tier_state_transition(vals)
