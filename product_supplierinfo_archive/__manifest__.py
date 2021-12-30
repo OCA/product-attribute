4951b7f14bfa18b7864d6fedff88bf64bba9b577
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2021-12-30 11:47:06 +0100

    [13.0][ADD]product_supplierinfo_archive

---
+++ product_supplierinfo_archive/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Product Supplier Info Archive",
+    "summary": """
+        Add the active field to the product supplier info
+        """,
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "depends": ["product"],
+    "installable": True,
+    "maintainers": ["GuillemCForgeFlow", "AlvaroTForgeFlow", "OriolVForgeFlow"],
+}
