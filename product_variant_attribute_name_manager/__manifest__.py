de2dc935491fe4875efef90e0fae9e6332b25b7a
OriolVForgeFlow <oriol.villamayor@forgeflow.com>
2022-01-12 13:20:19 +0100

    [15.0][MIG] product_variant_attribute_name_manager: Migration to 15.0

--- product_variant_attribute_name_manager/__manifest__.py
+++ product_variant_attribute_name_manager/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "category": "Product",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "depends": ["product"],
     "data": ["views/product_variant_attribute_name_manager_view.xml"],
     "license": "AGPL-3",
