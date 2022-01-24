8e5978f5ea9f511302901273d8caf4b0a076abdc
Nedas Žilinskas <nedas.zilinskas@web-veistamo.fi>
2022-01-24 12:44:26 +0000

    [MIG] product_code_unique: Migration to 15.0

--- product_code_unique/__manifest__.py
+++ product_code_unique/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Unique Product Internal Reference",
     "summary": "Set Product Internal Reference as Unique",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Product",
