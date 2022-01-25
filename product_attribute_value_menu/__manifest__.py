925c88655d4a55bd9cbf7f06da4f86eb48f95fbf
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2022-01-25 10:32:56 +0100

    [13.0][ADD]product_attribute_value_menu

---
+++ product_attribute_value_menu/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Attribute Value Menu",
+    "summary": """
+        Add a menu item containing the Attribute Values
+        """,
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "depends": ["sale", "stock"],
+    "data": ["views/product_attribute_value.xml"],
+    "website": "https://github.com/OCA/product-attribute",
+}
---
+++ product_attribute_value_menu/readme/USAGE.rst
@@ -0,0 +1,1 @@
+To acces the new view, simply go to Sales/Inventory > Configuration > Attribute Values.
