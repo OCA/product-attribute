1265e5ec9492160bd7cdc15105538a523574c8b1
AaronHForgeFlow <ahenriquez@eficent.com>
2022-01-25 09:28:42 +0100

    [13.0][MIG]product_category_code: backport from 14.0

--- product_category_code/__manifest__.py
+++ product_category_code/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Product Category Code",
     "summary": """
         Allows to define a code on product categories""",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["rousseldenis"],
     "website": "https://github.com/OCA/product-attribute",
-    "depends": ["product",],
-    "data": ["views/product_category.xml",],
+    "depends": ["product"],
+    "data": ["views/product_category.xml"],
 }
