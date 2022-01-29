d643869628c5287b85b755f9f84a0de0be169749
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-01-29 10:24:53 +0100

    [FIX] Fix access right issues when using actions

--- product_pricelist_revision/__manifest__.py
+++ product_pricelist_revision/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Pricelist Revision",
     "summary": "Product Pricelist Revision",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
