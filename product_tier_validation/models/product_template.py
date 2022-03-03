ecf16d6bd53ee367c1f90f38c25de5f2ae7a6148
Daniel Reis <dreis@opensourceintegrators.com>
2022-03-03 18:50:46 +0000

    [FIX] product_tier_validation: error on change stage for validated product

--- product_tier_validation/models/product_template.py
+++ product_tier_validation/models/product_template.py
@@ -8,3 +8,15 @@ class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = ["product.template", "tier.validation"]
     _tier_validation_manual_config = False
+
+    def write(self, vals):
+        # Tier Validation does not work with Stages, only States :-(
+        # So, signal state transition by adding it to the vals
+        if "product_state_id" in vals:
+            stage_id = vals.get("product_state_id")
+            stage = self.env["product.state"].browse(stage_id)
+            vals["state"] = stage.code  # yes, "code" is used to represent a "state"
+        res = super().write(vals)
+        if "product_state_id" in vals:
+            self.restart_validation()
+        return res
