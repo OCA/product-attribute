f2c3fe5e6deacb8200fdb7acd57570e6056f5abf
David Montull <david.montull@bt-group.com>
2022-03-08 08:25:49 +0100

    [15.0][MIG] sale_product_template_tags: Migration to 15.0

--- sale_product_template_tags/__manifest__.py
+++ sale_product_template_tags/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Sale Product Template Tags",
     "summary": "Show product tags menu in Sale app",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
