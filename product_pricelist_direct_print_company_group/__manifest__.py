1d3c5af5e91fe30d7fc5f0ed75831fded52dcb99
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-03-23 16:32:59 +0100

    [MIG] product_pricelist_direct_print_company_group: Migration to 14.0

--- product_pricelist_direct_print_company_group/__manifest__.py
+++ product_pricelist_direct_print_company_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product Pricelist Direct Print Company Group",
     "summary": "Print Pricelist items using the company group model",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
