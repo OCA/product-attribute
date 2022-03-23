c5b87d5d34c106f7889eb9bc90e8a582a6f43506
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-03-23 10:42:35 +0100

    [FIX] product_supplierinfo_for_customer: new line removes product and variant in old ones, fixes #986

--- product_supplierinfo_for_customer/models/product_customerinfo.py
+++ product_supplierinfo_for_customer/models/product_customerinfo.py
@@ -11,6 +11,15 @@ class ProductCustomerInfo(models.Model):
 
     name = fields.Many2one(string="Customer", help="Customer of this product")
 
+    def onchange(self, values, field_name, field_onchange):
+        if "product_tmpl_id" in values and field_name != "product_tmpl_id":
+            values.pop(
+                "product_tmpl_id", None
+            )  # otherwise it will be removed from the defaults
+        return super(ProductCustomerInfo, self).onchange(
+            values, field_name, field_onchange
+        )
+
     @api.model
     def get_import_templates(self):
         return [
