6cdf545f65465ab3f2d6b5bee70434794f78c4c6
Alba Riera <alba.riera@creublanca.es>
2022-04-13 08:36:15 +0200

    [IMP] product_expiry_configurable: add activities to warn of expiry dates

--- product_expiry_configurable/tests/test_stock_production_lot.py
+++ product_expiry_configurable/tests/test_stock_production_lot.py
@@ -32,6 +32,7 @@ class TestProductCategory(SavepointCase):
                 "name": "test product",
                 "categ_id": cls.categ_lvl_1_1_1.id,
                 "tracking": "lot",
+                "type": "product",
             }
         )
 
@@ -144,8 +145,8 @@ class TestProductCategory(SavepointCase):
              Set lot life_date after being created.
 
        Expected result:
-            First dates are expected to be computed by the standard odoo method.
-            After adding the life_date, the alert_date, use_date and removal_date
+            First dates are expected to be False.
+            After adding the life_date, the fields alert_date, use_date and removal_date
             should be computed according to 'life_date'.
         """
 
@@ -169,9 +170,7 @@ class TestProductCategory(SavepointCase):
                 }
             )
             for date in _get_dates():
-                self.assertEqual(
-                    fields.Datetime.to_string(getattr(lot, date)), "2022-02-14 10:00:00"
-                )
+                self.assertEqual(fields.Datetime.to_string(getattr(lot, date)), False)
             self.assertFalse(lot.life_date)
 
             with Form(lot) as lot_form:
@@ -181,3 +180,74 @@ class TestProductCategory(SavepointCase):
                         fields.Datetime.to_string(getattr(lot_form, date)),
                         "2022-02-26 10:00:00",
                     )
+
+    def test_cron_life_date_reached(self):
+        """
+         Test case:
+                Create a product with compute_dates_from = 'life_date'
+                and configure durations.
+                Create lot with a past life_date.
+                Run manually cron setting a date after life_date and
+                the other dates.
+
+         Expected result:
+                Activities warning the different lot expiry times
+                should rise.
+            """
+
+        def _get_times():
+            return ["alert_time", "use_time", "removal_time"]
+
+        def _get_dates():
+            return ["life_date", "use_date", "removal_date"]
+
+        self.product.specific_compute_dates_from = "life_date"
+
+        for time in _get_times():
+            setattr(self.product, "specific_%s" % time, 2)
+
+        lot = self.StockProductionLot.create(
+            {
+                "name": "lot1",
+                "product_id": self.product.id,
+                "company_id": self.env.company.id,
+                "life_date": datetime(2021, 2, 28, 10, 0, 0),
+            }
+        )
+
+        stock_location = self.env["stock.location"].create(
+            {"name": "Internal test location", "usage": "internal"}
+        )
+
+        self.env["stock.quant"].create(
+            {
+                "location_id": stock_location.id,
+                "lot_id": lot.id,
+                "quantity": 10,
+                "product_id": self.product.id,
+                "company_id": self.env.company.id,
+            }
+        )
+
+        with freezegun.freeze_time("2022-02-12 10:00:00"):
+            counter = 1
+            for date in _get_dates():
+                self.assertFalse(getattr(lot, "%s_reminded" % date))
+                self.env["stock.production.lot"]._expiry_date_exceeded(date_field=date)
+                activity_id = self.env.ref(
+                    "product_expiry_configurable.mail_activity_type_expiry_date_reached"
+                ).id
+                activity_count = self.env["mail.activity"].search_count(
+                    [
+                        ("activity_type_id", "=", activity_id),
+                        (
+                            "res_model_id",
+                            "=",
+                            self.env.ref("stock.model_stock_production_lot").id,
+                        ),
+                        ("res_id", "=", lot.id),
+                    ]
+                )
+                self.assertTrue(getattr(lot, "%s_reminded" % date))
+                self.assertEqual(activity_count, counter)
+                counter += 1
