7ca554dabe239b5316bbb22368f097665d82ad09
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-16 17:06:15 +0200

    [ADD] product_dimension_compute_volume
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ product_dimension_compute_volume/__manifest__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SCRLfs
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Product Dimension Compute Volume",
+    "summary": """
+        Automatically compute the volume depending on the dimensions.""",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://coopiteasy.be",
+    "author": "Odoo Community Association (OCA), Coop IT Easy SCRLfs",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "product_dimension",
+    ],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
