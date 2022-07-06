cbec8de362dc5b8ec0d2f0f15279599c59f89b42
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-07-06 12:36:26 +0200

    [MIG] product_pricelist_by_contact: Migration to 15.0

--- product_pricelist_by_contact/__manifest__.py
+++ product_pricelist_by_contact/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Pricelist Per Contact",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
