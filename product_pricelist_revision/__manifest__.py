0710a297a73bd075c489a449c006d87a78926bf7
Sébastien BEAU <sebastien.beau@akretion.com>
2022-07-11 22:36:57 +0200

    product_pricelist_revision: fix date_end/date_start are datetime

--- product_pricelist_revision/__manifest__.py
+++ product_pricelist_revision/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Pricelist Revision",
     "summary": "Product Pricelist Revision",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
