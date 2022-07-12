48fe25bb2c24f452c4c9ddd1f07efd102ae57e59
MarcBForgeFlow <marc.belmonte@forgeflow.com>
2022-07-12 11:19:35 +0200

    [ADD] product_category_description

---
+++ product_category_description/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Category Description",
+    "summary": """
+        Allows to add a detailed description for a product category.""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["MarcBForgeFlow"],
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+    "data": ["views/product_category.xml"],
+}
---
+++ product_category_description/readme/CONTRIBUTORS.rst
@@ -0,0 +1,1 @@
+* Marc Belmonte <marc.belmonte@forgeflow.com>
