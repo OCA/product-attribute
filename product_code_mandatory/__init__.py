3d14c6218ae9d77910891227ad214f6250b303eb
augusto-weiss <awe@adhoc.com.ar>
2022-09-15 10:56:33 -0300

    [FIX] product_code_mandatory: align code with Odoo way to set default code

--- product_code_mandatory/__init__.py
+++ product_code_mandatory/__init__.py
@@ -1,17 +1,27 @@
 from . import models
 
+from odoo import api, SUPERUSER_ID
+
 
 def pre_init_product_code(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
     cr.execute(
-        """UPDATE product_template
-        SET default_code = 'DEFAULT' || nextval('ir_default_id_seq')
+        """
+        SELECT product_tmpl_id from product_product
         WHERE default_code is NULL
-        OR LENGTH(default_code) = 0"""
+        OR LENGTH(default_code) = 0
+        GROUP BY product_tmpl_id
+        HAVING COUNT(product_tmpl_id) = 1"""
     )
+    product_template_ids = [x[0] for x in cr.fetchall()]
     cr.execute(
         """UPDATE product_product
         SET default_code = 'DEFAULT' || nextval('ir_default_id_seq')
         WHERE default_code is NULL
         OR LENGTH(default_code) = 0"""
     )
+
+    env["product.template"].browse(product_template_ids)._compute_default_code()
+
     return True
