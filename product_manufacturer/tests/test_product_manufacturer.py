4dbcc1ebdd65bbf43f384e98031a8cf533970e61
Maxime Franco <maxime.franco@acsone.eu>
2022-10-11 09:28:50 +0200

    [IMP] Change test class from SavepointCase to TransactionCase

--- product_manufacturer/tests/test_product_manufacturer.py
+++ product_manufacturer/tests/test_product_manufacturer.py
@@ -1,10 +1,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestProductManufacturer(SavepointCase):
+class TestProductManufacturer(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
