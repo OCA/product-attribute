18ede2590ed1b2527d9741d24de357c8c483ae38
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-21 14:29:22 +0200

    [IMP] product_standard_price_tax_included: black, isort, prettier

--- product_standard_price_tax_included/tests/test_module.py
+++ product_standard_price_tax_included/tests/test_module.py
@@ -11,29 +11,34 @@ class TestProductStandardPriceTaxIncluded(TransactionCase):
     def setUp(self):
         super(TestProductStandardPriceTaxIncluded, self).setUp()
 
-        self.SaleOrder = self.env['sale.order']
-        self.SaleOrderLine = self.env['sale.order.line']
+        self.SaleOrder = self.env["sale.order"]
+        self.SaleOrderLine = self.env["sale.order.line"]
 
         self.tax = self.env.ref(
-            'product_standard_price_tax_included.account_tax_tax_included')
-        self.partner = self.env.ref('base.res_partner_3')
-        self.uom = self.env.ref('uom.product_uom_unit')
+            "product_standard_price_tax_included.account_tax_tax_included"
+        )
+        self.partner = self.env.ref("base.res_partner_3")
+        self.uom = self.env.ref("uom.product_uom_unit")
         self.product = self.env.ref(
-            'product_standard_price_tax_included.product_product')
+            "product_standard_price_tax_included.product_product"
+        )
         self.pricelist = self.env.ref(
-            'product_standard_price_tax_included.pricelist_price_tax_included')
+            "product_standard_price_tax_included.pricelist_price_tax_included"
+        )
         self.pricelist_item = self.env.ref(
-            "product_standard_price_tax_included."
-            "pricelist_item_price_tax_included")
+            "product_standard_price_tax_included." "pricelist_item_price_tax_included"
+        )
         # ensure that the currency of the price list is the same as
         # the main company to avoid error due to undesired conversion
         self.pricelist.currency_id = self.env.ref("base.main_company").currency_id
 
         # Create an Order
-        self.order = self.SaleOrder.create({
-            'partner_id': self.partner.id,
-            'pricelist_id': self.pricelist.id,
-        })
+        self.order = self.SaleOrder.create(
+            {
+                "partner_id": self.partner.id,
+                "pricelist_id": self.pricelist.id,
+            }
+        )
 
     # Test Section
     def test_01_correct_tax_compute(self):
@@ -41,15 +46,19 @@ class TestProductStandardPriceTaxIncluded(TransactionCase):
         based on Price List Tax Included."""
 
         # Create an Order line with a product with Tax Included
-        self.SaleOrderLine.create({
-            'order_id': self.order.id,
-            'product_id': self.product.id,
-            'product_uom_qty': 1,
-            'product_uom': self.uom.id,
-        })
-        self.assertEquals(
-            self.order.amount_total, 12.0,
-            "Computation of Price based on Cost Price Tax Included incorrect.")
+        self.SaleOrderLine.create(
+            {
+                "order_id": self.order.id,
+                "product_id": self.product.id,
+                "product_uom_qty": 1,
+                "product_uom": self.uom.id,
+            }
+        )
+        self.assertEqual(
+            self.order.amount_total,
+            12.0,
+            "Computation of Price based on Cost Price Tax Included incorrect.",
+        )
 
     def test_02_correct_tax_compute_with_discount(self):
         """Test if the total of a sale order is correct with price
@@ -58,13 +67,17 @@ class TestProductStandardPriceTaxIncluded(TransactionCase):
         self.pricelist_item.price_discount = 50
 
         # Create an Order line with a product with Tax Included
-        self.SaleOrderLine.create({
-            'order_id': self.order.id,
-            'product_id': self.product.id,
-            'product_uom_qty': 1,
-            'product_uom': self.uom.id,
-        })
-        self.assertEquals(
-            self.order.amount_total, 6.0,
+        self.SaleOrderLine.create(
+            {
+                "order_id": self.order.id,
+                "product_id": self.product.id,
+                "product_uom_qty": 1,
+                "product_uom": self.uom.id,
+            }
+        )
+        self.assertEqual(
+            self.order.amount_total,
+            6.0,
             "Computation of Price based on Cost Price Tax Included incorrect."
-            " when discount is set")
+            " when discount is set",
+        )
