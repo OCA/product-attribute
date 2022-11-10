bbb2e75ae4f585ce9add82dcdb278e4303c8c95e
Denis Roussel <denis.roussel@acsone.eu>
2022-11-10 14:21:24 +0100

    [IMP] product_abc_classification/base: Restore module name

---
+++ product_abc_classification/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2020 ForgeFlow
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Abc Classification",
+    "summary": """
+        ABC classification for sales and warehouse management""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product", "stock"],
+    "data": [
+        "views/abc_classification_product_level.xml",
+        "views/abc_classification_profile.xml",
+        "views/product_template.xml",
+        "views/product_product.xml",
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+    ],
+}
