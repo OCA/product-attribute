f06fd7f1a4c6adad98cd40a4497da8c0b463d6f1
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-18 17:20:08 +0100

    [MIG] product_pricelist_supplierinfo: Migration to 15.0
    TT36975

--- product_pricelist_supplierinfo/models/product_product.py
+++ product_pricelist_supplierinfo/models/product_product.py
@@ -9,7 +9,7 @@ from odoo import models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    def _prepare_sellers(self, params):
+    def _prepare_sellers(self, params=False):
         """When we override min qty we want that _select_sellers gives us the
         first possible seller for every other criteria ignoring the quantity. As
         supplierinfos are sorted by min_qty descending, we want to revert such
@@ -25,7 +25,7 @@ class ProductProduct(models.Model):
             rule, date=date, quantity=quantity, product_id=self.id
         )
 
-    def price_compute(self, price_type, uom=False, currency=False, company=False):
+    def price_compute(self, price_type, uom=False, currency=False, company=None):
         """Return dummy not falsy prices when computation is done from supplier
         info for avoiding error on super method. We will later fill these with
         correct values.
