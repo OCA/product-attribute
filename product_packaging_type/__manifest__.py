89bca2ef577d36408a0ae7f1e4048e6be0a918d1
Denis Roussel <denis.roussel@acsone.eu>
2022-11-24 13:00:05 +0100

    [MIG][16.0] product_packaging_type

--- product_packaging_type/__manifest__.py
+++ product_packaging_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Packaging Type",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Product",
     "summary": "Product Packaging Type",
