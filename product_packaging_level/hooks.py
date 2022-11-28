4dedd6057c465d462228373632d1767beca16f59
Denis Roussel <denis.roussel@acsone.eu>
2022-11-28 10:15:19 +0100

    [IMP] product_packaging_level: Add migration scripts

---
+++ product_packaging_level/hooks.py
@@ -0,0 +1,25 @@
+# Copyright 2022 ACSONE SA/NV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+from openupgradelib import openupgrade
+
+from odoo import SUPERUSER_ID, api
+
+
+def pre_init_hook(cr):
+    if openupgrade.table_exists(cr, "product_packaging_type"):
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        # Former version of the module is present
+        models = [("product.packaging.type", "product.packaging.level")]
+        openupgrade.rename_models(env.cr, models)
+        fields = [
+            (
+                "product.packaging",
+                "product_packaging",
+                "packaging_level_id",
+                "packaging_level_id",
+            )
+        ]
+        openupgrade.rename_fields(env, fields, no_deep=True)
+
+        modules = [("product_packaging_type", "product_packaging_level")]
+        openupgrade.update_module_names(env.cr, modules, merge_modules=True)
