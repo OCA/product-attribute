66323ff7c5e962b3cfc2c9ae475c86bcd587259d
Mohammad Azeem <mohammad.azeem@dynapps.be>
2022-12-02 17:16:11 +0530

    [MIG] sale_product_template_tags: Migration to 16.0

--- sale_product_template_tags/__manifest__.py
+++ sale_product_template_tags/__manifest__.py
@@ -5,11 +5,11 @@
 {
     "name": "Sale Product Template Tags",
     "summary": "Show product tags menu in Sale app",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
-    "depends": ["product_template_tags", "sale"],
+    "depends": ["sale"],
     "data": ["views/product_template_tag.xml"],
     "maintainers": ["ivantodorovich"],
     "auto_install": True,
