087f887e745c8e28450991e1889826cb19a233c4
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2022-12-13 10:12:14 +0100

    [14.0][IMP] product_categ_image: remove unused field leftover from migration

--- product_categ_image/models/product_category.py
+++ product_categ_image/models/product_category.py
@@ -2,11 +2,9 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class ProductCategory(models.Model):
     _name = "product.category"
     _inherit = [_name, "image.mixin"]
-
-    image = fields.Binary(string="Category Image", attachment=True)
