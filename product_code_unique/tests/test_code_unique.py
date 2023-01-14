d44060a27458b361f50b4844dfd86ddebebf28bb
sergio-teruel <sergio.teruel@tecnativa.com>
2023-01-14 22:25:11 +0100

    [IMP] product_code_unique: Tests more resilient

--- product_code_unique/tests/test_code_unique.py
+++ product_code_unique/tests/test_code_unique.py
@@ -16,13 +16,13 @@ class TestCodeUnique(TransactionCase):
         )
 
     def test_01_check_code_other(self):
-        self.product2 = self.product_obj.create(
-            {"name": "Test Product 2", "default_code": "TSTP2"}
-        )
+        self.product2 = self.product_obj.with_context(
+            check_default_code_unique=True
+        ).create({"name": "Test Product 2", "default_code": "TSTP2"})
 
     def test_02_check_code_unique(self):
         with self.assertRaises(psycopg2.IntegrityError):
             with mute_logger("odoo.sql_db"), self.cr.savepoint():
-                self.product2 = self.product_obj.create(
-                    {"name": "Test Product 2", "default_code": "TSTP1"}
-                )
+                self.product2 = self.product_obj.with_context(
+                    check_default_code_unique=True
+                ).create({"name": "Test Product 2", "default_code": "TSTP1"})
