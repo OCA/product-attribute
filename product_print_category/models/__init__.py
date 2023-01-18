e3837690ec5699d525bb2ff236933d1f4d94efdf
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-01-18 15:43:55 +0100

    [IMP] product_print_category : Add product print category rule to have better default print category values. [REM] remove default_print_category on res.company model

--- product_print_category/models/__init__.py
+++ product_print_category/models/__init__.py
@@ -1,5 +1,5 @@
-from . import res_company
 from . import product_print_category
 from . import product_print_category_mixin
+from . import product_print_category_rule
 from . import product_product
 from . import product_template
