25123ce350444f3adb3e897c1ba1fd3f35ffba70
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-01-18 01:17:14 +0100

    [IMP] Add onchange feature to make the UI more simple for end user and avoid checkbox to be checked during the save of the product. [REF] make the module working if a create is done directly on product.template

--- product_print_category/models/product_print_category_mixin.py
+++ product_print_category/models/product_print_category_mixin.py
@@ -10,19 +10,59 @@ class ProductPrintCategoryMixin(models.AbstractModel):
     _name = "product.print.category.mixin"
     _description = "Abstract Model for Product Print Categories"
 
-    def _update_to_print_values(self, vals):
-        # This function work for item that are product.product and product.template
-        to_update_item_ids = []
-        # Set 'To print' if we change one field choosen in print_category
-        for item in self.filtered(lambda x: x.print_category_id):
-            triggering_fields = item.print_category_id.field_ids.mapped("name") + [
-                "print_category_id"
-            ]
-            if len(list(set(vals.keys()) & set(triggering_fields))):
-                to_update_item_ids.append(item.id)
-        to_update_items = self.browse(to_update_item_ids)
-        # This function is called from the write of the current RecordSet :
-        # prevent an infinite loop by calling the function super
-        return super(ProductPrintCategoryMixin, to_update_items).write(
-            {"to_print": True}
+    def write(self, vals):
+        ProductProduct = self.env["product.product"].with_context(
+            active_test=False, to_print_ok=True
         )
+        ProductPrintCategory = self.env["product.print.category"]
+
+        if "to_print" in vals or self.env.get("to_print_ok", False):
+            # "to_print" is explicitely set, nothing to do
+            # Or it is handled previously
+            return super(
+                ProductPrintCategoryMixin, self.with_context(to_print_ok=True)
+            ).write(vals)
+
+        if "print_category_id" in vals:
+            # print_category_id changed, we set
+            # to print, if a category is set
+            # not to print if no category is set
+            vals["to_print"] = bool(vals["print_category_id"])
+            return super(
+                ProductPrintCategoryMixin, self.with_context(to_print_ok=True)
+            ).write(vals)
+
+        # Here we have to group by print_category_id
+        # and for each print_category_id we check if related fields changed
+        res = super(
+            ProductPrintCategoryMixin, self.with_context(to_print_ok=True)
+        ).write(vals)
+
+        if self._name == "product.template":
+            domain = [
+                ("product_tmpl_id", "in", self.ids),
+                ("print_category_id", "!=", False),
+            ]
+        elif self._name == "product.product":
+            domain = [
+                ("print_category_id", "!=", False),
+            ]
+        else:
+            raise NotImplementedError()
+        product_groups = ProductProduct.read_group(
+            domain=domain, fields=["print_category_id"], groupby="print_category_id"
+        )
+        products_to_update = ProductProduct
+        for product_group in product_groups:
+
+            category = ProductPrintCategory.browse(
+                product_group["print_category_id"][0]
+            )
+            triggering_fields = category.field_ids.mapped("name")
+            if len(list(set(vals.keys()) & set(triggering_fields))):
+                # Value present in the label changed
+                products_to_update |= ProductProduct.search(product_group["__domain"])
+
+        if products_to_update:
+            products_to_update.write({"to_print": True})
+        return res
