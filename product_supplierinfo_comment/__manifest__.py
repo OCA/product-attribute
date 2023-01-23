4f7252b8d73a5f1a9efea2d298de37c1e24a8ab1
sergio-teruel <sergio.teruel@tecnativa.com>
2023-01-23 15:17:01 +0100

    [ADD] product_supplierinfo_comment: New module

---
+++ product_supplierinfo_comment/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Supplier Info Comment",
+    "summary": """
+        Add the comment field to the product supplier info
+        """,
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "depends": ["product"],
+    "data": ["views/product_views.xml"],
+    "installable": True,
+}
