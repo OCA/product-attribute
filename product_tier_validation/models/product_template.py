d386a14b531a219b0c89d5636378ce4c000ca4aa
Kev-Roche <kevin.roche@akretion.com>
2023-02-27 15:06:51 +0100

    [14.0][IMP] product_tier_validation state_to default values

--- product_tier_validation/models/product_template.py
+++ product_tier_validation/models/product_template.py
@@ -1,13 +1,24 @@
 # Copyright 2021 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import _, api, models, tools
 from odoo.exceptions import UserError
+from odoo.tools import config
+
 
 class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = ["product.template", "tier.validation"]
     _tier_validation_manual_config = False
+
+    @property
+    @tools.ormcache()
+    def _state_to(self):
+        return (
+            self.env["product.state"]
+            .search([("code", "not in", self._state_from + [self._cancel_state])])
+            .mapped("code")
+        )
 
     def write(self, vals):
         # Tier Validation does not work with Stages, only States :-(
@@ -24,9 +35,10 @@ class ProductTemplate(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         states = self.env["product.state"].search([("code", "in", self._state_from)])
-        if not states:
-            raise UserError("Wrong configuration of '_state_from' in product.state")
-        for vals in vals_list:
-            if vals.get("product_state_id") not in states.ids:
-                vals["product_state_id"] = states[0].id
+        if not states and not config["test_enable"]:
+            raise UserError(_("Wrong configuration of '_state_from' in product.state"))
+        if states:
+            for vals in vals_list:
+                if vals.get("product_state_id") not in states.ids:
+                    vals["product_state_id"] = states[0].id
         return super().create(vals_list)
