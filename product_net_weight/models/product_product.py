babf3111340a3049c50852e3e5c5ed8615dc8e8d
sergio-teruel <sergio.teruel@tecnativa.com>
2023-03-01 22:57:31 +0100

    [IMP] product_net_weight: Adapt module like Odoo manage weight field with multiple variants

--- product_net_weight/models/product_product.py
+++ product_net_weight/models/product_product.py
@@ -2,7 +2,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductProduct(models.Model):
@@ -15,3 +16,11 @@ class ProductProduct(models.Model):
 
     # Explicit field, renaming it
     weight = fields.Float(string="Gross Weight")
+
+    @api.constrains("net_weight", "weight")
+    def _check_net_weight(self):
+        for product in self:
+            if product.weight and product.net_weight > product.weight:
+                raise ValidationError(
+                    _("The net weight of product must be lower than gross weight.")
+                )
