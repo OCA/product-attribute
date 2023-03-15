d6e841d87a68b450929db6e79b6c48df91680dbf
sergio-teruel <sergio.teruel@tecnativa.com>
2023-03-15 14:02:41 +0100

    [IMP] product_secondary_unit: Access to template secondary_uom_ids from variants

--- product_secondary_unit/models/product_product.py
+++ product_secondary_unit/models/product_product.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductProduct(models.Model):
@@ -12,4 +12,24 @@ class ProductProduct(models.Model):
         string="Secondary Unit of Measure",
         help="Default Secondary Unit of Measure.",
         context={"active_test": False},
+        compute="_compute_secondary_uom_ids",
+        inverse="_inverse_secondary_uom_ids",
     )
+
+    @api.depends("product_tmpl_id")
+    def _compute_secondary_uom_ids(self):
+        for variant in self:
+            variant.secondary_uom_ids = (
+                variant.product_tmpl_id.secondary_uom_ids.filtered(
+                    lambda s: s.product_id == variant or not s.product_id
+                )
+            )
+
+    def _inverse_secondary_uom_ids(self):
+        for variant in self:
+            variant.product_tmpl_id.secondary_uom_ids = (
+                variant.product_tmpl_id.secondary_uom_ids.filtered(
+                    lambda s: s.product_id != variant
+                )
+                + variant.secondary_uom_ids
+            )
