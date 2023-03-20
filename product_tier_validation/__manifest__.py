174e52c483f41096f953802ea7c2b927303cfae0
Miquel Rosell <miquelrosell99@gmail.com>
2023-03-20 13:37:43 +0100

    Change version to 15

--- product_tier_validation/__manifest__.py
+++ product_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product Tier Validation",
     "summary": "Support a tier validation process for Products",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/OCA/product-attribute",
     "category": "Products",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
