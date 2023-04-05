18ff67c8006ad4ba73f5b309c596e2191a7167b4
jumeldi74 <jumeldi74@gmail.com>
2023-04-05 15:26:42 +0700

    [ADD] product_access_right: add new module

---
+++ product_access_right/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Access Right",
+    "version": "16.0.1.0.2",
+    "license": "AGPL-3",
+    "summary": "Access Right of Product",
+    "author": "Jumeldi, PT.SAI, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": ["purchase"],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
---
+++ product_access_right/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module add access right for group Purchase and Accounting to view certain kind of field in Product.
+
+The module does not provide in itself any specific usage of the estimates.
