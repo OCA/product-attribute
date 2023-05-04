d63e46833c8eface6955d7c8361347e54f4fdcda
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-05-04 18:20:21 +0200

    [REF] product_attribute_company_favorite : dynamic name for the ir.config.parameter to enable creation for all the companies

--- product_attribute_company_favorite/models/product_attribute.py
+++ product_attribute_company_favorite/models/product_attribute.py
@@ -4,7 +4,3 @@ from odoo import models
 class ProductAttribute(models.Model):
     _inherit = ["product.attribute", "product.attribute.favorite.mixin"]
     _name = "product.attribute"
-
-    CREATION_ACROSS_COMPANY_CONFIG = (
-        "product_attribute_company_favorite.new_attribute_favorite_for_all_companies"
-    )
--- product_attribute_company_favorite/models/product_attribute_value.py
+++ product_attribute_company_favorite/models/product_attribute_value.py
@@ -4,8 +4,3 @@ from odoo import models
 class ProductAttributeValue(models.Model):
     _inherit = ["product.attribute.value", "product.attribute.favorite.mixin"]
     _name = "product.attribute.value"
-
-    CREATION_ACROSS_COMPANY_CONFIG = (
-        "product_attribute_company_favorite."
-        "new_attribute_value_favorite_for_all_companies"
-    )
