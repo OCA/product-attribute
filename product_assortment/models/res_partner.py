26635bb2b47563448b4cc77fe64899fd7252074c
Carlos Dauden <carlos.dauden@tecnativa.com>
2023-06-05 18:10:34 +0200

    [IMP] product_assortment: New field applied_assortments_ids to avoid cache "inefficiency" using workers because cache is isolated by worker

--- product_assortment/models/res_partner.py
+++ product_assortment/models/res_partner.py
@@ -1,11 +1,18 @@
 # Copyright 2021 Tecnativa - Carlos Roca
 # Copyright 2021 Tecnativa - Carlos Dauden
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-from odoo import models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+
+    applied_assortment_ids = fields.Many2many(
+        comodel_name="ir.filters",
+        relation="ir_filter_all_partner_rel",
+        column1="partner_id",
+        column2="filter_id",
+    )
 
     def action_define_product_assortment(self):
         self.ensure_one()
@@ -26,9 +33,29 @@ class ResPartner(models.Model):
         action["context"] = ctx
         return action
 
+    def _update_partner_assortments(self):
+        # Using sudo to contemplate evaluation of domains with restricted fields
+        self = self.sudo()
+        assortments = self.env["ir.filters"].search([("is_assortment", "=", True)])
+        for partner in self:
+            # Use ids instead of record to improve performance (Remove in next versions)
+            partner_assortment_ids = []
+            for assortment in assortments:
+                if partner in assortment.partner_ids or partner.filtered_domain(
+                    assortment._get_eval_partner_domain()
+                ):
+                    partner_assortment_ids.append(assortment.id)
+            partner.applied_assortment_ids = assortments.browse(partner_assortment_ids)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        partners = super().create(vals_list)
+        self._update_partner_assortments()
+        return partners
+
     def write(self, vals):
         res = super().write(vals)
         IrFilters = self.env["ir.filters"]
         if IrFilters.get_partner_domain_fields() & set(vals.keys()):
-            IrFilters.clear_caches()
+            self._update_partner_assortments()
         return res
