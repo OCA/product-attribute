819e9b1eebe6f08b8b7a2d1ded39cf24acf5c688
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-22 23:45:06 +0200

    [MIG] product_uom_use_type in 16.0 (from 12.0) - adapt demo data - adapt test, using Form - improve test coverage - adapt the same code for product.product and product.template - bump module version - update pot and french translation

--- product_uom_use_type/models/product_product.py
+++ product_uom_use_type/models/product_product.py
@@ -8,7 +8,22 @@ from odoo import api, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    @api.onchange("uom_id", "uom_po_id")
+    @api.onchange("uom_id")
+    def _onchange_uom_id(self):
+        # If it's an UoM dedicated to sale, We can't set is as uom_po_id
+        # So prevent to call super(), but check if categories are consistent
+        if self.uom_id.use_type == "sale":
+            if self.uom_id.category_id != self.uom_po_id.category_id:
+                self.uom_po_id = False
+            return
+        return super()._onchange_uom_id()
+
+    @api.onchange("uom_po_id")
     def _onchange_uom(self):
-        if self.uom_id.use_type != "sale":
-            super(ProductProduct, self)._onchange_uom()
+        # If it's an PO UoM dedicated to purchase, We can't set is as uom_sid
+        # So prevent to call super(), but check if categories are consistent
+        if self.uom_po_id.use_type == "purchase":
+            if self.uom_id.category_id != self.uom_po_id.category_id:
+                self.uom_id = False
+            return
+        return super()._onchange_uom()
