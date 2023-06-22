fe138ac9696d448f64ba39d0479bac66a8965428
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-22 23:45:06 +0200

    [MIG] product_uom_use_type in 16.0 (from 12.0) - adapt demo data - adapt test, using Form - improve test coverage - adapt the same code for product.product and product.template - bump module version - update pot and french translation

--- product_uom_use_type/__manifest__.py
+++ product_uom_use_type/__manifest__.py
@@ -3,12 +3,13 @@
 {
     "name": "Product UoM - Use Type",
     "summary": "Define UoM for Sale and / or for Purchase purpose",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "GRAP, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
     "license": "AGPL-3",
-    "depends": ["product"],
+    "depends": ["product", "uom"],
     "data": ["views/view_product_uom.xml"],
     "demo": [
         "demo/product_uom.xml",
--- product_uom_use_type/models/product_template.py
+++ product_uom_use_type/models/product_template.py
@@ -2,7 +2,8 @@
 # @author: Quentin DUPONT <quentin.dupont@grap.coop>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -14,5 +15,32 @@ class ProductTemplate(models.Model):
 
     @api.onchange("uom_id")
     def _onchange_uom_id(self):
-        if self.uom_id.use_type != "sale":
-            super(ProductTemplate, self)._onchange_uom_id()
+        # If it's an UoM dedicated to sale, We can't set is as uom_po_id
+        # So prevent to call super(), but check if categories are consistent
+        if self.uom_id.use_type == "sale":
+            if self.uom_id.category_id != self.uom_po_id.category_id:
+                self.uom_po_id = False
+            return
+        return super()._onchange_uom_id()
+
+    @api.onchange("uom_po_id")
+    def _onchange_uom(self):
+        # If it's an PO UoM dedicated to purchase, We can't set is as uom_sid
+        # So prevent to call super(), but check if categories are consistent
+        if self.uom_po_id.use_type == "purchase":
+            if self.uom_id.category_id != self.uom_po_id.category_id:
+                self.uom_id = False
+            return
+        return super()._onchange_uom()
+
+    @api.constrains("uom_id", "uom_po_id")
+    def _check_uom(self):
+        if self.filtered(lambda x: x.uom_id.use_type == "purchase"):
+            raise ValidationError(
+                _("you can not set a 'Purchase' UoM as the main product UoM.")
+            )
+        if self.filtered(lambda x: x.uom_po_id.use_type == "sale"):
+            raise ValidationError(
+                _("you can not set a 'Sale' UoM as the product Purchase UoM.")
+            )
+        return super()._check_uom()
