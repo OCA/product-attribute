9cb91a4dd04a1b366b6c4f86377e0bd6b92508dd
François Honoré <francois.honore@acsone.eu>
2023-07-12 11:25:49 +0200

    [IMP] product_special_type: pre-commit stuff

--- product_special_type/__manifest__.py
+++ product_special_type/__manifest__.py
@@ -1,25 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2012-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Product Special Types',
-    'version': '10.0.1.0.0',
-    'author': 'Camptocamp,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'category': 'Product',
-    'development_status': 'Beta',
-    'maintainers': ['rousseldenis'],
-    'summary':
-    '''
+    "name": "Product Special Types",
+    "version": "10.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Product",
+    "development_status": "Beta",
+    "maintainers": ["rousseldenis"],
+    "summary": """
         Add a special type selection on products.
-    ''',
-    'website': 'https://github.com/OCA/product-attribute',
-    'depends': [
-        'product'
+    """,
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+    "data": [
+        "views/product_template.xml",
     ],
-    'data': [
-        'views/product_template.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
