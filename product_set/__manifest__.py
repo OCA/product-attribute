947e26de0d9c1f517ddd98cf5f932b6d14f6756d
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-07-13 11:09:55 +0000

    Add product_set
    [UPD] Update product_set.pot
    
    [UPD] README.rst
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: product-attribute-16.0/product-attribute-16.0-product_set
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-16-0/product-attribute-16-0-product_set/es/
    
    [UPD] README.rst
    
    Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: product-attribute-16.0/product-attribute-16.0-product_set
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-16-0/product-attribute-16-0-product_set/pt_BR/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: product-attribute-16.0/product-attribute-16.0-product_set
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-16-0/product-attribute-16-0-product_set/
    
    Translated using Weblate (Italian)
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: product-attribute-16.0/product-attribute-16.0-product_set
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-16-0/product-attribute-16-0-product_set/it/

---
+++ product_set/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2015 Anybox
+# Copyright 2018 Camptocamp, ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product set",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "author": "Anybox, Odoo Community Association (OCA)",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/rule_product_set.xml",
+        "views/product_set.xml",
+        "views/product_set_line.xml",
+    ],
+    "demo": ["demo/product_set.xml", "demo/product_set_line.xml"],
+    "installable": True,
+}
---
+++ product_set/readme/USAGE.rst
@@ -0,0 +1,1 @@
+To use this module, you need to install subsequent modules like `sale_product_set` and check their instructions.
