fc5b8d513d704fc35d248812b98ea6353c48b28a
François Honoré <francois.honore@acsone.eu>
2023-07-24 08:30:04 +0200

    [IMP] product_origin: pre-commit stuff

--- product_origin/__manifest__.py
+++ product_origin/__manifest__.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Origin',
-    'summary': '''
-        Adds the origin of the product''',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-attribute',
-    'maintainers': ['rousseldenis'],
-    'depends': [
-        'product',
+    "name": "Product Origin",
+    "summary": """
+        Adds the origin of the product""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "maintainers": ["rousseldenis"],
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_template.xml',
+    "data": [
+        "views/product_template.xml",
     ],
 }
--- product_origin/models/product_template.py
+++ product_origin/models/product_template.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
 
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     country_id = fields.Many2one(
-        comodel_name='res.country',
-        string='Country of Origin',
-        ondelete='restrict',
+        comodel_name="res.country",
+        string="Country of Origin",
+        ondelete="restrict",
         index=True,
     )
 
     state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string='Country State of Origin',
-        ondelete='restrict',
+        comodel_name="res.country.state",
+        string="Country State of Origin",
+        ondelete="restrict",
         index=True,
     )
