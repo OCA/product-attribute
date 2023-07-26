6b76af77bae5a7d3fc016fec4d46e08bc8c37ae7
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2023-07-26 09:50:12 +0930

    [MIG] product_state_active: Migration to 16.0
    Signed-off-by: michaelslade <48970841+michaelslade54@users.noreply.github.com>

--- product_state_active/__manifest__.py
+++ product_state_active/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product State Active",
     "summary": """
         Allows to define option on product state to activate or inactivate product""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["rousseldenis"],
