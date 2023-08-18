aed35acd13c6f587f321ec7a01394cee3003969b
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2023-08-18 14:20:14 +0930

    product_template_tags migration

---
+++ product_template_tags/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Product Template Tags",
+    "summary": "Migration script for product template tags, now inbuilt into Odoo",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "installable": False,
+    "application": False,
+    "auto_install": False,
+}
