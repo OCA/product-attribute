d1dc1d49ad5f5647e949dcd2e25a592720222487
Henrik Norlin <henrik.norl@gmail.com>
2023-08-30 21:37:00 +0200

    [ADD] partner_product_price

---
+++ partner_product_price/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Contact -> Products with the contact's prices",
+    "summary": "",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Product",
+    "data": [
+        "data/ir_actions_server.xml",
+        "views/product_product_views.xml",
+    ],
+    "depends": [
+        "product",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["ows-cloud"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/product-attribute",
+}
