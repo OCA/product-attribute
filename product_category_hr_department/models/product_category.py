53176bd283efde0c9ac0b066cd3b37fc40e839bd
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2023-09-05 12:22:17 -0600

    [ADD] product_category_hr_department

---
+++ product_category_hr_department/models/product_category.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+
+    hr_department_id = fields.Many2one("hr.department", required=False)
