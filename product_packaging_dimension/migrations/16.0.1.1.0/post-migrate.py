a61099a19cd084c02911bee1142c47e8cddc2da5
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2023-10-02 09:29:39 +0200

    [FIX] product_packaging_dimension: post-migrate script should only apply query if column max_weight exists. This column is no longer defined in 16.0

--- product_packaging_dimension/migrations/16.0.1.1.0/post-migrate.py
+++ product_packaging_dimension/migrations/16.0.1.1.0/post-migrate.py
@@ -7,14 +7,15 @@ def _move_max_weight(env):
     """
     Move max_weight value to weight if not defined
     """
-    query = """
-        UPDATE product_packaging
-            SET weight = max_weight
-            WHERE weight IS NULL OR weight = 0.0
-            AND max_weight IS NOT NULL
-            AND product_id IS NOT NULL
-    """
-    openupgrade.logged_query(env.cr, query)
+    if openupgrade.column_exists(env.cr, "product_packaging", "max_weight"):
+        query = """
+            UPDATE product_packaging
+                SET weight = max_weight
+                WHERE weight IS NULL OR weight = 0.0
+                AND max_weight IS NOT NULL
+                AND product_id IS NOT NULL
+        """
+        openupgrade.logged_query(env.cr, query)
 
 
 @openupgrade.migrate()
