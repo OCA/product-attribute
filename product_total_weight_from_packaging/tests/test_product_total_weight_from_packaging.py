204d6d1334a94f6526144056d53d9b14577e5f87
duongtq <duongtq@trobz.com>
2023-10-05 10:12:28 +0700

    [IMP] product_total_weight_from_packaging: Convert the total weight to product's uom

--- product_total_weight_from_packaging/tests/test_product_total_weight_from_packaging.py
+++ product_total_weight_from_packaging/tests/test_product_total_weight_from_packaging.py
@@ -10,10 +10,10 @@ class TestProductTotalWeightFromPackaging(common.TransactionCase):
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.product = cls.env.ref("product.product_product_20")
         cls.product.weight = 5
-        cls.env["product.packaging"].create(
+        cls.pair_packaging = cls.env["product.packaging"].create(
             {"name": "pair", "product_id": cls.product.id, "qty": 2, "weight": 12.5}
         )
-        cls.env["product.packaging"].create(
+        cls.cardbox_packaging = cls.env["product.packaging"].create(
             {
                 "name": "cardbox",
                 "product_id": cls.product.id,
@@ -21,10 +21,30 @@ class TestProductTotalWeightFromPackaging(common.TransactionCase):
                 "weight": 55,
             }
         )
-        cls.env["product.packaging"].create(
+        cls.pallet_packaging = cls.env["product.packaging"].create(
             {"name": "pallet", "product_id": cls.product.id, "qty": 200}
         )
 
     def test_weight_from_packaging(self):
         weight = self.product.get_total_weight_from_packaging(259)
         self.assertEqual(weight, 25 * 55 + 4 * 12.5 + 5)
+
+    def test_weight_in_different_uom(self):
+        # Test with product in gram uom
+        self.product.product_tmpl_id.write(
+            {
+                "weight_uom_id": self.env.ref("uom.product_uom_gram").id,
+            }
+        )
+        weight = self.product.get_total_weight_from_packaging(259)
+        self.assertEqual(weight, (25 * 55 + 4 * 12.5 + 5) * 1000)
+
+        # Test with 1 packaging in gram uom
+        self.pair_packaging.write(
+            {
+                "weight_uom_id": self.env.ref("uom.product_uom_gram").id,
+                "weight": 10000,
+            }
+        )
+        weight = self.product.get_total_weight_from_packaging(259)
+        self.assertEqual(weight, (25 * 55 + 5) * 1000 + (4 * 10000))
