0b1de3ecd2df8a9bfb70ed4ce916c499137b3ea6
duongtq <duongtq@trobz.com>
2023-10-05 10:12:28 +0700

    [IMP] product_total_weight_from_packaging: Convert the total weight to product's uom

--- product_total_weight_from_packaging/models/product.py
+++ product_total_weight_from_packaging/models/product.py
@@ -15,12 +15,21 @@ class ProductProduct(models.Model):
                 "_packaging_values_handler": self._prepare_qty_by_packaging_values_with_weight,  # noqa
             }
         ).product_qty_by_packaging(qty)
-        total_weight = sum(
-            [
-                pck.get("qty", 0) * pck.get("weight", 0)
-                for pck in qty_by_packaging_with_weight
-            ]
-        )
+        # Convert to the uom of the product
+        total_weight = 0
+
+        for packaging in qty_by_packaging_with_weight:
+            weight_uom_id = packaging.get("weight_uom_id")
+            quantity = packaging.get("qty", 0)
+            weight = packaging.get("weight", 0)
+
+            uom = self.env["uom.uom"].browse(weight_uom_id)
+            weight_in_product_uom = uom._compute_quantity(
+                qty=quantity * weight,
+                to_unit=self.product_tmpl_id.weight_uom_id,
+                round=False,
+            )
+            total_weight += weight_in_product_uom
         return total_weight
 
     def _prepare_qty_by_packaging_values_with_weight(
@@ -30,8 +39,10 @@ class ProductProduct(models.Model):
             "qty": qty_per_pkg,
         }
         if packaging_tuple.is_unit:
-            res["weight"] = self.weight
+            res["weight"] = self.product_weight
+            res["weight_uom_id"] = self.product_tmpl_id.weight_uom_id.id
         else:
             packaging = self.env["product.packaging"].browse(packaging_tuple.id)
             res["weight"] = packaging.weight
+            res["weight_uom_id"] = packaging.weight_uom_id.id
         return res
