cdd51fa1c9eaf57377037e110e0ee39758abde9f
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2023-10-10 12:50:14 +0200

    [ADD] product_code_regex_validation

---
+++ product_code_regex_validation/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Code RegEx Validation",
+    "summary": "Configure regEx validation for product codes.",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+    "data": ["views/product_category.xml"],
+    "installable": True,
+}
