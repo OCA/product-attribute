a108f21268527bc44d00403fa3cd556d6f6ade4c
Lois Rilo <lois.rilo@forgeflow.com>
2023-10-16 11:42:24 +0200

    [MIG] product_pricelist_item_list_view: Migration to 16.0

--- product_pricelist_item_list_view/__manifest__.py
+++ product_pricelist_item_list_view/__manifest__.py
@@ -1,18 +1,21 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Pricelist rules list view",
     "summary": "View and search the list of pricelist items",
-    "version": "9.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/product-attribute",
-    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "author": "Agile Business Group, ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["LoisRForgeFlow"],
+    "development_status": "Production/Stable",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
     "depends": [
-        "product",
+        "sale",
     ],
     "data": [
         "views/pricelist_view.xml",
