34ebc61bf16728d8b77b8c371dda91e053374445
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-23 17:02:30 +0200

    [IMP] product_pricelist_direct_print: test performance improvement - Include context keys for avoiding mail operations overhead.

--- product_pricelist_direct_print/tests/test_product_pricelist_direct_print.py
+++ product_pricelist_direct_print/tests/test_product_pricelist_direct_print.py
@@ -10,7 +10,16 @@ class TestProductPricelistDirectPrint(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestProductPricelistDirectPrint, cls).setUpClass()
-
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         # Set report layout to void to wizard selection layout crashes the test
         report_layout = cls.env.ref("web.report_layout_standard")
         main_company = cls.env.ref("base.main_company")
