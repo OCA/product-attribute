ca1ee86e2a1c00cd6b8f0a016faef4c87a777789
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-23 17:10:10 +0200

    [IMP] product_supplierinfo_for_customer: test performance improvement

--- product_supplierinfo_for_customer/tests/test_product_supplierinfo_for_customer.py
+++ product_supplierinfo_for_customer/tests/test_product_supplierinfo_for_customer.py
@@ -10,6 +10,16 @@ class TestProductSupplierinfoForCustomer(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.supplierinfo_model = cls.env["product.supplierinfo"]
         cls.customerinfo_model = cls.env["product.customerinfo"]
         cls.pricelist_item_model = cls.env["product.pricelist.item"]
