83c8b707c7d7fff17dbb7099ac5c539815f3a8fd
Nils Coenen <74965194+NICO-SOLUTIONS@users.noreply.github.com>
2023-11-21 16:52:49 +0100

    [MIG] product_state: Migration to 17.0

--- product_state/__init__.py
+++ product_state/__init__.py
@@ -3,14 +3,11 @@
 from . import models
 
 
-from odoo import api, SUPERUSER_ID
-
-
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """This hook is used to add a state on existing products
     when module product_state is installed.
     """
-    env = api.Environment(cr, SUPERUSER_ID, {})
+
     product_without_state = env["product.template"].search([("state", "=", False)])
     product_without_state.write({"state": "sellable"})
     product_without_state = env["product.template"].search(
