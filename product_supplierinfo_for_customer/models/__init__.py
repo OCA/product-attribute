89942236e6bc30583a3851ad35185a187ca40a44
Akim Juillerat <akim.juillerat@camptocamp.com>
2023-12-08 20:19:03 +0100

    Reimplement product name_get for customer info
    Do not force context to allow selection of supplier or customer info
    Ease maintenance by remove hack on search + read

--- product_supplierinfo_for_customer/models/__init__.py
+++ product_supplierinfo_for_customer/models/__init__.py
@@ -1,6 +1,5 @@
 from . import product_pricelist
 from . import product_customerinfo
-from . import product_supplierinfo
 from . import product_product
 from . import product_template
 from . import res_partner
--- product_supplierinfo_for_customer/models/product_product.py
+++ product_supplierinfo_for_customer/models/product_product.py
@@ -11,56 +11,85 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def name_get(self):
-        res = super(ProductProduct, self.with_context(customerinfo=True)).name_get()
-        return res
+        if not self.env.context.get("customerinfo"):
+            return super().name_get()
+    
+        # Copied from Odoo with following changes:
+        # s/supplier/customer
+        # s/seller/buyer
+        def _name_get(d):
+            name = d.get('name', '')
+            code = self._context.get('display_default_code', True) and d.get('default_code', False) or False
+            if code:
+                name = '[%s] %s' % (code,name)
+            return (d['id'], name)
 
-    @api.model
-    def _name_search(
-        self, name="", args=None, operator="ilike", limit=100, name_get_uid=None
-    ):
-        res = super(ProductProduct, self)._name_search(
-            name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid
-        )
-        res_ids = list(res)
-        res_ids_len = len(res_ids)
-        if not limit or res_ids_len >= limit:
-            limit = (limit - res_ids_len) if limit else False
-        if (
-            not name
-            and limit
-            or not self._context.get("partner_id")
-            or res_ids_len >= limit
-        ):
-            return res_ids
-        limit -= res_ids_len
-        customerinfo_ids = self.env["product.customerinfo"]._search(
-            [
-                ("partner_id", "=", self._context.get("partner_id")),
-                "|",
-                ("product_code", operator, name),
-                ("product_name", operator, name),
-            ],
-            limit=limit,
-            access_rights_uid=name_get_uid,
-        )
-        if not customerinfo_ids:
-            return res_ids
-        res_templates = self.browse(res_ids).mapped("product_tmpl_id")
-        product_tmpls = (
-            self.env["product.customerinfo"]
-            .browse(customerinfo_ids)
-            .mapped("product_tmpl_id")
-            - res_templates
-        )
-        product_ids = list(
-            self._search(
-                [("product_tmpl_id", "in", product_tmpls.ids)],
-                limit=limit,
-                access_rights_uid=name_get_uid,
-            )
-        )
-        res_ids.extend(product_ids)
-        return res_ids
+        partner_id = self._context.get('partner_id')
+        if partner_id:
+            partner_ids = [partner_id, self.env['res.partner'].browse(partner_id).commercial_partner_id.id]
+        else:
+            partner_ids = []
+        company_id = self.env.context.get('company_id')
+
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        self.check_access_rights("read")
+        self.check_access_rule("read")
+
+        result = []
+
+        # Prefetch the fields used by the `name_get`, so `browse` doesn't fetch other fields
+        # Use `load=False` to not call `name_get` for the `product_tmpl_id`
+        self.sudo().read(['name', 'default_code', 'product_tmpl_id'], load=False)
+        product_template_ids = self.sudo().mapped('product_tmpl_id').ids
+
+        if partner_ids:
+            customer_info = self.env['product.customerinfo'].sudo().search([
+                ('product_tmpl_id', 'in', product_template_ids),
+                ('partner_id', 'in', partner_ids),
+            ])
+            # Prefetch the fields used by the `name_get`, so `browse` doesn't fetch other fields
+            # Use `load=False` to not call `name_get` for the `product_tmpl_id` and `product_id`
+            customer_info.sudo().read(['product_tmpl_id', 'product_id', 'product_name', 'product_code'], load=False)
+            customer_info_by_template = {}
+            for r in customer_info:
+                customer_info_by_template.setdefault(r.product_tmpl_id, []).append(r)
+        for product in self.sudo():
+            variant = product.product_template_attribute_value_ids._get_combination_name()
+
+            name = variant and "%s (%s)" % (product.name, variant) or product.name
+            buyers = self.env['product.customerinfo'].sudo().browse(self.env.context.get('buyer_id')) or []
+            if not buyers and partner_ids:
+                product_customer_info = customer_info_by_template.get(product.product_tmpl_id, [])
+                buyers = [x for x in product_customer_info if x.product_id and x.product_id == product]
+                if not buyers:
+                    buyers = [x for x in product_customer_info if not x.product_id]
+                # Filter out sellers based on the company. This is done afterwards for a better
+                # code readability. At this point, only a few sellers should remain, so it should
+                # not be a performance issue.
+                if company_id:
+                    buyers = [x for x in buyers if x.company_id.id in [company_id, False]]
+            if buyers:
+                for s in buyers:
+                    buyer_variant = s.product_name and (
+                        variant and "%s (%s)" % (s.product_name, variant) or s.product_name
+                        ) or False
+                    mydict = {
+                              'id': product.id,
+                              'name': buyer_variant or name,
+                              'default_code': s.product_code or product.default_code,
+                              }
+                    temp = _name_get(mydict)
+                    if temp not in result:
+                        result.append(temp)
+            else:
+                mydict = {
+                          'id': product.id,
+                          'name': name,
+                          'default_code': product.default_code,
+                          }
+                result.append(_name_get(mydict))
+        return result
 
     def _get_price_from_customerinfo(self, partner_id):
         self.ensure_one()
