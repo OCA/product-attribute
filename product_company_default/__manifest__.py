0421eb04bbf7e22a6060dfb9f29e238c628ba9a5
CarlosRiquelme <tato26.09@gmail.com>
2024-01-12 12:34:02 -0300

    [IMP] product_company_default: black, isort, prettier

---
+++ product_company_default/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Company Default",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product"],
+    "license": "AGPL-3",
+    "data": ["data/ir_config_parameter.xml"],
+    "demo": ["demo/demo_ir_config_parameter.xml"],
+    "installable": True,
+}
