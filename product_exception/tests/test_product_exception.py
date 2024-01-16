99469142c91f42e57059f568e03dc635d315bbd7
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2024-01-16 16:42:40 +0100

    [IMP]product_exception: various improvements - Raise Validation Error on Creation/Write if there is one Exception for the Product Template/Variant. - Adapt tests to include new cases - Rename Exception Rules view file and improve structure + namings - Remove `test_button` method which was pointless to have, and replace it with button to manually trigger the Exceptions detection

--- product_exception/tests/test_product_exception.py
+++ product_exception/tests/test_product_exception.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
 
@@ -25,3 +26,36 @@ class TestProductException(TransactionCase):
         self.assertTrue(len(product.exception_ids) == 0)
         product.check_product_template_cron()
         self.assertTrue(len(product.exception_ids) == 1)
+
+    def test_check_error_raises_on_create_with_field_to_check(self):
+        exception = self.env.ref("product_exception.excep_no_dump")
+        exception.active = True
+        with self.assertRaises(ValidationError):
+            self.env["product.template"].with_context(
+                test_product_check_exception=True
+            ).create(
+                {
+                    "name": "Test Product",
+                    "type": "product",
+                    "lst_price": 5,
+                    "standard_price": 10,
+                    "ignore_exception": False,
+                }
+            )
+
+    def test_check_error_raises_on_write_with_field_to_check(self):
+        exception = self.env.ref("product_exception.excep_no_dump")
+        exception.active = True
+        product = self.env.ref("product.product_product_6").product_tmpl_id
+        with self.assertRaises(ValidationError):
+            product.with_context(test_product_check_exception=True).write(
+                {"ignore_exception": False, "standard_price": 10000}
+            )
+
+    def test_check_skip_error_on_write_with_context(self):
+        exception = self.env.ref("product_exception.excep_no_dump")
+        exception.active = True
+        product = self.env.ref("product.product_product_6").product_tmpl_id
+        product.with_context(skip_product_check_exception=True).write(
+            {"standard_price": 100}
+        )
