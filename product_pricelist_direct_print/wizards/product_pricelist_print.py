120c3dbd2fd7e57e9f2401c679623b088ffd5f81
Carlos Roca <carlos.roca@tecnativa.com>
2024-01-19 12:46:09 +0100

    [IMP] product_pricelist_direct_print: Allow printing the products sold from x date

--- product_pricelist_direct_print/wizards/product_pricelist_print.py
+++ product_pricelist_direct_print/wizards/product_pricelist_print.py
@@ -75,6 +75,10 @@ class ProductPricelistPrint(models.TransientModel):
     lang = fields.Selection(
         _lang_get, string="Language", default=lambda self: self.env.user.lang
     )
+    product_selling_date_threshold = fields.Datetime(
+        string="Selling date threshold",
+        help="Filter only the products ordered since this date",
+    )
 
     product_price = fields.Float(compute="_compute_product_price")
 
@@ -268,10 +272,15 @@ class ProductPricelistPrint(models.TransientModel):
 
     @api.model
     def _get_sale_order_domain(self, partner):
-        return [
+        domain = [
             ("state", "not in", ["draft", "sent", "cancel"]),
             ("partner_id", "child_of", partner.id),
         ]
+        if self.product_selling_date_threshold:
+            domain = expression.AND(
+                [domain, [("date_order", ">=", self.product_selling_date_threshold)]]
+            )
+        return domain
 
     def get_last_ordered_products_to_print(self):
         self.ensure_one()
@@ -283,7 +292,10 @@ class ProductPricelistPrint(models.TransientModel):
         )
         orders = orders.sorted(key=lambda r: r.date_order, reverse=True)
         products = orders.mapped("order_line").mapped("product_id")
-        return products[: self.last_ordered_products]
+        if self.last_ordered_products:
+            return products[: self.last_ordered_products]
+        else:
+            return products
 
     def get_pricelist_to_print(self):
         self.ensure_one()
@@ -346,7 +358,7 @@ class ProductPricelistPrint(models.TransientModel):
 
     def get_products_to_print(self):
         self.ensure_one()
-        if self.last_ordered_products:
+        if self.last_ordered_products or self.product_selling_date_threshold:
             products = self.get_last_ordered_products_to_print()
         else:
             if self.show_variants:
