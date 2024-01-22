fa5028445d7763bd46ff9d9ed694ba78532a0cb8
clementmbr <clement.mombereau@akretion.com.br>
2024-01-22 17:53:25 -0300

    [MIG] product_supplierinfo_code: Migration to 16.0

--- product_supplierinfo_code/__manifest__.py
+++ product_supplierinfo_code/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Supplierinfo Code",
     "summary": """
         Allows to get main supplierinfo product_code on product level""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
