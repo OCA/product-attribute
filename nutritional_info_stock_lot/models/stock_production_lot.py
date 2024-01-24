da201ec09fc8ddd8f19ae52ad455083f6879793d
Carlos Roca <carlos.roca@tecnativa.com>
2024-01-24 17:03:30 +0100

    [IMP] nutritional_info_*: Check if are types repeated on nutritional values

--- nutritional_info_stock_lot/models/stock_production_lot.py
+++ nutritional_info_stock_lot/models/stock_production_lot.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class StockProductionLot(models.Model):
@@ -10,3 +11,13 @@ class StockProductionLot(models.Model):
         comodel_name="nutritional.value.lot",
         inverse_name="lot_id",
     )
+
+    @api.constrains("nutritional_value_ids", "nutritional_value_ids.type_id")
+    def _check_nutritional_type_not_repeated(self):
+        for lot in self:
+            if lot.nutritional_value_ids and len(lot.nutritional_value_ids) != len(
+                lot.nutritional_value_ids.type_id
+            ):
+                raise UserError(
+                    _("Repeating types of nutritional values is not allowed.")
+                )
