74037125a9f8035e33043bc0ea39d4deda8b16c6
SilvioC2C <silvio.gregorini@camptocamp.com>
2024-01-29 16:03:38 +0100

    [IMP] product_template_tag: add parent tags

--- product_template_tags/models/product_template_tag.py
+++ product_template_tags/models/product_template_tag.py
@@ -1,13 +1,15 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplateTag(models.Model):
     _name = "product.template.tag"
     _description = "Product Tag"
     _order = "sequence, name"
+    _parent_store = True
 
     name = fields.Char(required=True, translate=True)
     sequence = fields.Integer(default=10)
@@ -27,6 +29,9 @@ class ProductTemplateTag(models.Model):
         string="Company",
         default=lambda self: self.env.company,
     )
+    parent_id = fields.Many2one("product.template.tag", index=True, ondelete="cascade")
+    child_ids = fields.One2many("product.template.tag", "parent_id")
+    parent_path = fields.Char(index=True, unaccent=False)
 
     _sql_constraints = [
         (
@@ -50,3 +55,33 @@ class ProductTemplateTag(models.Model):
             tag_id_product_count = dict(self.env.cr.fetchall())
         for rec in self:
             rec.products_count = tag_id_product_count.get(rec.id, 0)
+
+    def name_get(self):
+        res = []
+        for tag in self:
+            names = []
+            current = tag
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append((tag.id, " / ".join(reversed(names))))
+        return res
+
+    @api.model
+    def _name_search(
+        self, name="", args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        if name:
+            args = [("name", operator, name.split(" / ")[-1])] + list(args or [])
+        return super()._name_search(
+            name=name,
+            args=args,
+            operator=operator,
+            limit=limit,
+            name_get_uid=name_get_uid,
+        )
+
+    @api.constrains("parent_id")
+    def _check_parent_recursion(self):
+        if not self._check_recursion("parent_id"):
+            raise ValidationError(_("Tags can't be recursive."))
