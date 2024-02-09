d13910b744e879aa2329e436b9f3cd40710ea1f9
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-02-09 16:07:12 +0100

    [MIG] base_product_mass_addition: Migration to 16.0 [REF] Remove call to flush that are obsolete, in tests.

--- base_product_mass_addition/__manifest__.py
+++ base_product_mass_addition/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Base Product Mass Addition",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "license": "AGPL-3",
