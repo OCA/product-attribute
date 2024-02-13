7c125b51a142f1c5d190045e3781159cac9f6d41
Cyril VINH-TUNG <cyril@invitu.com>
2024-02-12 14:41:57 -1000

    [MIG] product_code_remove: Migration to 16.0

--- product_code_remove/__manifest__.py
+++ product_code_remove/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Remove Product Code from sale order line name",
-    "version": "10.0.0.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Product",
--- product_code_remove/models/product_product.py
+++ product_code_remove/models/product_product.py
@@ -1,13 +1,12 @@
 # Copyright 2019 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 # pylint: disable=missing-docstring
-from odoo import api, models
+from odoo import models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    @api.multi
     def name_get(self):
         result = []
         for this in self:
