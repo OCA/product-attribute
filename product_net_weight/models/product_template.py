ffb22a5e77b04b95b55aa77c8cdb2377a77f620c
peeranutjobgo <peeranutjobgo@gmail.com>
2024-02-15 12:55:11 +0700

    [MIG] product_net_weight : Migration to 17.0

--- product_net_weight/models/product_template.py
+++ product_net_weight/models/product_template.py
@@ -35,7 +35,7 @@ class ProductTemplate(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        templates = super(ProductTemplate, self).create(vals_list)
+        templates = super().create(vals_list)
         # This is needed to set given values to first variant after creation
         for template, vals in zip(templates, vals_list):
             if vals.get("net_weight"):
