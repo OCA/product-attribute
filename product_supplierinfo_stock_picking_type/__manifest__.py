21d43cd6304d70b74d7ffec6cbad112431a0d60b
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-27 08:56:06 +0100

    [MIG] product_supplierinfo_stock_picking_type: Migration to 16.0
    TT47849

--- product_supplierinfo_stock_picking_type/__manifest__.py
+++ product_supplierinfo_stock_picking_type/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product supplierinfo stock picking type",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
