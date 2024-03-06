d53def118673bc28d0a1de7ae663036c6d772882
Jairo Llopis <jairo@moduon.team>
2024-03-06 13:48:57 +0000

    [FIX] product_cost_security: ORM-level security
    Before this patch, inheriting modules had to manually add some logic related to guessing wether the user can access cost information. Also, smartypants users could still obtain the desired information through API calls.
    
    Now:
    - You can't read/write cost fields via API anymore.
    - Views automatically set those fields to readonly if the user has only read permissions (and the model inherits from the new mixin).
    - You don't need to enable debug mode anymore to follow configuration instructions.
    - Instructions improved.
    
    @moduon MT-5158

--- product_cost_security/models/product_template.py
+++ product_cost_security/models/product_template.py
@@ -1,19 +1,11 @@
 # Copyright 2018 Sergio Teruel - Tecnativa <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
+    _name = "product.template"
+    _inherit = ["product.template", "product.cost.security.mixin"]
 
+    # Inherited fields
     standard_price = fields.Float(groups="product_cost_security.group_product_cost")
-    user_can_update_cost = fields.Boolean(compute="_compute_user_can_update_cost")
-
-    @api.depends_context("uid")
-    def _compute_user_can_update_cost(self):
-        """A user could have full cost permissions but no product edition permissions.
-        We want to prevent those from updating costs."""
-        for product in self:
-            product.user_can_update_cost = self.env.user.has_group(
-                "product_cost_security.group_product_edit_cost"
-            )
