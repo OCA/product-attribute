614cbbb3eb9eca5eabc4791a1a6a1cb0ddc28e36
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-03-20 16:00:26 +0100

    product_lot_sequence: port fix from 15.0 to 16.0

--- product_lot_sequence/models/stock_move.py
+++ product_lot_sequence/models/stock_move.py
@@ -8,16 +8,12 @@ class StockMove(models.Model):
 
     def action_show_details(self):
         """Avoid calling and incrementing the sequence if not needed or already done"""
-        seq_policy = self.env["stock.production.lot"]._get_sequence_policy()
+        seq_policy = self.env["stock.lot"]._get_sequence_policy()
         if seq_policy in ("product", "global"):
             # If move is not supposed to assign serial pass empty string for next serial
             if not self.display_assign_serial:
-                return super(
-                    StockMove, self.with_context(force_next_serial="")
-                ).action_show_details()
+                self = self.with_context(force_next_serial="")
             # If the sequence was already called once, avoid calling it another time
             elif self.next_serial:
-                return super(
-                    StockMove, self.with_context(force_next_serial=self.next_serial)
-                ).action_show_details()
-        return super().action_show_details()
+                self = self.with_context(force_next_serial=self.next_serial)
+        return super(StockMove, self).action_show_details()
