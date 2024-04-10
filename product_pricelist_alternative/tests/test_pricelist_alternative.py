4334606af78e2a676bfbbf7ccb42831f482cb305
Telmo Santos <telmo.santos@camptocamp.com>
2024-04-10 15:45:41 +0200

    [IMP] product_pricelist_alternative validate alternative pricelist with based formula pricelist

--- product_pricelist_alternative/tests/test_pricelist_alternative.py
+++ product_pricelist_alternative/tests/test_pricelist_alternative.py
@@ -1,6 +1,6 @@
 # Copyright 2024 Camptocamp (<https://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+from odoo.exceptions import ValidationError
 from odoo.tests import common, tagged
 
 from .common import CommonProductPricelistAlternative
@@ -92,3 +92,15 @@ class TestPricelistAlternative(
             )._get_product_price(self.usb_adapter, 1.0),
             95,
         )
+
+    def test_check_pricelist_alternative_items_based_on_other_pricelist(self):
+        with self.assertRaises(ValidationError) as e:
+            self.alternative_pricelist_01.item_ids.write(
+                {
+                    "compute_price": "formula",
+                    "base": "pricelist",
+                    "base_pricelist_id": self.alternative_pricelist_02.id,
+                }
+            )
+        msg = "Formulas based on another pricelist are not allowed on alternative pricelists."
+        self.assertIn(msg, e.exception.args[0])
