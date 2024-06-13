4994c508121427ec8a9364624ef7fb2f553f31d1
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-06-13 14:40:00 +0200

    Replace 'Camptocamp SA' with 'Camptocamp'

--- stock_product_template_tags/__manifest__.py
+++ stock_product_template_tags/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": "Show product tags menu in Inventory app",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "depends": ["product_template_tags", "stock"],
     "data": ["views/product_template_tag.xml"],
