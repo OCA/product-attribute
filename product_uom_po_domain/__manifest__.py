e8391d8a01639ea29251e20d1becc1dd2f64fcae
Ruchir Shukla <ruchir@bizzappdev.com>
2024-07-01 12:20:09 +0530

    [MIG] product_uom_po_domain: Migration to 17.0

--- product_uom_po_domain/__manifest__.py
+++ product_uom_po_domain/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product - Domain on Purchase UoM",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Product",
     "maintainers": ["legalsylvain"],
     "author": "GRAP, Odoo Community Association (OCA)",
