dcc0986cf07e31ece6380097b5bf3d47739801ba
Franck <fgaviria1@gmail.com>
2024-07-12 20:47:41 -0400

    [IMP] product_stock_state: pre-commit auto fixes

--- product_stock_state/__init__.py
+++ product_stock_state/__init__.py
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
+=======
+# -*- coding: utf-8 -*-
+
+>>>>>>> [ADD] add product_stock_state
 from . import models
--- product_stock_state/models/__init__.py
+++ product_stock_state/models/__init__.py
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 from . import res_company
 from . import res_config_settings
 from . import product_category
 from . import product_template
 from . import product_product
+=======
+# -*- coding: utf-8 -*-
+
+from . import product
+>>>>>>> [ADD] add product_stock_state
