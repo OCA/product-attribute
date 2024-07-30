29b61dfda9915cedcf6ead4bd2d50f680ee78696
Celina Devigili <ced@adhoc.com.ar>
2024-07-30 12:37:55 -0300

    [MIG] product_code_mandatory: Migration to 17.0

--- product_code_mandatory/__init__.py
+++ product_code_mandatory/__init__.py
@@ -1,12 +1,8 @@
 from . import models
 
-from odoo import api, SUPERUSER_ID
 
-
-def pre_init_product_code(cr):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-
-    cr.execute(
+def pre_init_product_code(env):
+    env.cr.execute(
         """
         SELECT product_tmpl_id from product_product
         WHERE default_code is NULL
@@ -14,8 +10,8 @@ def pre_init_product_code(cr):
         GROUP BY product_tmpl_id
         HAVING COUNT(product_tmpl_id) = 1"""
     )
-    product_template_ids = [x[0] for x in cr.fetchall()]
-    cr.execute(
+    product_template_ids = [x[0] for x in env.cr.fetchall()]
+    env.cr.execute(
         """UPDATE product_product
         SET default_code = 'DEFAULT' || nextval('ir_default_id_seq')
         WHERE default_code is NULL
