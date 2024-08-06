0ab740c923a08394b89c4978a6da3c3d8af8007d
kobros-tech <alkobroslymohamed@gmail.com>
2024-08-06 16:40:37 +0300

    [IMP] product_pricelist_fixed_currency_rate: pre-commit auto fixes

--- product_pricelist_fixed_currency_rate/__init__.py
+++ product_pricelist_fixed_currency_rate/__init__.py
@@ -1,1 +1,2 @@
 from . import models
+from .hooks import pre_init_hook, post_init_hook, uninstall_hook, post_load
---
+++ product_pricelist_fixed_currency_rate/hooks.py
@@ -0,0 +1,6 @@
+{
+    'pre_init_hook': 'pre_init_hook',
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+    'post_load': 'post_load',
+}
