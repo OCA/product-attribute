46cf599e66affcbcab4e59cdf843c2289bdd1376
Víctor Martínez <victor.martinez@tecnativa.com>
2024-08-08 13:02:50 +0200

    [ADD] product_readonly_security: New module
    TT50471
    
    [UPD] Update product_readonly_security.pot
    
    [BOT] post-merge updates

---
+++ product_readonly_security/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Readonly Security",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "version": "13.0.1.0.0",
+    "depends": ["product"],
+    "license": "AGPL-3",
+    "category": "Product",
+    "data": ["security/product_readonly_security.xml"],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
---
+++ product_readonly_security/readme/USAGE.rst
@@ -0,0 +1,4 @@
+#. Create or edit a user and uncheck the 'Product edition' permission.
+#. Login with that user.
+#. Go to any product.
+#. None can be created, modified or deleted.
