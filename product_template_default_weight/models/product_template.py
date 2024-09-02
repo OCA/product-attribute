439e9ee2207d7c09e918bd9672474a030d1efeeb
Mathieu <mathieu.delva@hotmail.com>
2024-09-02 13:49:46 +0200

    [FIX] pre-commit

--- product_template_default_weight/models/product_template.py
+++ product_template_default_weight/models/product_template.py
@@ -8,9 +8,10 @@ from odoo import api, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-
-    @api.depends('product_variant_ids', 'product_variant_ids.weight')
+    @api.depends("product_variant_ids", "product_variant_ids.weight")
     def _compute_weight(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        unique_variants = self.filtered(
+            lambda template: len(template.product_variant_ids) == 1
+        )
         for template in unique_variants:
             template.weight = template.product_variant_ids.weight
