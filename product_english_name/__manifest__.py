111866b23344d4b47caee114dce4c6c2ef7b0972
David Beal <david.beal@akretion.com>
2024-09-30 16:55:50 +0200

    [ADD] product_english_name: module which displays the product name in English near the products name in user's language

---
+++ product_english_name/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Akretion
+{
+    "name": "Product English Name",
+    "summary": """Display the product name in English besides the product name
+     in the users language.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Alpha",
+    "category": "Product",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": [
+        "product",
+    ],
+    "maintainers": ["bealdav"],
+    "data": [
+        "security/groups.xml",
+        "views/product_template.xml",
+    ],
+}
