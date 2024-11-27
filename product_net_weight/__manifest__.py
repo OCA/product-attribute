e265f34a8f8b9582079f27c89b5738cd697ec2d2
Alexis de Lattre <alexis.delattre@akretion.com>
2024-11-27 17:27:12 +0100

    [IMP] product_net_weight: pre-commit auto fixes for v18 migration

--- product_net_weight/__manifest__.py
+++ product_net_weight/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Products - Net Weight",
     "summary": "Add 'Net Weight' on product models",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "author": "GRAP,Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
