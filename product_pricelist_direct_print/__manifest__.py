f3e1ce24b092770474a338635a4648ecd0d09d62
David Beal <david.beal@akretion.com>
2024-12-06 14:23:04 +0100

    [IMP] product_pricelist_direct_print: pre-commit auto fixes

--- product_pricelist_direct_print/__manifest__.py
+++ product_pricelist_direct_print/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Pricelist Direct Print",
     "summary": "Print price list from menu option, product templates, "
     "products variants or price lists",
-    "version": "16.0.1.2.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, GRAP, Odoo Community Association (OCA)",
@@ -15,7 +15,7 @@
     "data": [
         "security/ir.model.access.csv",
         "reports/report_product_pricelist.xml",
-        "data/mail_template_data.xml",
+        # "data/mail_template_data.xml",
         "wizards/product_pricelist_print_view.xml",
     ],
 }
