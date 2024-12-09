b676d3c965109d913cddc28ad367d743bc7380bd
kobros-tech <alkobroslymohamed@gmail.com>
2024-12-09 23:40:55 +0300

    [16.0][FIX] product_abc_classification_sale_stock: fix deprecated methods and tests

--- product_abc_classification_sale_stock/tests/test_abc_classification_profile.py
+++ product_abc_classification_sale_stock/tests/test_abc_classification_profile.py
@@ -255,15 +255,15 @@ class TestABCClassificationProfile(TransactionCase):
         # test computed classification and check that inactive products are
         # not taken into account
         self.product1.active = False
-        self.product1.refresh()
+        self.product1.flush_recordset()
         self.stock_profile._compute_abc_classification()
         self.assertFalse(self.product1.abc_classification_product_level_ids)
         self.product1.active = True
-        self.product1.refresh()
+        self.product1.flush_recordset()
         self.stock_profile._compute_abc_classification()
         self.assertTrue(self.product1.abc_classification_product_level_ids)
         self.product1.active = False
-        self.product1.refresh()
+        self.product1.flush_recordset()
         self.stock_profile._compute_abc_classification()
         self.assertFalse(self.product1.abc_classification_product_level_ids)
 
