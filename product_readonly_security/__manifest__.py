12f1a538d9c4d8c686e10b09d23c860fb5970952
Víctor Martínez <victor.martinez@tecnativa.com>
2024-12-12 12:32:04 +0100

    [MIG] product_readonly_security: Migration to 14.0
    TT52176

--- product_readonly_security/__manifest__.py
+++ product_readonly_security/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Product Readonly Security",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["product"],
     "license": "AGPL-3",
     "category": "Product",
