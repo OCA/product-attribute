68ca912e3bd7bfe60e712098b897992abf5bea59
Víctor Martínez <victor.martinez@tecnativa.com>
2024-12-18 13:31:44 +0100

    [MIG] product_attachment_zipped_download: Migration to 16.0
    TT52271
    
    [UPD] Update product_attachment_zipped_download.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: product-attribute-16.0/product-attribute-16.0-product_attachment_zipped_download
    Translate-URL: https://translation.odoo-community.org/projects/product-attribute-16-0/product-attribute-16-0-product_attachment_zipped_download/

--- product_attachment_zipped_download/__manifest__.py
+++ product_attachment_zipped_download/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product Attachment Zipped Download",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Product Management",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["attachment_zipped_download", "product"],
-    "data": ["views/product_product_views.xml", "views/product_template_views.xml"],
+    "data": ["data/ir_actions_server_data.xml"],
     "installable": True,
     "maintainers": ["victoralmau"],
 }
