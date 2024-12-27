25ff0eac5531220f9b4a41b8b863ca259bca1f48
Heliconia Solutions <oca@heliconia.in>
2024-12-27 11:51:14 +0530

    [MIG] product_document_domain: Migration to 18.0

--- product_document_domain/models/product_product.py
+++ product_document_domain/models/product_product.py
@@ -8,18 +8,22 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def _compute_product_document_count(self):
-        res = super()._compute_product_document_count()
+        super()._compute_product_document_count()
+        all_template_ids = self.mapped("product_tmpl_id").ids
+        if not all_template_ids:
+            return
+        tmpl_doc_groups = self.env["product.document"].read_group(
+            domain=[
+                ("res_model", "=", "product.template"),
+                ("res_id", "in", all_template_ids),
+            ],
+            fields=["res_id"],
+            groupby=["res_id"],
+        )
+        count_by_template = {g["res_id"]: g["res_id_count"] for g in tmpl_doc_groups}
         for product in self:
-            template = product.product_tmpl_id
-            product.product_document_count += product.env[
-                "product.document"
-            ].search_count(
-                [
-                    ("res_model", "=", "product.template"),
-                    ("res_id", "=", template.id),
-                ]
-            )
-        return res
+            tmpl_count = count_by_template.get(product.product_tmpl_id.id, 0)
+            product.product_document_count += tmpl_count
 
     def action_open_documents(self):
         res = super().action_open_documents()
--- product_document_domain/models/product_template.py
+++ product_document_domain/models/product_template.py
@@ -8,17 +8,24 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     def _compute_product_document_count(self):
+        super()._compute_product_document_count()
+        all_variant_ids = self.mapped("product_variant_ids").ids
+        if not all_variant_ids:
+            return
+        variant_doc_groups = self.env["product.document"].read_group(
+            domain=[
+                ("res_model", "=", "product.product"),
+                ("res_id", "in", all_variant_ids),
+            ],
+            fields=["res_id"],
+            groupby=["res_id"],
+        )
+        count_by_variant = {g["res_id"]: g["res_id_count"] for g in variant_doc_groups}
         for template in self:
-            res = super()._compute_product_document_count()
-            template.product_document_count -= template.env[
-                "product.document"
-            ].search_count(
-                [
-                    ("res_model", "=", "product.product"),
-                    ("res_id", "in", template.product_variant_ids.ids),
-                ]
+            variant_doc_count = sum(
+                count_by_variant.get(vid, 0) for vid in template.product_variant_ids.ids
             )
-            return res
+            template.product_document_count -= variant_doc_count
 
     def action_open_documents(self):
         res = super().action_open_documents()
