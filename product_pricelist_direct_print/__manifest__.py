3e7779e251bb7e6faa8dfdb7acb51503d33e6662
Alejo Marinsalda <marinsaldaalejo@gmail.com>
2025-01-31 07:01:42 -0300

    [MOD]ticket_9207 -manifest version update

--- product_pricelist_direct_print/__manifest__.py
+++ product_pricelist_direct_print/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Pricelist Direct Print",
     "summary": "Print price list from menu option, product templates, "
     "products variants or price lists",
-    "version": "17.0.1.2.6",
+    "version": "17.0.1.2.7",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, GRAP, Odoo Community Association (OCA)",
