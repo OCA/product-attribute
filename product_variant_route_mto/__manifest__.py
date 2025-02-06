6ec3ddee430c7ba930eab5a7d09f04db76d16f0c
chaule97 <lebaochau97@gmail.com>
2025-02-06 10:08:45 +0700

    [MIG] rename stock_product_variant_mto

--- product_variant_route_mto/__manifest__.py
+++ product_variant_route_mto/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    "name": "Stock Product Variant MTO",
+    "name": "Product Variant Route MTO",
     "summary": "Allow to individually set variants as MTO",
     "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "website": "https://github.com/OCA/product-attribute",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "maintainers": ["mmequignon"],
     "license": "AGPL-3",
