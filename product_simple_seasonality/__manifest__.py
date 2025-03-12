bbde805e9b417a7b722d8193f5896527965b94de
Bhavesh Heliconia <oca@heliconia.in>
2025-03-12 16:52:37 +0530

    [MIG] product_simple_seasonality: Migration to 18.0

--- product_simple_seasonality/__manifest__.py
+++ product_simple_seasonality/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Simple Seasonality",
     "summary": """
         Product seasonality""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
