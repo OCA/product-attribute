b1247b2f7a38bd7fb37dcce9fc829ef584a5310d
Jacques-Etienne Baudoux <je@bcim.be>
2025-05-07 13:27:56 +0200

    [MIG] product_route_mto: Migration to 18.0

--- product_route_mto/__manifest__.py
+++ product_route_mto/__manifest__.py
@@ -6,10 +6,11 @@
     "summary": """
         This module allows to compute if a product is an 'MTO' one from its
         configured routes""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
+    "maintainers": ["jbaudoux"],
     "depends": [
         "stock_route_mto",
     ],
