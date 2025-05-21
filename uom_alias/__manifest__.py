9bb5859e50c28ec701f11f3a2d5c1c4c4f03351d
Renato Lima <renatonlima@gmail.com>
2025-05-21 15:34:19 -0300

    [MIG] uom_alias: Migration to 17.0

--- uom_alias/__manifest__.py
+++ uom_alias/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "UOM Alias",
     "summary": """Adds alias for UOM""",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "development_status": "Production/Stable",
     "author": "Akretion, Odoo Community Association (OCA)",
--- uom_alias/readme/USAGE.md
+++ uom_alias/readme/USAGE.md
@@ -1,4 +1,4 @@
 When you search for a UoM in a document, you can search by the name of
 the UoM or by one of its alias.
 
-\![image\](../static/img/screenshot-2.png)
+.. image:: ../static/img/screenshot-2.png
