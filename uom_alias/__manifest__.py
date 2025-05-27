159c700e7e756fbb3de99836327d653790534fca
Renato Lima <renatonlima@gmail.com>
2025-05-27 15:36:34 -0300

    [MIG] uom_alias: Migration to 18.0

--- uom_alias/__manifest__.py
+++ uom_alias/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "UOM Alias",
     "summary": """Adds alias for UOM""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "development_status": "Production/Stable",
     "author": "Akretion, Odoo Community Association (OCA)",
