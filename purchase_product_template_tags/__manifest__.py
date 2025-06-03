62c59ce34154364200b4bfbfc3755047f643d768
Angel Rivas <angel.rivas@sygel.es>
2025-06-03 13:13:59 +0200

    [MIG] purchase_product_template_tags: Migration to 17.0

--- purchase_product_template_tags/__manifest__.py
+++ purchase_product_template_tags/__manifest__.py
@@ -5,11 +5,11 @@
 {
     "name": "Purchase Product Template Tags",
     "summary": "Show product tags menu in Purchase app",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
-    "depends": ["product_template_tags", "purchase"],
+    "depends": ["purchase"],
     "data": ["views/product_template_tag.xml"],
     "maintainers": ["ivantodorovich"],
     "auto_install": True,
