f77be245f4aca14ffe2f4755c9cfa0bdd8d248a7
AJamal13 <135590484+AJamal13@users.noreply.github.com>
2025-06-24 01:40:47 +0300

    Revert "Add tests for finance profile validation and margin exclusion"
    This reverts commit e1d7e1e48e86afb3fef9f491a9eacd5abf8a165d.

--- product_abc_classification_finance/__manifest__.py
+++ product_abc_classification_finance/__manifest__.py
@@ -6,15 +6,12 @@
     "author": "AJamal13,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/product-attribute",
-    "depends": ["product_abc_classification_sale_stock", "sale_margin"],
+    "depends": ["product_abc_classification_sale_stock","sale_margin"],
     "data": [
         "security/ir.model.access.csv",
         "views/abc_finance_sale_level_history_views.xml",
         "views/abc_classification_product_level_views.xml",
         "views/abc_classification_profile.xml",
-    ],
-    "demo": [
-        "data/abc_classification_finance_demo.xml",
     ],
     "installable": True,
     "application": False,
