e9a0e3869e5764f8470958a310daf1431d5217f8
Ruchir Shukla <ruchir@bizzappdev.com>
2025-09-30 21:34:21 +0530

    [MIG] product_category_product_link: Migration to 19.0

--- product_category_product_link/__manifest__.py
+++ product_category_product_link/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Category Product Link",
     "summary": """
         Allows to get products from a category""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
