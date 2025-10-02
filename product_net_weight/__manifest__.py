b535253f650b3f6e91fc2b0793c99e8117e748af
Vicent-S73 <vicent@studio73.es>
2025-10-02 15:09:22 +0200

    [IMP] product_net_weight: pre-commit auto fixes

--- product_net_weight/__manifest__.py
+++ product_net_weight/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Products - Net Weight",
     "summary": "Add 'Net Weight' on product models",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Product",
     "author": "GRAP,Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
