c4cb411b0f1f1acf4bd0af33490fc11a17258b6b
tendil <mejta.diman@gmail.com>
2025-11-20 16:32:14 +0000

    [ADD] product_sale_team: add module
    - Add sales_team_id Many2one field on product.template to store
    the Sales Team for a product.
    - Extend the product template form view to display the Sales Team
    field on the General Information tab.
    - Add basic documentation (CONTEXT, DESCRIPTION, USAGE) and a test.
    
    Task: 5084

---
+++ product_sale_team/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2025 Cetmix OÜ
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Product Sale Team",
+    "summary": "Sale Team for products",
+    "version": "18.0.1.0.0",
+    "category": "Product",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "LGPL-3",
+    "depends": [
+        "crm",
+        "product",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
---
+++ product_sale_team/readme/CONTEXT.md
@@ -0,0 +1,1 @@
+Sometimes you would need to define a Sales Team for specific product. Eg it's a piece of  a complex equipment that has a specific sales flow.
