751421525a070cf725c5834ffd5beead35c055f2
christian-ramos-tecnativa <christian.ramos@tecnativa.com>
2025-11-28 19:09:22 +0000

    [FIX] product_catalog_stock: avoid price check error

--- product_catalog_stock/__manifest__.py
+++ product_catalog_stock/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Product Catalog",
     "summary": "Use the product catalog on stock pickings",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "license": "AGPL-3",
