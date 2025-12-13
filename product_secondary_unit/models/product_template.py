cb141a3527436403874100dfe1258b258ac6073e
yostashiro <tashiro@quartile.co>
2025-12-13 08:45:21 +0000

    [IMP] product_secondary_unit: add template/variant sync helpers
    Add helper methods for syncing secondary UoM fields between product.template and product.product:
    - _compute_template_secondary_uom_field
    - _inverse_template_secondary_uom_field
    
    Can be used by dependent modules like stock_secondary_unit.

--- product_secondary_unit/models/product_template.py
+++ product_secondary_unit/models/product_template.py
@@ -21,3 +21,39 @@ class ProductTemplate(models.Model):
             and self.secondary_uom_ids[0]
             or self.secondary_uom_ids
         )
+
+    def _compute_template_secondary_uom_field(self, fname):
+        """Helper to sync template secondary UoM field from variant.
+
+        Can be used in modules depending on product_secondary_unit to implement
+        compute methods for secondary UoM fields on product.template.
+
+        - Single variant: Sync the variant's value to the template
+        - Multiple variants: Keep the existing template value
+
+        :param str fname: name of the secondary UoM field to compute
+        """
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                template[fname] = template.product_variant_ids[fname]
+            # Keep existing value when multiple variants (don't clear)
+
+    def _inverse_template_secondary_uom_field(self, fname):
+        """Helper to propagate template secondary UoM field to variants.
+
+        Can be used in modules depending on product_secondary_unit to implement
+        inverse methods for secondary UoM fields on product.template.
+
+        - Single variant: Always sync the template's value to the variant
+        - Multiple variants: Only update variants without their own setting
+
+        :param str fname: name of the secondary UoM field to propagate
+        """
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                variants_to_update = template.product_variant_ids
+            else:
+                variants_to_update = template.product_variant_ids.filtered(
+                    lambda v, f=fname: not v[f]
+                )
+            variants_to_update[fname] = template[fname]
