d436b6c5a8f40811099745c958fe9c3a3e576cd7
Paolo Baroncini <paolo.baroncini@gmail.com>
2025-12-23 09:02:32 +0100

    [FIX] product_abc_classification: aggiunto todo migrazione 15.0

--- product_abc_classification/__manifest__.py
+++ product_abc_classification/__manifest__.py
@@ -2,6 +2,8 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# TODO: MIGRATION 15.0, saltare perchè questo modulo è backport dalla 16.0
+
 {
     "name": "Product Abc Classification",
     "summary": """
