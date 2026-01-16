c830117447a1a6c4dcda90c16a2bd0dcbb63792d
Aungkokolin1997 <lin@quartile.co>
2026-01-16 04:30:14 +0000

    [ADD] product_tag_view

---
+++ product_tag_view/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Tag View",
+    "summary": "Show product tags on General Information tab",
+    "version": "18.0.1.0.0",
+    "category": "Product",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "depends": ["product"],
+    "data": [
+        "views/product_views.xml",
+    ],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
+    "installable": True,
+}
