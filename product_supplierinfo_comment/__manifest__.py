e7dee57b96e2c12c02fc249facb535a5998a7e68
cristina-hidalgo-tecnativa <cristina.hidalgo@tecnativa.com>
2026-03-03 11:59:43 +0100

    [MIG] product_supplierinfo_comment: Migration to 18.0

--- product_supplierinfo_comment/__manifest__.py
+++ product_supplierinfo_comment/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Add the comment field to the product supplier info
         """,
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
