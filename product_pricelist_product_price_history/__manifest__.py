7985dde75fdeda3e67edfc20cbbcb1e0465edb16
Antoni Marroig Campomar <amarroig@apsl.net>
2026-03-11 18:04:40 +0100

    [ADD] product_pricelist_product_history: New module product_pricelist_product_history

---
+++ product_pricelist_product_price_history/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 APSL-Nagarro Antoni Marroig
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Pricelist Product Price History",
+    "summary": "Track product price history within pricelists",
+    "version": "18.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "APSL-Nagarro, Odoo Community Association (OCA)",
+    "maintainers": ["peluko00"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_pricelist_item_history_views.xml",
+    ],
+}
---
+++ product_pricelist_product_price_history/readme/USAGE.md
@@ -0,0 +1,11 @@
+1. Automatic Tracking You don’t need to do anything! Every time you change a "Fixed Price" in any pricelist, the system automatically saves the old price, the new price, and the person who made the change.
+
+2. View the History Go to Sales > Products > Pricelist History. Here you will see a list of all recent price adjustments.
+
+3. Print Labels for New Prices 
+
+* Open the Pricelist History list.
+
+* Select the products (tick the checkboxes) that have new prices.
+
+* Click the "Print Labels" button that appears in the top header.
