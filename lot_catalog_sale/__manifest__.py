191366987d426d703bc86c1726ccbf98ef7d8c1c
Víctor Martínez <victor.martinez@tecnativa.com>
2026-04-21 10:26:11 +0200

    [ADD] lot_catalog_sale: New module
    TT61752

---
+++ lot_catalog_sale/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Sale Lot Catalog",
+    "version": "18.0.1.0.0",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["lot_catalog", "sale_order_lot_selection"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "maintainers": ["victoralmau"],
+}
