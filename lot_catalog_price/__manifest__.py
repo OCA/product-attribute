eb97ea51e2b2a889336960f23c38900f1f45c1ca
Víctor Martínez <victor.martinez@tecnativa.com>
2026-04-21 10:01:04 +0200

    [ADD] lot_catalog_price: New module
    TT61752

---
+++ lot_catalog_price/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Lot Catalog Price",
+    "version": "18.0.1.0.0",
+    "website": "https://github.com/OCA/product-attribute",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["lot_catalog", "stock_lot_price"],
+    "data": [],
+    "maintainers": ["victoralmau"],
+}
---
+++ lot_catalog_price/models/stock_lot.py
@@ -0,0 +1,12 @@
+# Copyright 2026 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockLot(models.Model):
+    _inherit = "stock.lot"
+
+    def _get_lst_price(self):
+        _res = super()._get_lst_price()
+        return self.lst_price
