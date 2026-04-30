e48d0ba9fa4415a62386f63b291ab5db7b0bff9a
Raphael Vicini <raphael.vicini@acsone.eu>
2026-04-30 14:24:50 +0200

    [IMP] uom_qty_unique: pre-commit auto fixes

--- uom_qty_unique/models/uom_uom.py
+++ uom_qty_unique/models/uom_uom.py
@@ -5,13 +5,12 @@ from odoo import models
 
 
 class UomUom(models.Model):
-
     _inherit = "uom.uom"
 
     _sql_constraints = [
         (
             "uom_qty_category_unique",
-            "EXCLUDE (factor WITH =, category_id WITH =)" "WHERE (active=True)",
+            "EXCLUDE (factor WITH =, category_id WITH =)WHERE (active=True)",
             "Only one active unit of measure per factor and per category should be active.",
         )
     ]
--- uom_qty_unique/readme/USAGE.rst
+++
@@ -1,2 +0,0 @@
-* Create a unit of measure with a duplicate quantity factor and category
-* An error should be raised.
