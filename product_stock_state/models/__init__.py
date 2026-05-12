f0a41114e213920c659860016667f8078f5c39a0
uncannycs <uncannycs@gmail.com>
2026-05-12 20:10:50 +0530

    [MIG] product_stock_state : Migration to 19.0.

--- product_stock_state/models/__init__.py
+++ product_stock_state/models/__init__.py
@@ -1,5 +1,7 @@
-from . import res_company
-from . import res_config_settings
-from . import product_category
-from . import product_template
-from . import product_product
+from . import (
+    product_category,
+    product_product,
+    product_template,
+    res_company,
+    res_config_settings,
+)
--- product_stock_state/tests/test_product_stock_state.py
+++ product_stock_state/tests/test_product_stock_state.py
@@ -12,14 +12,54 @@ class TestProductStockState(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.company = cls.env.ref("base.main_company")
-        cls.category_furniture = cls.env.ref("product.product_category_5")
-        cls.category_saleable = cls.env.ref("product.product_category_1")
-        cls.product_chair = cls.env.ref("product.product_product_12")
-        cls.product_threshold_on_company = cls.env.ref(
-            "product_stock_state.product_setting_by_company"
+        # Create categories locally
+        cls.category_saleable = cls.env["product.category"].create(
+            {
+                "name": "Test Saleable",
+                "manual_stock_state_threshold": 10,
+            }
         )
-        cls.product_threshold_on_product = cls.env.ref(
-            "product_stock_state.product_setting_by_product"
+        cls.category_furniture = cls.env["product.category"].create(
+            {
+                "name": "Test Furniture",
+                "parent_id": cls.category_saleable.id,
+            }
+        )
+        cls.category_no_threshold = cls.env["product.category"].create(
+            {
+                "name": "Test No Threshold",
+            }
+        )
+        # Create products locally
+        cls.product_chair = cls.env["product.product"].create(
+            {
+                "name": "Test Chair",
+                "categ_id": cls.category_furniture.id,
+                "type": "consu",
+            }
+        )
+        cls.product_threshold_on_company = cls.env["product.product"].create(
+            {
+                "name": "Test Product Company Threshold",
+                "categ_id": cls.category_saleable.id,
+                "type": "consu",
+                "company_id": cls.company.id,
+            }
+        )
+        cls.product_threshold_on_product = cls.env["product.product"].create(
+            {
+                "name": "Test Product Manual Threshold",
+                "categ_id": cls.category_saleable.id,
+                "type": "consu",
+                "manual_stock_state_threshold": 30,
+            }
+        )
+        cls.product_no_threshold = cls.env["product.product"].create(
+            {
+                "name": "Test Product No Threshold",
+                "categ_id": cls.category_no_threshold.id,
+                "type": "consu",
+            }
         )
 
     def test_01_global_product(self):
@@ -53,3 +93,10 @@ class TestProductStockState(TransactionCase):
     def test_05_state_out_of_stock(self):
         """Test Stock State computation"""
         self.assertEqual(self.product_threshold_on_product.stock_state, "out_of_stock")
+
+    def test_06_company_fallback(self):
+        """Test fallback to company threshold"""
+        self.assertEqual(
+            self.product_no_threshold._get_stock_state_threshold(),
+            self.company.stock_state_threshold,
+        )
