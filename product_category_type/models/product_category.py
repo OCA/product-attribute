b11242d9e30e48b1a2a8a24cc121775997f63509
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-26 23:46:29 +0200

    [IMP] product_category_type: when creating in batch (import / script / load ...) many categories, directly configure correctly new 'view' categories

--- product_category_type/models/product_category.py
+++ product_category_type/models/product_category.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductCategory(models.Model):
@@ -18,3 +18,13 @@ class ProductCategory(models.Model):
         " that can be used as the parent of another category"
         " to create a hierarchical structure.",
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        new_items = super().create(vals_list)
+        # new created parent categories will be set to 'view' type
+        # to avoid futur misconfiguration
+        new_items.filtered(lambda x: x.child_id and x.type == "normal").write(
+            {"type": "view"}
+        )
+        return new_items
