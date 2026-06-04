70b57e37b5680b278d705f012e4cf74d9133b664
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-06-04 14:20:26 +0200

    [PERF] product_state_shortage: optimize product state reset during stock move validation
    Avoid relational filtering loops on incoming moves.

--- product_state_shortage/models/stock_move.py
+++ product_state_shortage/models/stock_move.py
@@ -11,12 +11,16 @@ class StockMove(models.Model):
         res = super()._action_done(cancel_backorder=cancel_backorder)
 
         incoming_moves = self.filtered(lambda m: m.picking_code == "incoming")
-        if incoming_moves:
-            shortage_products_to_reset = incoming_moves.product_id.filtered(
-                lambda p: p.product_state_id.is_shortage
-            )
+        if not incoming_moves:
+            return res
 
-            if shortage_products_to_reset:
-                shortage_products_to_reset.product_tmpl_id._reset_default_state()
+        shortage_products = self.env["product.product"].search(
+            [
+                ("id", "in", incoming_moves.product_id.ids),
+                ("product_state_id.is_shortage", "=", True),
+            ]
+        )
+        if shortage_products:
+            shortage_products.product_tmpl_id._reset_default_state()
 
         return res
