aac6b79b75e2c5747457a969eeac15a4d977e965
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-07-27 15:52:00 +0530

    [19.0][MIG]product_pricelist_by_contact: Migration to 19.0

--- product_pricelist_by_contact/__manifest__.py
+++ product_pricelist_by_contact/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Pricelist Per Contact",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
