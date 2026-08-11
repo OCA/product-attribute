78da100f913f316729e0598afce4d8c61af9eff1
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-08-11 13:10:24 +0530

    [19.0][MIG]product_pricelist_direct_print_website_sale: Migration to 19.0

--- product_pricelist_direct_print_website_sale/__manifest__.py
+++ product_pricelist_direct_print_website_sale/__manifest__.py
@@ -3,13 +3,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Pricelist Print Website Sale",
-    "summary": "Extend Product Pricelist Direct Print for filter by public"
-    " categories",
-    "version": "18.0.1.0.1",
+    "summary": "Extend Product Pricelist Direct Print for filter by public categories",
+    "version": "19.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "maintainers": ["CarlosRoca13"],
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "product_pricelist_direct_print",
