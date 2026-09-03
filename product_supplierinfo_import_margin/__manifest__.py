979811d5abb2b8c7a94f8f916d98b0778832f65c
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-09-03 19:11:18 +0530

    [19.0][MIG]product_supplierinfo_import_margin: Migration to 19.0

--- product_supplierinfo_import_margin/__manifest__.py
+++ product_supplierinfo_import_margin/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Import supplier pricelists set margins",
     "summary": "Import supplier pricelists and margins",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Tecnativa, Odoo Community Association (OCA)",
